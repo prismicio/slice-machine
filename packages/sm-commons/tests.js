af39eabf524aa7827161c65154681513a7802014
Hugo Villain <hugo.villain@prismic.io>
2020-02-11 15:21:31 +0100

    Update workflow

--- packages/sm-commons/tests.js
+++ packages/sm-commons/tests.js
@@ -5,6 +5,7 @@ function smConfig(config) {
   expect(config).to.have.property('gitUrl')
   expect(config).to.have.property('framework')
   expect(config).to.have.property('this-should-break')
+  console.log('here!')
 }
 
 module.exports = {
