05a67c262f890e0f77ccc7dac9faddd13fb5d504
Hugo Villain <hugo.villain@prismic.io>
2020-02-11 16:03:45 +0100

    Update sm-commons package

--- packages/sm-commons/tests.js
+++ packages/sm-commons/tests.js
@@ -5,7 +5,7 @@ function smConfig(config) {
   expect(config).to.have.property('gitUrl')
   expect(config).to.have.property('framework')
   expect(config).to.have.property('this-should-break')
-  console.log('here!')
+  console.log('here E!')
 }
 
 module.exports = {
