3221bb37435117516fcc02dab1be853afede90bb
Hugo Villain <hugo.villain@prismic.io>
2020-04-10 14:39:59 +0200

    update to new Mongo; fetch by packageName

--- packages/sm-api/api/libraries.js
+++ packages/sm-api/api/libraries.js
@@ -1,24 +1,48 @@
+const Mongo = require('../common/mongo');
 const handleStripKeys = require("../common").handleStripKeys;
 const { defaultStripKeys } = require('../common/consts');
 const cors = require("../common/cors");
-const Mongo = require('../common/mongo');
+
+async function fetchLibraries({ framework, list }) {
+  const search = {
+    ...(framework ? {
+      framework
+    } : null),
+    ...(list ? {
+      packageName: {
+        '$in': list.trim().split(',')
+      }
+    } : null)
+  }
+  const cursor = await Mongo.collections.libraries(coll => (
+    coll.find(search)
+  ));
+  return await cursor.toArray()
+}
 
 module.exports = cors(async (req, res) => {
   const {
-    query: { framework, strip, preserveDefaults }
+    query: {
+      framework,
+      strip,
+      list,
+      preserveDefaults
+    }
   } = req;
 
   const keysToStrip = handleStripKeys(strip, defaultStripKeys.library, preserveDefaults);
-  const libraries = await Mongo.collections.libraries(async coll => (
-    await coll.find({
-      ...(framework ? { framework } : {})
-    }).toArray()
-  ));
+
+  const libraries = await fetchLibraries({
+    framework,
+    list
+  })
 
   libraries.forEach((library) => {
     keysToStrip.forEach((key) => {
       delete library[key]
     })
   })
+
   res.send(libraries)
+
 });
