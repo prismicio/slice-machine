45385102d1036f7b7f4619a46f5f82cab970f845
Arnaud Lewis <arnaud.lewis@gmail.com>
2020-06-17 14:48:51 +0200

    manifest for next

---
+++ packages/sm-api/bootstrap/next/files/next.config.mustache
@@ -0,0 +1,3 @@
+const withTM = require('next-transpile-modules')(['next-slicezone', 'essential-slices']);
+
+module.exports = withTM();
--- packages/sm-api/bootstrap/next/index.js
+++ packages/sm-api/bootstrap/next/index.js
@@ -1,6 +1,13 @@
+const Mustache = require('mustache');
 const merge = require("../../common/manifest").merge
 
 const defaultLibrary = 'react-essential-slices';
+
+const _app = require('./files/_app.mustache'),
+const _document = require('./files/_document.mustache'),
+const nextConfig = require('./files/next.config.mustache'),
+const prismic = require('./files/prismic.mustache'),
+const smResolver = require('./files/sm=resolver.mustache')
 
 module.exports = {
   defaultLibrary,
@@ -9,26 +16,42 @@ module.exports = {
     frameworkName: "Next",
     configPath: "next.config.js",
     firstCommand: "npm run dev",
-    projectTests: [{
-        arg: "-f",
-        path: "next.config.js",
-        reason: "No `next.config.js` file found"
-      },
+    projectTests: [
       {
         arg: "-d",
         path: "pages",
         reason: "No `pages` folder found"
       }
     ],
-    prompts: [{
-      type: 'folder',
-      path: 'custom_types',
-      strategies: ['bootstrap', 'init']
-    }, ],
-    devDependencies: [],
-    libraries: [],
     bootstraper: ["npx", ["create-react-app"]],
-    transpile: ["next-slicezone"],
-    dependencies: ["prismic-javascript", "prismic-reactjs", "next-slicezone"]
+    dependencies: ["prismic-javascript", "prismic-reactjs", "next-slicezone", "next-transpile-modules"],
+    files: [{
+      name: '_app.js',
+      path: './pages',
+      content: Mustache.render(_app)
+    }, {
+      name: '_document.js',
+      path: './pages',
+      content: Mustache.render(_document)
+    }, {
+      name: 'next.config.js',
+      path: './',
+      content: Mustache.render(nextConfig)
+    }, {
+      name: 'prismic.js',
+      path: './',
+      content: Mustache.render(prismic, routes)
+    }, {
+      name: 'sm-resolver.js',
+      path: './',
+      content: Mustache.render(smResolver)
+    }],
   }, library))
 }
+
+JSON.stringify(routes)
+[{
+  type: 'page',
+  path: '/:uid',
+  href: '/[uid]'
+}]
