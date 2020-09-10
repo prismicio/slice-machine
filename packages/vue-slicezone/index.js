cc3dd02cdc8eccae7d7319bd60bd90e4c2a3bc64
Sam Littlefair <sam.littlefair@prismic.io>
2020-09-10 18:11:24 +0200

    Edit control statement in vue-slicezone to render render PageFetch with uid OR queryType===single

--- packages/vue-slicezone/index.js
+++ packages/vue-slicezone/index.js
@@ -1,29 +1,30 @@
-import PageFetch from './PageFetch'
-import SliceZone from './SliceZone'
+import PageFetch from "./PageFetch";
+import SliceZone from "./SliceZone";
 
 export default {
-  name: 'SliceZone',
+  name: "SliceZone",
   render(h) {
     const {
       slices,
       type,
       uid,
-      resolver: maybeResolver
-    } = this.$attrs
+      queryType,
+      resolver: maybeResolver,
+    } = this.$attrs;
 
-    const resolver = maybeResolver || this.$sliceMachine.resolver
-    if (!slices && type && uid) {
+    const resolver = maybeResolver || this.$sliceMachine.resolver;
+    if (!slices && type && (uid || queryType === "single")) {
       return h(PageFetch, {
         props: {
           ...this.$attrs,
           resolver,
           scopedSlots: this.$scopedSlots,
-        }
-      })
+        },
+      });
     }
     return h(SliceZone, {
       scopedSlots: this.$scopedSlots,
-      props: { ...this.$attrs, resolver }}
-    )
-  }
-}
+      props: { ...this.$attrs, resolver },
+    });
+  },
+};
