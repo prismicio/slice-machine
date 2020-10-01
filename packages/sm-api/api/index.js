0736f8183aba68a80a1116d0ef6dcf374630cb9a
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-10-01 15:20:45 +0200

    fix: remove jobs and add cors to bootstrap

--- packages/sm-api/api/index.js
+++ packages/sm-api/api/index.js
@@ -5,12 +5,10 @@ const library = require('./library');
 const publish = require('./publish');
 const slices = require('./slices');
 const version = require('./version');
-const job = require('./job')
 
 module.exports = {
   bootstrap,
   frameworks,
-  job,
   libraries,
   library,
   publish,
--- packages/sm-api/handler.js
+++ packages/sm-api/handler.js
@@ -3,9 +3,8 @@ var reqRes = require('serverless-req-res');
 const api = require('./api');
 
 // https://www.npmjs.com/package/serverless-req-res
-module.exports.bootstrap = reqRes(api.bootstrap).run;
+module.exports.bootstrap = reqRes(api.bootstrap).cors(true).run;
 module.exports.frameworks = reqRes(api.frameworks).cors(true).run;
-module.exports.job = reqRes(api.job).run;
 module.exports.libraries = reqRes(api.libraries).cors(true).run;
 module.exports.library = reqRes(api.library).cors(true).run;
 module.exports.publish = reqRes(api.publish).cors(true).run;
