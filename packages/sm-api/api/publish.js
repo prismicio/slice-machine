86ddca51fca32a351f286c742bea972b07eb677f
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-10-02 16:34:08 +0200

    chore(sm-api): clean up some code

--- packages/sm-api/api/publish.js
+++ packages/sm-api/api/publish.js
@@ -9,26 +9,23 @@ const githubWhiteList = {
 
 module.exports = async (event) => {
   const body = event.body || {}
-  const headers =  { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'POST','Content-Type': 'application/json'};
+  const headers = { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'POST','Content-Type': 'application/json'};
 
   if (!body.ref || !body.head_commit || !body.repository) {
-    // res.error ?
     const message = `missing parameters:${!body.ref && " ref"}${!body.head_commit && " head_commit"}${!body.repository && " repository"}`
     return { statusCode: 400, headers, body: JSON.stringify({ error: true, message })}
   }
 
   const repoName = body.repository.full_name
   if (!githubWhiteList[repoName]) {
-    // res.error ?
     const message = `Unauthorised repository ${repoName}`;
     return { statusCode: 403, headers, body: JSON.stringify({ error: true, message })}
   }
 
   const branch = body.ref.split("/").pop()
-  if (branch !== "master") {
-    // res.end() ?
-    const message = `ref should be master branch`;
 
+  if (branch !== "master") {
+    const message = `ref should be master branch`;
     return { statusCode: 200, headers, body: JSON.stringify({ error: true, message }) }
   }
 
@@ -41,14 +38,19 @@ module.exports = async (event) => {
 
     try {
       const response = await fetch(smFileUrl);
-      // const sm = JSON.parse(await response.text());
       const sm = await response.json();
       
-      await Mongo.collections.libraries(coll => (
-        coll.updateOne({ packageName: sm.packageName }, { $set: sm }, {
-          upsert: true
-        })
-      ));
+      /**
+       * Memory leak deteced in tests,
+       * comment out mongo to remove the warning.
+      */
+      await Mongo.collections.libraries(coll => coll.updateOne({
+        packageName: sm.packageName
+      }, {
+        $set: sm
+      }, {
+        upsert: true
+      }));
 
       return { statusCode: 200, headers, body:JSON.stringify(sm) };
     } catch(e) {
