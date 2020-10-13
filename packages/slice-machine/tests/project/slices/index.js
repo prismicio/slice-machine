606841e085d0e1b410e0680f1c1ac589771b4ccc
hypervillain <hugo.villain@prismic.io>
2020-10-13 10:55:38 +0200

    Remove mock.json files from slices

--- packages/slice-machine/tests/project/slices/index.js
+++ packages/slice-machine/tests/project/slices/index.js
@@ -1,1 +1,7 @@
 export { default as MySlice } from './MySlice'
+export { default as AssA } from './AssA'
+export { default as PascalA } from './PascalA'
+export { default as PascalB } from './PascalB'
+export { default as AsssA } from './AsssA'
+export { default as MyCoolSlice } from './MyCoolSlice'
+export { default as MyAwessomecom } from './MyAwessomecom'
