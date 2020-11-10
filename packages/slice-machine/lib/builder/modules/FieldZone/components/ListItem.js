2a420726021385db91852dfa44981f948fbc243f
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-11-10 17:17:09 +0100

    Revert "wip(add hints): add framework to env and some hints for vue components"
    This reverts commit 5a52dbdaf5ae7f7e866767685987f0bd3adb4ec2.

--- packages/slice-machine/lib/builder/modules/FieldZone/components/ListItem.js
+++ packages/slice-machine/lib/builder/modules/FieldZone/components/ListItem.js
@@ -1,7 +1,5 @@
 import { Draggable } from 'react-beautiful-dnd'
 import { MenuButton, Menu, MenuItem, MenuList } from '@reach/menu-button'
-import { useContext } from 'react';
-import { ConfigContext } from '../../../../../src/config-context';
 
 import {
   Flex,
@@ -19,51 +17,16 @@ import * as widgets from 'lib/widgets'
 import { MdSettings } from "react-icons/md";
 import { BsThreeDotsVertical } from 'react-icons/bs'
 
-
-const toPrismicVueComponentName = (type) => {
-  switch(type) {
-    case "StructuredText": return 'prismic-rich-text';
-    case "Link": return "prismic-link";
-    case "Image": return "prismic-image";
-    case "Embed": return "prismic-embed";
-    // other types / missing components
-    case "Select":
-    case "GeoPoint":
-    case "Text": 
-    case "TimeStamp": 
-    case "Number": 
-    case "Boolean": 
-    case "Color":
-    case "Group":
-    case "UID":
-    case "Date":    
-    // any-more?
-    default: return ""; // what should be a sane default?
-  } 
-}
-
-const toVue = (item, modelFieldName, key) => {
-  const component = toPrismicVueComponentName(item.value.type);
-
-  return component ? `<${component} :field=slice.${modelFieldName}.${key} />` : `slice.${modelFieldName}.${key}`;
-}
-
 const ListItem = ({
   item,
   index,
   deleteItem,
   enterEditMode,
-  modelFieldName,
+  modelFieldName
 }) => {
   const { key } = item
   const { theme } = useThemeUI()
   const { config: { label }, type } = item.value
-  const { env: { framework } } = useContext(ConfigContext);
-
-  const isVue = framework === 'nuxt' || framework === 'vue' ;
-
-  const sliceProperty = isVue ? toVue(item, modelFieldName, key) : `slice.${modelFieldName}.${key}`;
-
   if (!widgets[type].Meta) {
     return (
       <Li><Text>Field type "{type}" not supported</Text></Li>
@@ -77,12 +40,11 @@ const ListItem = ({
         <Li
           ref={provided.innerRef}
           {...provided.draggableProps}
-        > 
-          {/* here */}
-          <ItemHeader 
+        >
+          <ItemHeader
             theme={theme}
             text={label || key}
-            sliceProperty={sliceProperty}
+            sliceProperty={`slice.${modelFieldName}.${key}`}
             iconButtonProps={provided.dragHandleProps}
             WidgetIcon={WidgetIcon}
           />
