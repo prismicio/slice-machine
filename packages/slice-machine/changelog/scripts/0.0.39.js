aef2e2beb9a97976ccc4b2356821bb7b91cedeb6
lihbr <lihbr@users.noreply.github.com>
2020-11-26 12:56:20 +0100

    fix(slice-machine): globby needs posix paths

--- packages/slice-machine/changelog/scripts/0.0.39.js
+++ packages/slice-machine/changelog/scripts/0.0.39.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const glob = require('glob')
+const slash = require('slash')
 const { shouldIRun } = require('../common')
 const { getInfoFromPath } = require('sm-commons/methods/lib')
 const { createPathToScreenshot } = require('../../lib/queries/screenshot')
@@ -8,9 +9,9 @@ const { createPathToScreenshot } = require('../../lib/queries/screenshot')
 main()
 
 async function main() {
-  console.log('\nSliceMachine nows supports both default and custom previews (screenshots)!')
-  console.log('Default screenshots are now stored in a special .slicemachine folder.')
-  console.log('To use custom previews, simply keep storing them in your component folder.')
+  console.info('\nSliceMachine nows supports both default and custom previews (screenshots)!')
+  console.info('Default screenshots are now stored in a special .slicemachine folder.')
+  console.info('To use custom previews, simply keep storing them in your component folder.')
   const { yes } = await shouldIRun('Would you like me to move current previews to .slicemachine?')
   if (yes) {
     const cwd = require.main.paths[0].split('node_modules')[0]
@@ -25,9 +26,9 @@ async function main() {
             pathToSlices,
           } = getInfoFromPath(lib, cwd)
           if (isLocal && pathExists) {
-            const matches = glob.sync(`${pathToSlices}/**/preview.png`)
+            const matches = glob.sync(`${slash(pathToSlices)}/**/preview.png`)
             matches.forEach((match) => {
-              const split = match.split(path.sep)
+              const split = match.split(path.posix.sep)
               const sliceName = split[split.length - 2]
               if (sliceName) {
                 const pathToNewFile = createPathToScreenshot({ cwd, from: split[split.length - 3], sliceName })
@@ -38,7 +39,7 @@ async function main() {
           }
         })
         fs.writeFileSync(pathToSmFile, JSON.stringify({ ...json, _latest: '0.0.39' }, null, 2))
-        return console.log('Done!')
+        return console.info('Done!')
       } catch(e) {
         console.log(e)
       }
