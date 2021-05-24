46ee01fc8ca7d71be3b5d3c9460ec5f4806b7bbd
duaner <renaudbressand@gmail.com>
2021-05-24 16:41:53 +0200

    Rename Templates into custom type

--- packages/slice-machine/lib/builders/CustomTypeBuilder/Layout/Header.tsx
+++ packages/slice-machine/lib/builders/CustomTypeBuilder/Layout/Header.tsx
@@ -79,7 +79,7 @@ const Header = ({
           <Link href="/" passHref>
             <ThemeLinK variant="invisible">
               <Flex sx={{ alignItems: "center" }}>
-                <FiLayout /> <Text ml={2}>Templates</Text>
+                <FiLayout /> <Text ml={2}>Custom Types</Text>
               </Flex>
             </ThemeLinK>
           </Link>
