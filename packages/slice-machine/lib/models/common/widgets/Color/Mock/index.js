fe48c8349cc5a4478ffa22d5e4db698f15b287e4
Miniplop <kevin.jean@prismic.io>
2021-09-20 14:20:04 +0200

    fix hexa generator to generate always 6 characters

--- packages/slice-machine/lib/models/common/widgets/Color/Mock/index.js
+++ packages/slice-machine/lib/models/common/widgets/Color/Mock/index.js
@@ -2,8 +2,17 @@ import { createDefaultHandleMockContentFunction } from "../../../../../utils";
 
 export const initialValues = null;
 
+function getRandomColor() {
+  const letters = "0123456789abcdef";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 export const handleMockConfig = () => {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return getRandomColor();
 };
 
 export const handleMockContent = createDefaultHandleMockContentFunction(
