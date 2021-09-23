22bdbfa9d2071e794f45efcd064b6e49616e2d80
Miniplop <kevin.jean@prismic.io>
2021-09-23 17:05:19 +0200

    remove sm-api

--- packages/sm-api/api/version.js
+++
@@ -1,7 +0,0 @@
-const current = "0.0.15-alpha.4";
-const deprecatedUnder = "0.0.14";
-const cors = require("../common/cors");
-
-module.exports = cors((req, res) => {
-  res.send({ current, deprecatedUnder });
-});
--- packages/sm-api/common/cors.js
+++
@@ -1,8 +0,0 @@
-function cors(asyncFn) {
-  return (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    return asyncFn(req, res);
-  };
-}
-
-module.exports = cors;
