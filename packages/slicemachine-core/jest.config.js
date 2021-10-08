c726592fe2fa59b5dec02c03cbc039fd9103714d
MarcMcIntosh <marcmcintosh1987@gmail.com>
2021-10-08 16:49:03 +0200

    fix(core): add node_modules back into jest transformIgnore

--- packages/slicemachine-core/jest.config.js
+++ packages/slicemachine-core/jest.config.js
@@ -179,7 +179,7 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
-  //   "/node_modules/", // error from node fetch :/
+    "/node_modules/", // error from node fetch :/
     "\\.pnp\\.[^\\/]+$"
   ],
 
