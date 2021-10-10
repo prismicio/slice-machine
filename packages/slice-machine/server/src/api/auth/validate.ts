b9dcb890da358e03858f1c0c84fef592c3f7aefc
Miniplop <kevin.jean@prismic.io>
2021-10-10 19:51:52 +0200

    migrate server/index file + parse-oembed file to typescript

--- packages/slice-machine/server/src/api/auth/validate.ts
+++ packages/slice-machine/server/src/api/auth/validate.ts
@@ -3,6 +3,7 @@ import { validate } from "@lib/env/client";
 type ValidateResponse = {
   connected: boolean;
   reason: string;
+  err?: any;
 };
 export default async function handler(): Promise<ValidateResponse> {
   return validate();
