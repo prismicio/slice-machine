ef93ba529594b8133de5f5c2372844492c96b5fc
MarcMcIntosh <marcmcintosh1987@gmail.com>
2021-10-11 15:54:58 +0200

    chore(sm-init): fix linter warnings

--- packages/sm-init/src/steps/maybe-existing-repo.ts
+++ packages/sm-init/src/steps/maybe-existing-repo.ts
@@ -1,15 +1,15 @@
 import * as inquirer from 'inquirer'
-const chalk = require('chalk')
+import chalk from 'chalk'
 import { Communication, Utils } from 'slicemachine-core'
 
 export const CREATE_REPO = "$_CREATE_REPO" // not a valid domain name
 const DEFAULT_BASE = Utils.CONSTS.DEFAULT_BASE
 
-export function prettyRepoName(address: URL, value?: string) {
-  const reponame = value ? chalk.cyan(value) : chalk.dim.cyan('repo-name')
+export function prettyRepoName(address: URL, value?: string): string {
+  const repoName = value ? chalk.cyan(value) : chalk.dim.cyan('repo-name')
   const msg = [
     chalk.dim(`${address.protocol}//`),
-    reponame,
+    repoName,
     chalk.dim(`.${address.hostname}`),
   ]
   return msg.join('')
@@ -17,19 +17,19 @@ export function prettyRepoName(address: URL, value?: string) {
 
 export async function promptForCreateRepo(base: string): Promise<string> {
   const address = new URL(base)
-  return inquirer.prompt([
+  return inquirer.prompt<Record<string, string>>([
     {
       name: 'repoName',
       message: "Name your Prismic repository",
       type: "input",
       required: true,
-      transformer: (value) => prettyRepoName(address, value),
+      transformer: (value) => prettyRepoName(address, String(value)),
       async validate(name) {
         const result = await Communication.validateRepositoryName(name, base, false)
         return result === name || result
       },
     }
-  ]).then(res => res.repoName)
+  ]).then(res=> res.repoName)
 }
 
 export async function maybeExistingRepo(cookie: string, base = DEFAULT_BASE): Promise<string> {
@@ -38,7 +38,7 @@ export async function maybeExistingRepo(cookie: string, base = DEFAULT_BASE): Pr
 
   if(repos.length === 0) return promptForCreateRepo(base)
 
-  const res = await inquirer.prompt([
+  const res = await inquirer.prompt<Record<string, string>>([
     {
       type: "list",
       name: "repoName",
