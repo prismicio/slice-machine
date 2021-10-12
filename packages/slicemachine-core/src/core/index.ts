48bf3667290461cbb143655f807ee9adecfa5072
Amaury Cahuet <amaury.cahuet@gmail.com>
2021-10-12 13:09:39 +0200

    remove commentary on jest configs

--- packages/slicemachine-core/src/core/index.ts
+++ packages/slicemachine-core/src/core/index.ts
@@ -8,6 +8,21 @@ export interface Core {
   base: string;
   manifest: Manifest;
 
+  /*CustomTypes?: {
+    get: (apiEndpoint: string, token: string, customTypeId: string) => Promise<any>,
+    getAll: (apiEndpoint: string, token: string) => Promise<any>,
+    insert: (apiEndpoint: string, token: string, data: any) => Promise<void>,
+    update: (apiEndpoint: string, token: string, data: any) => Promise<void>,
+    remove: (apiEndpoint: string, token: string, customTypeId: string) => Promise<void>
+  },
+  Slices?: {
+    get: (apiEndpoint: string, token: string, sliceId: string) => Promise<any>,
+    getAll: (apiEndpoint: string, token: string) => Promise<any>,
+    insert: (apiEndpoint: string, token: string, data: any) => Promise<void>,
+    update: (apiEndpoint: string, token: string, data: any) => Promise<void>,
+    remove: (apiEndpoint: string, token: string, sliceId: string) => Promise<void>
+  },*/
+
   Repository: {
     list: (token: string) => Promise<string[]>;
     create?: (apiEndpoint: string, token: string) => Promise<void>;
