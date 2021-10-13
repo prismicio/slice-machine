05f4e6788a281536a7c0c69e7ee3e9d8f692073f
MarcMcIntosh <marcmcintosh1987@gmail.com>
2021-10-13 10:34:45 +0200

    chore(sm-init): fix test

--- packages/sm-init/__tests__/create-repo.test.ts
+++ packages/sm-init/__tests__/create-repo.test.ts
@@ -1,65 +1,61 @@
 import { describe, expect, test } from "@jest/globals";
 
-import nock from 'nock'
-import {createRepository} from '../src/steps/create-repo'
-import {
-  stderr
-} from 'stdout-stderr'
+import nock from "nock";
+import { createRepository } from "../src/steps/create-repo";
+import { stderr } from "stdout-stderr";
 
+describe("createRepository", () => {
+  test("success", async () => {
+    const domain = "foo-bar";
+    const base = "https://prismic.io";
+    const framework = "foo.js";
+    const cookie = "prismic-auth=biscuits;";
 
-describe('createRepository', () => {
+    nock(base)
+      .post("/authentication/newrepository?app=slicemachine")
+      .reply(200, { domain });
 
-  test('success', async () => {
-    const domain = "foo-bar"
-    const base = "https://prismic.io"
-    const framework = "foo.js"
-    const cookie = "prismic-auth=biscuits;"
+    stderr.start();
+    await createRepository(domain, cookie, framework, base);
+    stderr.stop();
+    expect(stderr.output).toContain(domain);
+    expect(stderr.output).toContain("Creating Prismic Repository");
+    expect(stderr.output).toContain("We created your new repository");
+  });
 
-    nock(base).post('/authentication/newrepository?app=slicemachine').reply(200, {domain})
+  test("success without domain in the response", async () => {
+    const domain = "foo-bar";
+    const base = "https://prismic.io";
+    const framework = "foo.js";
+    const cookie = "prismic-auth=biscuits;";
 
-    stderr.start()
-    const result = await createRepository(domain, cookie, framework, base)
-    stderr.stop()
-    expect(result).toContain(domain)
-    expect(stderr.output).toContain(domain)
-    expect(stderr.output).toContain("Creating Prismic Repository")
-    expect(stderr.output).toContain("We created your new repository")
-   
-  })
+    nock(base)
+      .post("/authentication/newrepository?app=slicemachine")
+      .reply(200, {});
 
-  test('success without domain in the response', async () => {
-    const domain = "foo-bar"
-    const base = "https://prismic.io"
-    const framework = "foo.js"
-    const cookie = "prismic-auth=biscuits;"
+    stderr.start();
+    await createRepository(domain, cookie, framework, base);
+    stderr.stop();
+    expect(stderr.output).toContain(domain);
+    expect(stderr.output).toContain("Creating Prismic Repository");
+    expect(stderr.output).toContain("We created your new repository");
+  });
 
-    nock(base).post('/authentication/newrepository?app=slicemachine').reply(200, {})
+  test("failure", async () => {
+    const domain = "foo-bar";
+    const base = "https://prismic.io";
+    const framework = "foo.js";
+    const cookie = "prismic-auth=biscuits;";
 
-    stderr.start()
-    const result = await createRepository(domain, cookie, framework, base)
-    stderr.stop()
-    expect(result).toContain(domain)
-    expect(stderr.output).toContain(domain)
-    expect(stderr.output).toContain("Creating Prismic Repository")
-    expect(stderr.output).toContain("We created your new repository")
-   
-  })
-
-  test('failure', async () => {
-
-    const domain = "foo-bar"
-    const base = "https://prismic.io"
-    const framework = "foo.js"
-    const cookie = "prismic-auth=biscuits;"
-    
-
-    nock(base).post('/authentication/newrepository?app=slicemachine').reply(500, {})
-    stderr.start()
-    expect(() => createRepository(domain, cookie, framework, base)).rejects.toThrow()
-    .then(() => {
-      stderr.stop()
-      expect(stderr.output).toContain("Error creating repository")
-    })
-
-  })
-})
+    nock(base)
+      .post("/authentication/newrepository?app=slicemachine")
+      .reply(500, {});
+    stderr.start();
+    expect(() => createRepository(domain, cookie, framework, base))
+      .rejects.toThrow()
+      .then(() => {
+        stderr.stop();
+        expect(stderr.output).toContain("Error creating repository");
+      });
+  });
+});
