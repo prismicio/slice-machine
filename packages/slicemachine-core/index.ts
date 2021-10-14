220d2edbfe7b9695ba163fdb945d90f6b8f869bb
MarcMcIntosh <marcmcintosh1987@gmail.com>
2021-10-14 17:51:30 +0200

    chore(prettier): run prettier

--- packages/slicemachine-core/index.ts
+++ packages/slicemachine-core/index.ts
@@ -1,3 +1,3 @@
-export * as FileSystem from './src/filesystem'
-export * as Utils from './src/utils'
-export * from './src/core'
+export * as FileSystem from "./src/filesystem";
+export * as Utils from "./src/utils";
+export * from "./src/core";
