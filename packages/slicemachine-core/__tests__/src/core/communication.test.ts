fdac1a6281a81616fc93a930d0acd78504881092
MarcMcIntosh <marcmcintosh1987@gmail.com>
2021-10-18 16:43:51 +0200

    chore(ls-repos sm-core): lint tests

--- packages/slicemachine-core/__tests__/src/core/communication.test.ts
+++ packages/slicemachine-core/__tests__/src/core/communication.test.ts
@@ -82,50 +82,50 @@ describe("communication", () => {
   describe("validateRepositoryName", () => {
     const fakeBase = "https://prismic.io";
 
-    test("should fail if subdomain is not defined", async () => {
+    test("should fail if subdomain is not defined", () => {
       const fn = () => communication.validateRepositoryName();
-      expect(fn).rejects.toThrow("repository name is required");
+      return expect(fn).rejects.toThrow("repository name is required");
     });
 
-    test("should fail if name length is less than 4", async () => {
+    test("should fail if name length is less than 4", () => {
       const fn = () => communication.validateRepositoryName("abc");
-      expect(fn).rejects.toThrow(
+      return expect(fn).rejects.toThrow(
         "Must have four or more alphanumeric characters and/or hyphens."
       );
     });
 
-    test("should fail if the name contains non alphanumeric characters", async () => {
+    test("should fail if the name contains non alphanumeric characters", () => {
       const fn = () => communication.validateRepositoryName("a.bc");
-      expect(fn).rejects.toThrow(
+      return expect(fn).rejects.toThrow(
         "Must contain only lowercase letters, numbers and hyphens"
       );
     });
 
-    test("should fail if the name starts with a hyphen", async () => {
+    test("should fail if the name starts with a hyphen", () => {
       const fn = () => communication.validateRepositoryName("-abc");
-      expect(fn).rejects.toThrow("start with a letter");
+      return expect(fn).rejects.toThrow("start with a letter");
     });
 
-    test("should fail if the name ends with a hyphen", async () => {
+    test("should fail if the name ends with a hyphen", () => {
       const fn = () => communication.validateRepositoryName("abc-");
-      expect(fn).rejects.toThrow("Must end in a letter or a number");
+      return expect(fn).rejects.toThrow("Must end in a letter or a number");
     });
 
-    test("Max length 30 characters", async () => {
+    test("Max length 30 characters", () => {
       const repoName = Array.from({ length: 31 }, () => "a").join("");
       const fn = () => communication.validateRepositoryName(repoName);
-      expect(fn).rejects.toThrow("30 characters or less");
+      return expect(fn).rejects.toThrow("30 characters or less");
     });
 
     test("multiple errors", () => {
       const repoName = "-abc.d";
       const fn = () => communication.validateRepositoryName(repoName);
-      expect(fn).rejects.toThrow(
+      return expect(fn).rejects.toThrow(
         "(1: Must start with a letter. (2: Must contain only lowercase letters, numbers and hyphens."
       );
     });
 
-    test("should fail if repo name is not available", async () => {
+    test("should fail if repo name is not available", () => {
       const repoName = "test";
       nock(fakeBase)
         .get(`/app/dashboard/repositories/${repoName}/exists`)
@@ -133,7 +133,7 @@ describe("communication", () => {
 
       const fn = () => communication.validateRepositoryName(repoName);
 
-      expect(fn).rejects.toThrow("already in use");
+      return expect(fn).rejects.toThrow("already in use");
     });
 
     test("existing repo", () => {
@@ -141,7 +141,7 @@ describe("communication", () => {
       nock(fakeBase)
         .get(`/app/dashboard/repositories/${repoName}/exists`)
         .reply(200, () => false);
-      expect(
+      return expect(
         communication.validateRepositoryName(repoName, fakeBase, true)
       ).resolves.toEqual(repoName);
     });
@@ -151,20 +151,20 @@ describe("communication", () => {
       nock(fakeBase)
         .get(`/app/dashboard/repositories/${repoName}/exists`)
         .reply(200, () => true);
-      expect(
+      return expect(
         communication.validateRepositoryName(repoName, fakeBase, true)
       ).rejects.toThrow("does not exist");
     });
 
-    test("should pass if repo name is valid and available", async () => {
+    test("should pass if repo name is valid and available", () => {
       const repoName = "test";
       nock(fakeBase)
         .get(`/app/dashboard/repositories/${repoName}/exists`)
         .reply(200, () => true);
 
-      expect(communication.validateRepositoryName(repoName)).resolves.toEqual(
-        repoName
-      );
+      return expect(
+        communication.validateRepositoryName(repoName)
+      ).resolves.toEqual(repoName);
     });
 
     test("different base", () => {
@@ -173,7 +173,7 @@ describe("communication", () => {
         .get(`/app/dashboard/repositories/${repoName}/exists`)
         .reply(200, () => true);
 
-      expect(
+      return expect(
         communication.validateRepositoryName(repoName, "https://example.com")
       ).resolves.toEqual(repoName);
     });
