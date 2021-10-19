6178b45a1dccabfdffe284cd6d2ef0e109514f69
MarcMcIntosh <marcmcintosh1987@gmail.com>
2021-10-19 11:19:50 +0200

    fix(roles): fix the test for new roles

--- packages/slicemachine-core/__tests__/src/utils/roles.test.ts
+++ packages/slicemachine-core/__tests__/src/utils/roles.test.ts
@@ -5,7 +5,10 @@ describe("canUpdateCutsomTypes", () => {
   test("should return true only if role is owner or admin", () => {
     Object.values(roles.Roles).forEach((role) => {
       const result = roles.canUpdateCustomTypes(role);
-      const wanted = role === roles.Roles.ADMIN || role === roles.Roles.OWNER;
+      const wanted =
+        role === roles.Roles.ADMIN ||
+        role === roles.Roles.OWNER ||
+        role === roles.Roles.SUPERUSER;
       return expect(result).toBe(wanted);
     });
   });
