4e5b49969d2d0ef89039f21bd4752824d428993c
MarcMcIntosh <marcmcintosh1987@gmail.com>
2021-10-25 17:05:56 +0200

    test(on-boarding): add cypress test to check onboarding loads

--- packages/slice-machine/tests/project/cypress/integration/0-is-running/check.spec.js
+++ packages/slice-machine/tests/project/cypress/integration/0-is-running/check.spec.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
 
 describe("check slicemachine is running", () => {
+
+  beforeEach(() => {
+    cy.removeLocalStorage()
+  })
+
   it('visit http://localhost:9999', () => {
     cy.visit('http://localhost:9999')
   })
