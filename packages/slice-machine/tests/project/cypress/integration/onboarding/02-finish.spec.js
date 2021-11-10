86948dd538ce5b4c60f2f15bfc68ff0dd1626735
MarcMcIntosh <marcmcintosh1987@gmail.com>
2021-11-10 18:29:31 +0100

    chore(onboarding): tidy up the layout of the cypress tests

--- packages/slice-machine/tests/project/cypress/integration/onboarding/02-finish.spec.js
+++ packages/slice-machine/tests/project/cypress/integration/onboarding/02-finish.spec.js
@@ -1,37 +1,5 @@
-/// <reference types="cypress" />
 
-describe('onboarding', () => {
-
-  before(() => {
-    cy.clearLocalStorageSnapshot();
-  });
-
-  beforeEach(() => {
-    cy.restoreLocalStorage();
-  });
-
-  afterEach(() => {
-    cy.saveLocalStorage();
-  });
-
-  it('should redirect to /onboarding when is-onboared is not in local storage', () => {
-    cy.intercept('POST', '/tracking/onboarding', ({body}) => {
-      expect(body.id).equal("slicemachine_onboarding_start")
-    })
-    
-    cy.visit('/')
-
-    cy.location('pathname', {timeout: 1000}).should('eq', '/onboarding')
-
-    cy.wait(1500)
-    cy.getLocalStorage("is-onboarded").should('eq', 'true')
-  })
-
-  it('when is-onboarded is in local storage it should not redirect', () => {
-    cy.setLocalStorage("is-onboarded", true)
-    cy.visit('/')
-    cy.location('pathname', {timeout: 1000}).should('eq', '/')
-  })
+describe("onboarding finish", () => {
 
   it('begin button and continue button eventually redirect to /', () => {
 
@@ -42,30 +10,31 @@ describe('onboarding', () => {
       "slicemachine_onboarding_continue_2",
       "slicemachine_onboarding_continue_3",
     ]
+
     cy.intercept('POST', '/tracking/onboarding', ({body}) => {
       expect(body.id).equal(ids.shift())
     })
-
+  
     const closeReviewSelector = '[data-cy=close-review]'
-
+  
     cy.visit('/onboarding')
-
+  
     // close the popup 
     cy.get('body').then(body => {
       body.find(closeReviewSelector).length && cy.get(closeReviewSelector).click()
     })
-
+  
     
     cy.get('[data-cy=get-started]').click()
-
+  
     const clickContinue = () => cy.get('[data-cy=continue]').click()
     
     for (let i = 0; i <= 2; i++) {
       clickContinue()
     }
-
-
+  
     cy.location('pathname', {timeout: 1000}).should('eq', '/')
-
+  
   })
+  
 })
