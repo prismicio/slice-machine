009891c5ddad9b8546c45045086e98fe4187bdd8
Miniplop <kevin.jean@prismic.io>
2021-11-12 17:50:36 +0100

    update onboarding test

--- packages/slice-machine/tests/project/cypress/integration/onboarding/00-redirects.spec.js
+++ packages/slice-machine/tests/project/cypress/integration/onboarding/00-redirects.spec.js
@@ -14,7 +14,7 @@ describe('onboarding redirects and local storage', () => {
     cy.saveLocalStorage();
   });
 
-  it('should redirect to /onboarding when is-onboared is not in local storage', () => {
+  it('should redirect to /onboarding when is-onboarded is not in local storage', () => {
     cy.intercept('POST', '/tracking/onboarding', ({body}) => {
       expect(body.id).equal("slicemachine_onboarding_start")
     })
@@ -22,13 +22,10 @@ describe('onboarding redirects and local storage', () => {
     cy.visit('/')
 
     cy.location('pathname', {timeout: 1000}).should('eq', '/onboarding')
-
-    cy.wait(1500)
-    cy.getLocalStorage("is-onboarded").should('eq', 'true')
   })
 
   it('when is-onboarded is in local storage it should not redirect', () => {
-    cy.setLocalStorage("is-onboarded", true)
+    cy.setLocalStorage("persist:root", JSON.stringify({userContext: JSON.stringify({isOnboarded: true})}))
     cy.visit('/')
     cy.location('pathname', {timeout: 1000}).should('eq', '/')
   })
