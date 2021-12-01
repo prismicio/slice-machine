bee2cab2dab8b2e795f3f777821bc9daaf728ac8
Miniplop <kevin.jean@prismic.io>
2021-12-01 20:18:02 +0100

    Separate NuxtStep for SliceBuilder compoents

--- packages/slice-machine/lib/builders/SliceBuilder/SetupDrawer/index.tsx
+++ packages/slice-machine/lib/builders/SliceBuilder/SetupDrawer/index.tsx
@@ -4,7 +4,7 @@ import Drawer from "rc-drawer";
 import { Close, Flex, Link, Text } from "theme-ui";
 import { FaRegQuestionCircle } from "react-icons/fa";
 
-import StepSection from "./components/StepSection";
+import NuxtSetupSteps from "@builders/SliceBuilder/SetupDrawer/NuxtSetupSteps";
 
 type SetupDrawerProps = {
   isOpen: boolean;
@@ -61,103 +61,53 @@ const SetupDrawer: React.FunctionComponent<SetupDrawerProps> = ({
           }}
         >
           <Flex as={"section"} sx={{ flexDirection: "column" }}>
-            <StepSection
-              stepNumber={1}
-              title={"Install Slice Canvas"}
-              isOpen={activeStep === 1}
-              onOpenStep={onOpenStep(1)}
-            >
-              <Flex>
-                <Text sx={{ color: (t) => t.colors?.textClear }}>
-                  Slice Canvas is used to develop your components with mock
-                  data, run the following command to install it with npm
-                </Text>
-              </Flex>
-            </StepSection>
-            <StepSection
-              stepNumber={2}
-              title={"Create a page for Slice Canvas"}
-              isOpen={activeStep === 2}
-              onOpenStep={onOpenStep(2)}
-            >
-              <Flex>
-                <Text sx={{ color: (t) => t.colors?.textClear }}>
-                  In your “pages” directory, create a file called _canvas.jsx
-                  and add the following code. This page is the route you hit to
-                  preview and develop your components.
-                </Text>
-              </Flex>
-            </StepSection>
-            <StepSection
-              stepNumber={3}
-              title={"Update sm.json"}
-              isOpen={activeStep === 3}
-              onOpenStep={onOpenStep(3)}
-            >
-              <Flex>
-                <Text sx={{ color: (t) => t.colors?.textClear }}>
-                  Update your <Text variant={"pre"}>sm.json</Text> file with the
-                  property <Text variant={"pre"}>localSliceCanvasURL</Text> in
-                  the shape of{" "}
-                  <Text variant={"pre"}>http://localhost:PORT/PATH</Text>.
-                </Text>
-              </Flex>
-            </StepSection>
-            <StepSection
-              stepNumber={4}
-              title={"Check configuration"}
-              isOpen={activeStep === 4}
-              onOpenStep={onOpenStep(4)}
-            >
-              <Flex>
-                <Text sx={{ color: (t) => t.colors?.textClear }}>
-                  After you’ve done the previous steps, we need to check that
-                  everything works in order.
-                </Text>
-              </Flex>
-            </StepSection>
+            <NuxtSetupSteps activeStep={activeStep} onOpenStep={onOpenStep} />
           </Flex>
         </Flex>
-        <Flex
-          sx={{
-            padding: "16px 20px",
-          }}
-        >
-          <Flex
-            sx={{
-              padding: 3,
-              backgroundColor: (t) => `${t.colors?.gray}`,
-              borderRadius: 8,
-              flexDirection: "column",
-            }}
-          >
-            <Flex
-              sx={{
-                alignItems: "center",
-                mb: 3,
-              }}
-            >
-              <Flex
-                sx={{
-                  mr: 2,
-                }}
-              >
-                <FaRegQuestionCircle size={20} color={"#667587"} />
-              </Flex>
-              <Text sx={{ fontSize: 2, fontWeight: 500 }}>Help Section</Text>
-            </Flex>
-            <Text sx={{ color: (t) => t.colors?.textClear }}>
-              Are you having difficulties setting up the preview? You can check{" "}
-              <Link target={"_blank"} href={"https://prismic.io"}>
-                the documentation
-              </Link>{" "}
-              for more info to set it up correctly.
-            </Text>
-          </Flex>
-        </Flex>
+        <HelpSection />
       </Flex>
     </Drawer>
   );
 };
+
+const HelpSection = () => (
+  <Flex
+    sx={{
+      padding: "16px 20px",
+    }}
+  >
+    <Flex
+      sx={{
+        padding: 3,
+        backgroundColor: (t) => `${t.colors?.gray}`,
+        borderRadius: 8,
+        flexDirection: "column",
+      }}
+    >
+      <Flex
+        sx={{
+          alignItems: "center",
+          mb: 3,
+        }}
+      >
+        <Flex
+          sx={{
+            mr: 2,
+          }}
+        >
+          <FaRegQuestionCircle size={20} color={"#667587"} />
+        </Flex>
+        <Text sx={{ fontSize: 2, fontWeight: 500 }}>Help Section</Text>
+      </Flex>
+      <Text sx={{ color: (t) => t.colors?.textClear }}>
+        Are you having difficulties setting up the preview? You can check{" "}
+        <Link target={"_blank"} href={"https://prismic.io"}>
+          the documentation
+        </Link>{" "}
+        for more info to set it up correctly.
+      </Text>
+    </Flex>
+  </Flex>
+);
 
 export default SetupDrawer;
