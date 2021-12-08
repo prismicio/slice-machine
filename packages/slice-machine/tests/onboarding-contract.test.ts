d6afcfcaa8b20a117e429d46427640310165a27f
Miniplop <kevin.jean@prismic.io>
2021-12-08 13:43:55 +0100

    add a test on the fail status

--- packages/slice-machine/tests/onboarding-contract.test.ts
+++ packages/slice-machine/tests/onboarding-contract.test.ts
@@ -1,32 +1,66 @@
+// We mock the getEnv service
+jest.mock("../server/src/api/services/getEnv");
+
 import onboarding from "../server/src/api/tracking/onboarding";
 import { TrackingEventId } from "@models/common/TrackingEvent";
-
-let mockSendOnboarding = jest.fn(async () =>
-  Promise.resolve({
-    status: 200,
-  })
-);
-
-const mockClient = jest.fn(() => ({
-  sendOnboarding: mockSendOnboarding,
-}))();
-
-jest.mock(
-  `../server/src/api/services/getEnv`,
-  () => async () =>
-    Promise.resolve({
-      env: {
-        client: mockClient,
-      },
-    })
-);
+import getEnv from "../server/src/api/services/getEnv";
 
 describe("onboarding tracking", () => {
   test("it should return no error on 200 status from the tracking service", async () => {
+    const onboardingResponse = {
+      status: 200,
+    };
+
+    const mockSendOnboarding = jest.fn(async () =>
+      Promise.resolve(onboardingResponse)
+    );
+
+    getEnv.mockReturnValue(
+      Promise.resolve({
+        env: {
+          client: {
+            sendOnboarding: mockSendOnboarding,
+          },
+        },
+      })
+    );
+
     const result = await onboarding({
       id: TrackingEventId.ONBOARDING_START,
     });
 
+    expect(mockSendOnboarding).toHaveBeenCalledWith({
+      id: TrackingEventId.ONBOARDING_START,
+    });
     expect(result.err).toBeNull();
+  });
+
+  test("it should return no error on 500 status from the tracking service", async () => {
+    const onboardingResponse = {
+      status: 500,
+    };
+
+    const mockSendOnboarding = jest.fn(async () =>
+      Promise.resolve(onboardingResponse)
+    );
+
+    getEnv.mockReturnValue(
+      Promise.resolve({
+        env: {
+          client: {
+            sendOnboarding: mockSendOnboarding,
+          },
+        },
+      })
+    );
+
+    const result = await onboarding({
+      id: TrackingEventId.ONBOARDING_START,
+    });
+
+    expect(mockSendOnboarding).toHaveBeenCalledWith({
+      id: TrackingEventId.ONBOARDING_START,
+    });
+    expect(result.err).toEqual(onboardingResponse);
   });
 });
