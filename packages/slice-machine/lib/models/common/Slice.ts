eca0e589df26c9e4838ea29fceb7d050ac1214f5
Amaury Cahuet <amaury.cahuet@gmail.com>
2021-12-21 18:15:42 +0100

    clean slice save response / request types, return warning and not error when some screenshots have been taken but not all

--- packages/slice-machine/lib/models/common/Slice.ts
+++ packages/slice-machine/lib/models/common/Slice.ts
@@ -1,4 +1,6 @@
 import type Models from "@slicemachine/core/build/src/models";
+import { SliceMockConfig } from "./MockConfig";
+import { Screenshots } from "./Screenshots";
 import { Variation } from "./Variation";
 
 const Slice = {
@@ -16,5 +18,17 @@ const Slice = {
     };
   },
 };
+
+export interface SliceSaveBody {
+  sliceName: string;
+  from: string;
+  model: Models.SliceAsObject;
+  mockConfig?: SliceMockConfig;
+}
+
+export interface SliceSaveResponse {
+  screenshots: Screenshots;
+  warning: string | null;
+}
 
 export default Slice;
