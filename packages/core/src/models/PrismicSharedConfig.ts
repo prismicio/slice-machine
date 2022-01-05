726bd1b30abc5039e318cdd31d39e21645e14e60
Miniplop <kevin.jean@prismic.io>
2022-01-05 17:33:09 +0100

    use the shortId for alias

--- packages/core/src/models/PrismicSharedConfig.ts
+++ packages/core/src/models/PrismicSharedConfig.ts
@@ -7,6 +7,7 @@ export const PrismicSharedConfig = t.intersection([
   }),
   t.partial({
     userId: t.union([t.string, t.undefined]),
+    shortId: t.union([t.string, t.undefined]),
     oauthAccessToken: t.string,
     authUrl: t.string,
   }),
