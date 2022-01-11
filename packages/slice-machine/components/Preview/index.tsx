3fcf8bd296f63040d80f3bac785312434f497da4
Amaury Cahuet <amaury.cahuet@gmail.com>
2022-01-11 11:32:36 +0100

    fix build, adjust max warning

--- packages/slice-machine/components/Preview/index.tsx
+++ packages/slice-machine/components/Preview/index.tsx
@@ -16,7 +16,6 @@ import {
   selectPreviewUrl,
 } from "@src/modules/environment";
 import { SliceMachineStoreType } from "@src/redux/type";
-import { SetupError } from "./components/SetupError";
 
 export type SliceView = SliceViewItem[];
 export type SliceViewItem = Readonly<{ sliceID: string; variationID: string }>;
