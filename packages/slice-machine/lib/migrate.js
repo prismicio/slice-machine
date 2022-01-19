26ffb9c6e01b30a5273e703d68043b0b6c2baba4
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-01-19 19:54:45 +0100

    linter(sm-ui): lint js

--- packages/slice-machine/lib/migrate.js
+++ packages/slice-machine/lib/migrate.js
@@ -4,14 +4,19 @@ import Files from "./utils/files";
 import { CustomPaths } from "./models/paths";
 
 const migrate = (model, info, env, write = true) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { type, fieldset, "non-repeat": nonRepeat = {}, repeat = {} } = model;
   if (type !== "Slice") {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     return { model, migrated: false };
   }
   const newModel = {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
     id: snakelize(info.sliceName),
     type: "SharedSlice",
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
     name: info.sliceName,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
     description: fieldset,
     variations: [
       {
@@ -19,18 +24,25 @@ const migrate = (model, info, env, write = true) => {
         name: "Default slice",
         docURL: "...",
         version: uniqid(),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         description: fieldset,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         primary: nonRepeat,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         items: repeat,
       },
     ],
   };
 
   if (write) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
     const modelPath = CustomPaths(env.cwd)
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
       .library(info.from)
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       .slice(info.sliceName)
       .model();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     Files.write(modelPath, newModel);
   }
 
