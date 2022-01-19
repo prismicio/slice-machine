b9d9cff54a193d88cbd84c6b5aa571d233f60366
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-01-19 17:27:42 +0100

    chore(linter): add more eslint-disable lines

--- packages/slice-machine/lib/models/common/CustomType/index.ts
+++ packages/slice-machine/lib/models/common/CustomType/index.ts
@@ -55,6 +55,7 @@ export const CustomType = {
     };
   },
   toJsonModel(ct: CustomType<ObjectTabs>): CustomTypeJsonModel {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-unused-vars
     const { tabs, previewUrl, ...rest } = ct;
     return {
       ...rest,
