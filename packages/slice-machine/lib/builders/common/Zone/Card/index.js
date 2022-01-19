bb65c4d636f3fa8b3d28e13e6934194794c7d804
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-01-19 22:28:07 +0100

    linter(sm-ui): lint js

--- packages/slice-machine/lib/builders/common/Zone/Card/index.js
+++ packages/slice-machine/lib/builders/common/Zone/Card/index.js
@@ -35,63 +35,92 @@ const FieldZone = ({
   }));
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId={title}>
+    <DragDropContext
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      onDragEnd={onDragEnd}
+    >
+      <Droppable
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-assignment
+        droppableId={title}
+      >
         {(provided, snapshot) => (
           <Box
             as="ul"
             ref={provided.innerRef}
             {...provided.droppableProps}
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
             style={{ ...provided.droppableProps.style, padding: "4px 0" }}
           >
-            {fields.map((item, index) => {
-              const {
-                value: { config, type },
-              } = item;
-              const widget = findWidgetByConfigOrType(Widgets, config, type);
-              if (!widget) {
-                return (
-                  <Li>
-                    <Text>Field type "{type}" not supported</Text>
-                  </Li>
+            {
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+              fields.map((item, index) => {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                const {
+                  value: { config, type },
+                } = item;
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument
+                const widget = findWidgetByConfigOrType(Widgets, config, type);
+                if (!widget) {
+                  return (
+                    <Li>
+                      <Text>Field type "{type}" not supported</Text>
+                    </Li>
+                  );
+                }
+
+                const props = {
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                  item,
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                  index,
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                  store,
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                  Model,
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-assignment
+                  tabId,
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                  widget,
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-assignment
+                  showHints,
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+                  key: item.key,
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-assignment
+                  enterSelectMode,
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                  renderFieldAccessor,
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-assignment
+                  enterEditMode,
+                  parentSnapshot: snapshot,
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                  deleteItem: onDeleteItem,
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unsafe-member-access
+                  draggableId: `list-item-${item.key}`,
+                };
+
+                if (widget.CustomListItem) {
+                  const { CustomListItem } = widget;
+                  return <CustomListItem {...props} framework={framework} />;
+                }
+
+                const HintElement = (
+                  <Hint
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
+                    item={item}
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                    show={showHints}
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                    isRepeatable={isRepeatable}
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                    renderHintBase={renderHintBase}
+                    framework={framework}
+                    Widgets={Widgets}
+                    typeName={widget.CUSTOM_NAME || widget.TYPE_NAME}
+                  />
                 );
-              }
-
-              const props = {
-                item,
-                index,
-                store,
-                Model,
-                tabId,
-                widget,
-                showHints,
-                key: item.key,
-                enterSelectMode,
-                renderFieldAccessor,
-                enterEditMode,
-                parentSnapshot: snapshot,
-                deleteItem: onDeleteItem,
-                draggableId: `list-item-${item.key}`,
-              };
-
-              if (widget.CustomListItem) {
-                const { CustomListItem } = widget;
-                return <CustomListItem {...props} framework={framework} />;
-              }
-
-              const HintElement = (
-                <Hint
-                  item={item}
-                  show={showHints}
-                  isRepeatable={isRepeatable}
-                  renderHintBase={renderHintBase}
-                  framework={framework}
-                  Widgets={Widgets}
-                  typeName={widget.CUSTOM_NAME || widget.TYPE_NAME}
-                />
-              );
-              return <ListItem {...props} HintElement={HintElement} />;
-            })}
+                return <ListItem {...props} HintElement={HintElement} />;
+              })
+            }
             {provided.placeholder}
             <NewFieldC />
           </Box>
