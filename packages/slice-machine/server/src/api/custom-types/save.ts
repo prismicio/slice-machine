55d3cc8ffc1fe2b8ff8f45b2082ec9c5b2429ace
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-01-19 16:52:58 +0100

    chore(linter): add more eslint-disable lines

--- packages/slice-machine/server/src/api/custom-types/save.ts
+++ packages/slice-machine/server/src/api/custom-types/save.ts
@@ -15,23 +15,28 @@ interface Body {
 export default async function handler(req: { body: Body }) {
   const { env } = await getEnv();
   const { model, mockConfig } = req.body;
-
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
   const modelPath = CustomTypesPaths(env.cwd).customType(model.id).model();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
   const mockPath = GeneratedCustomTypesPaths(env.cwd)
     .customType(model.id)
     .mock();
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const updatedMockConfig = insertMockConfig(env.cwd, {
     key: model.id,
     prefix: "_cts",
     value: mockConfig,
   });
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   Files.write(modelPath, CustomType.toJsonModel(model));
   const mocked = await mock(
     model,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-argument
     CustomTypeMockConfig.getCustomTypeMockConfig(updatedMockConfig, model.id)
   );
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   Files.write(mockPath, mocked);
   return {};
 }
