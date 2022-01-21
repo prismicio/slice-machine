904a099945bf4382f1a2bba8fb92dcd83f44b209
Miniplop <kevin.jean@prismic.io>
2022-01-21 10:28:12 +0100

    Add a try catch to prevent the API to crash if puppetter timeout

--- packages/slice-machine/server/src/api/screenshots/screenshots.ts
+++ packages/slice-machine/server/src/api/screenshots/screenshots.ts
@@ -9,7 +9,7 @@ import { Frameworks } from "@slicemachine/core/build/src/models";
 
 export function validateEnv(
   framework: Frameworks,
-  previewUrl: string | undefined
+  simulatorUrl: string | undefined
 ) {
   if (!simulatorIsSupported(framework)) {
     const reason = "Could not generate preview: framework is not supported";
@@ -20,7 +20,7 @@ export function validateEnv(
       screenshots: {},
     };
   }
-  if (!previewUrl) {
+  if (!simulatorUrl) {
     const reason =
       "Could not generate screenshots: localSlicePreviewURL undefined in sm.json file";
 
@@ -46,33 +46,44 @@ export default async function handler({
     return maybeErr;
   }
 
-  const { screenshots, failure } = await generateScreenshotAndRemoveCustom(
-    env,
-    libraryName,
-    sliceName
-  );
+  try {
+    const { screenshots, failure } = await generateScreenshotAndRemoveCustom(
+      env,
+      libraryName,
+      sliceName
+    );
 
-  if (failure.length > 0) {
-    const message:
-      | string
-      | null = `Could not generate screenshots for variations: ${failure
-      .map((f) => f.variationId)
-      .join(" | ")}`;
+    if (failure.length > 0) {
+      const message:
+        | string
+        | null = `Could not generate screenshots for variations: ${failure
+        .map((f) => f.variationId)
+        .join(" | ")}`;
 
-    /* We display an error if no screenshot has been taken */
-    const isError = Object.keys(screenshots).length === 0;
+      /* We display an error if no screenshot has been taken */
+      const isError = Object.keys(screenshots).length === 0;
+
+      return {
+        err: isError ? new Error(message) : null,
+        reason: isError ? message : null,
+        warning: isError ? null : message,
+        screenshots,
+      };
+    }
 
     return {
-      err: isError ? new Error(message) : null,
-      reason: isError ? message : null,
-      warning: isError ? null : message,
+      err: null,
+      reason: null,
       screenshots,
     };
+  } catch (e) {
+    const crashMessage =
+      "Could not generate screenshots for this slice, upload manually a screenshot";
+    return {
+      err: new Error(crashMessage),
+      reason: crashMessage,
+      warning: null,
+      screenshots: {},
+    };
   }
-
-  return {
-    err: null,
-    reason: null,
-    screenshots,
-  };
 }
