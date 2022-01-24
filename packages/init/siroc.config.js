a9b2c7f09619a2011e3f0b6ccdc2ba4a15ed8b15
Miniplop <kevin.jean@prismic.io>
2022-01-24 12:03:20 +0100

    Rebase and fix audit

---
+++ packages/init/siroc.config.js
@@ -0,0 +1,18 @@
+import { defineSirocConfig } from "siroc";
+import replace from "@rollup/plugin-replace";
+
+export default defineSirocConfig({
+  rollup: {
+    output: {
+      sourcemap: true,
+    },
+    plugins: [
+      replace({
+        "process.env.NEXT_PUBLIC_SEGMENT_KEY": `"${
+          process.env.NEXT_PUBLIC_SEGMENT_KEY || ""
+        }"`,
+        preventAssignment: true,
+      }),
+    ],
+  },
+});
