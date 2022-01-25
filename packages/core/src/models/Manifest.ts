0eb3f945854bd321f68b8a65cdc7abc53258d7d8
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-01-25 19:37:30 +0100

    chore(core): validate apiEndpoint

--- packages/core/src/models/Manifest.ts
+++ packages/core/src/models/Manifest.ts
@@ -1,9 +1,34 @@
 import * as t from "io-ts";
 import { FrameworksC } from "./Framework";
 
+const apiEndpoint = new t.Type<string, string, unknown>(
+  "apiEndpoint",
+  (input: unknown): input is string => typeof input === "string",
+  (input, context) => {
+    if (typeof input !== "string") return t.failure(input, context);
+    try {
+      const url = new URL(input);
+      if (url.pathname.replace(/\/$/, "") !== "/api/v2") {
+        return t.failure(input, context);
+      }
+      if (
+        url.hostname.endsWith(".prismic.io") ||
+        url.hostname.endsWith(".wroom.io") ||
+        url.hostname.endsWith(".wroom.test")
+      ) {
+        return t.success(input);
+      }
+      return t.failure(input, context);
+    } catch {
+      return t.failure(input, context);
+    }
+  },
+  t.identity
+);
+
 export const Manifest = t.intersection([
   t.type({
-    apiEndpoint: t.string,
+    apiEndpoint,
   }),
   t.partial({
     storybook: t.string,
