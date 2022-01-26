89a6436990387a9fb5f76d07029a5a043b7d8505
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-01-26 13:57:12 +0100

    refactor: apiEndpoint validation

--- packages/core/src/models/Manifest.ts
+++ packages/core/src/models/Manifest.ts
@@ -6,22 +6,12 @@ const apiEndpoint = new t.Type<string, string, unknown>(
   (input: unknown): input is string => typeof input === "string",
   (input, context) => {
     if (typeof input !== "string") return t.failure(input, context);
-    try {
-      const url = new URL(input);
-      if (url.pathname.replace(/\/$/, "") !== "/api/v2") {
-        return t.failure(input, context);
-      }
-      if (
-        url.hostname.endsWith(".prismic.io") ||
-        url.hostname.endsWith(".wroom.io") ||
-        url.hostname.endsWith(".wroom.test")
-      ) {
-        return t.success(input);
-      }
-      return t.failure(input, context);
-    } catch {
-      return t.failure(input, context);
-    }
+    const regx = new RegExp(
+      "^https?://[a-z0-9][a-z0-9-]{2,}[a-z0-9].(prismic.io|wroom.io|wroom.test)/api/v2/?$",
+      "i"
+    );
+    const result = regx.test(input);
+    return result ? t.success(input) : t.failure(input, context);
   },
   t.identity
 );
