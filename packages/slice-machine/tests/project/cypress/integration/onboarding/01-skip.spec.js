c7a61f255c73984299c1daa6edc4fdb4046e860d
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-02-01 18:23:50 +0100

    test(skip-onbarding): udpate spec

--- packages/slice-machine/tests/project/cypress/integration/onboarding/01-skip.spec.js
+++ packages/slice-machine/tests/project/cypress/integration/onboarding/01-skip.spec.js
@@ -12,7 +12,8 @@ describe("onboarding skip", () => {
     cy.get('[data-cy=skip-onboarding]').click()
 
     cy.location('pathname', {timeout: 1000}).should('eq', '/')
+
   
-    cy.getLocalStorage("persist:root").should('eq', '{"userContext":"{\\"hasSendAReview\\":false,\\"isOnboarded\\":true}","_persist":"{\\"version\\":-1,\\"rehydrated\\":true}"}')
+    cy.getLocalStorage("persist:root").should('eq', '{"userContext":"{\\"hasSendAReview\\":false,\\"isOnboarded\\":true,\\"dismissedUpdate\\":\\"\\"}","_persist":"{\\"version\\":-1,\\"rehydrated\\":true}"}')
   })
 })
