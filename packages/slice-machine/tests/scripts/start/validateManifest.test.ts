21e24b55f83e77e12f9ac039be13240c6194caea
Amaury Cahuet <amaury.cahuet@gmail.com>
2022-02-02 18:19:57 +0100

    add test for the start script.

---
+++ packages/slice-machine/tests/scripts/start/validateManifest.test.ts
@@ -0,0 +1,64 @@
+import { validateManifest } from "../../../scripts/start/validateManifest";
+import { ManifestState } from "../../../lib/env/manifest";
+
+describe("Scripts.start.validateManifest", () => {
+  it("Should return true if the manifest is valid", () => {
+    jest.spyOn(global.console, "log").mockImplementation(() => null);
+
+    const { isManifestValid } = validateManifest({
+      state: ManifestState.Valid,
+      message: "",
+      content: null,
+    });
+
+    expect(isManifestValid).toBe(true);
+  });
+
+  it("Should return false if the manifest is missing", () => {
+    jest.spyOn(global.console, "log").mockImplementation(() => null);
+
+    const { isManifestValid } = validateManifest({
+      state: ManifestState.NotFound,
+      message: "",
+      content: null,
+    });
+
+    expect(isManifestValid).toBe(false);
+  });
+
+  it("Should return false if apiEndpoint is missing", () => {
+    jest.spyOn(global.console, "log").mockImplementation(() => null);
+
+    const { isManifestValid } = validateManifest({
+      state: ManifestState.MissingEndpoint,
+      message: "",
+      content: null,
+    });
+
+    expect(isManifestValid).toBe(false);
+  });
+
+  it("Should return false if apiEndpoint is invalid", () => {
+    jest.spyOn(global.console, "log").mockImplementation(() => null);
+
+    const { isManifestValid } = validateManifest({
+      state: ManifestState.InvalidEndpoint,
+      message: "",
+      content: null,
+    });
+
+    expect(isManifestValid).toBe(false);
+  });
+
+  it("Should return false if the manifest's format is invalid", () => {
+    jest.spyOn(global.console, "log").mockImplementation(() => null);
+
+    const { isManifestValid } = validateManifest({
+      state: ManifestState.InvalidJson,
+      message: "",
+      content: null,
+    });
+
+    expect(isManifestValid).toBe(false);
+  });
+});
