50bc4e3ee4bc74b131c489ecdc911ad953a03f50
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-02-11 14:54:04 +0100

    test(cypress): remove custom-type and slie before runing test

--- packages/slice-machine/tests/project/cypress/integration/customTypes/00-create.spec.js
+++ packages/slice-machine/tests/project/cypress/integration/customTypes/00-create.spec.js
@@ -1,9 +1,13 @@
 import {randomString, capitalizeFirstChar} from '../../support/utils'
 
 describe("Custom Types specs", () => {
+
+  const name = "My Test"
+  const id = "my-test"
   beforeEach(() => {
     cy.clearLocalStorageSnapshot();
     cy.cleanSliceMachineUserContext();
+    cy.task("rmrf", `customtypes/${id}`)
   });
 
   it('A user can create a custom type', () => {
@@ -15,9 +19,6 @@ describe("Custom Types specs", () => {
     cy
       .get('[data-cy=create-ct-modal]')
       .should('be.visible');
-
-    const name = [randomString(), randomString()].map(capitalizeFirstChar).join(" ")
-    const id = name.toLowerCase().replace(" ", '-')
 
     cy.get('input[data-cy=ct-name-input]').type(name)
     cy.get('input[data-cy=ct-id-input]').type(id)
--- packages/slice-machine/tests/project/cypress/integration/slices/00-create.spec.js
+++ packages/slice-machine/tests/project/cypress/integration/slices/00-create.spec.js
@@ -1,25 +1,27 @@
 import {randomPascalCase} from '../../support/utils'
 
 describe("Create Slices", () => {
+  const name = "TestSlice"
+  const lib = "slices"
   beforeEach(() => {
     cy.clearLocalStorageSnapshot();
     cy.cleanSliceMachineUserContext();
+    cy.task("rmrf", `${lib}/${name}`)
   });
 
   it('A user can create a slice', () => {
     cy.setupSliceMachineUserContext()
-    cy.visit('/slices')
+    cy.visit(`/${lib}`)
     cy.waitUntil(() => cy.get('[data-cy=create-slice]'))
     cy.get('[data-cy=create-slice]').click()
     cy
       .get('[data-cy=create-slice-modal]')
       .should('be.visible');
 
-    const name = randomPascalCase()
     cy.get('input[data-cy=slice-name-input]').type(name)
     cy.get('[data-cy=create-slice-modal]').submit()
-    cy.location('pathname', {timeout: 5000}).should('eq', `/slices/${name}/default-slice`)
-    cy.visit(`/slices/${name}/default-slice`)
-    cy.location('pathname', {timeout: 5000}).should('eq', `/slices/${name}/default-slice`)
+    cy.location('pathname', {timeout: 5000}).should('eq', `/${lib}/${name}/default-slice`)
+    cy.visit(`/${lib}/${name}/default-slice`)
+    cy.location('pathname', {timeout: 5000}).should('eq', `/${lib}/${name}/default-slice`)
   })
 })
