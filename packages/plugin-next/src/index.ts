030ab77c6fb447b386c0ff6d8d37901480fa6e13
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-02-16 14:51:20 +0100

    add snippets to next plugin

--- packages/plugin-next/src/index.ts
+++ packages/plugin-next/src/index.ts
@@ -1,4 +1,4 @@
 import type { Plugin } from "@slicemachine/plugin-middleware";
-export { slice, story, index } from "@slicemachine/plugin-react";
+export { slice, story, index, snippets } from "@slicemachine/plugin-react";
 
 export const framework: Plugin["framework"] = "next";
