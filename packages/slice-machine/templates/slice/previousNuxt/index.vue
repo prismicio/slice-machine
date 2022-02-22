7c55a27358a469d8a526729d1617716e5c4a1ebc
Miniplop <kevin.jean@prismic.io>
2022-02-22 13:06:33 +0100

    create the template for the previous nuxt version

---
+++ packages/slice-machine/templates/slice/previousNuxt/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="section">
+    <prismic-rich-text :field="slice.primary.title" class="title" />
+    <prismic-rich-text :field="slice.primary.description" />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "{{componentName}}",
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      },
+    },
+  },
+}
+</script>
+
+<style scoped>
+.section {
+  position: relative;
+  background: #f7f7f7;
+  color: #111;
+  padding: 4em;
+  text-align: center;
+}
+.title {
+  margin-bottom: 2em;
+}
+</style>
