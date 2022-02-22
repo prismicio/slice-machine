acab22163776564a097fcf48e1e4895375b0f890
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-02-22 16:35:08 +0100

    remove extra tabs

--- packages/slice-machine/lib/builders/SliceBuilder/SetupDrawer/steps/next.tsx
+++ packages/slice-machine/lib/builders/SliceBuilder/SetupDrawer/steps/next.tsx
@@ -11,16 +11,16 @@ import { SetupStatus } from "@src/modules/simulator/types";
 const CreatePageInstructions = {
   code: `import { SliceSimulator } from "@prismicio/slice-simulator-react";
 import { SliceZone } from "@prismicio/react";
-  
+
 import state from "../.slicemachine/libraries-state.json";
 import { components } from "../slices/components";
-  
+
 const SliceSimulatorPage = () => (<SliceSimulator
   // The "sliceZone" prop should be a function receiving slices and rendering them using your "SliceZone" component.
   sliceZone={(props) => <SliceZone {...props} components={components} />}
   state={state}
 />);
-  
+
 export default SliceSimulatorPage;`,
   instructions: (
     <>
