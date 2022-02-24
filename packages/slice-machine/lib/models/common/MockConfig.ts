e7f631d2626b2630ce8030ecb129403abce238a4
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-02-24 15:57:27 +0100

    fix(maybe): image-mock-form

--- packages/slice-machine/lib/models/common/MockConfig.ts
+++ packages/slice-machine/lib/models/common/MockConfig.ts
@@ -23,6 +23,15 @@ export const SliceMockConfig = {
     sliceMockConfig: SliceMockConfig,
     variationId: string
   ) {
+    if (Array.isArray(sliceMockConfig)) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const result = sliceMockConfig.find(
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        (config) => config.variation === variationId
+      );
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return result || {};
+    }
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return sliceMockConfig[variationId] || {};
   },
