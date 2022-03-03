a5b6b5ea7614f2b30a569896bc292ed0e8af5334
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-03-03 20:39:58 +0100

    render video tool tip

--- packages/slice-machine/components/AppLayout/Navigation/Menu/Navigation/Item.tsx
+++ packages/slice-machine/components/AppLayout/Navigation/Menu/Navigation/Item.tsx
@@ -2,43 +2,48 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { Box, Link as ThemeLink } from "theme-ui";
-import { LinkProps } from "components/AppLayout/Navigation";
+import type { LinkProps } from "../../";
 
-interface itemProps {
+export interface ItemProps {
   link: LinkProps;
   theme?: "emphasis";
+  "data-for"?: string;
+  "data-tip"?: string;
 }
 
-const Item: React.FC<itemProps> = ({ link, theme }) => {
-  const router = useRouter();
-  return (
-    <Box as="li" key={link.title}>
-      <Link href={link.href} passHref>
-        <ThemeLink
-          variant={
-            theme === "emphasis" ? "links.sidebarEmphasis" : "links.sidebar"
-          }
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            mb: "10px",
-            ...(link.match(router.asPath)
-              ? {
-                  color: "text",
-                  bg: "#E6E6EA",
-                }
-              : {}),
-          }}
-          target={link.target}
-        >
-          <link.Icon size={24} />
-          <Box as="span" sx={{ ml: 2, fontWeight: 400 }}>
-            {link.title}
-          </Box>
-        </ThemeLink>
-      </Link>
-    </Box>
-  );
-};
+//const Item: React.FC<ItemProps> = ({ link, theme, ...rest }) => {
+const Item = React.forwardRef<HTMLDivElement, ItemProps>(
+  ({ link, theme, ...rest }, ref) => {
+    const router = useRouter();
+    return (
+      <Box {...rest} ref={ref} as="li" key={link.title}>
+        <Link href={link.href} passHref>
+          <ThemeLink
+            variant={
+              theme === "emphasis" ? "links.sidebarEmphasis" : "links.sidebar"
+            }
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              mb: "10px",
+              ...(link.match(router.asPath)
+                ? {
+                    color: "text",
+                    bg: "#E6E6EA",
+                  }
+                : {}),
+            }}
+            target={link.target}
+          >
+            <link.Icon size={24} />
+            <Box as="span" sx={{ ml: 2, fontWeight: 400 }}>
+              {link.title}
+            </Box>
+          </ThemeLink>
+        </Link>
+      </Box>
+    );
+  }
+);
 
 export default Item;
