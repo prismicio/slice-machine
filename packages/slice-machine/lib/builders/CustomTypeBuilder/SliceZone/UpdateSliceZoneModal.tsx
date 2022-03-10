81aed8c8aeb714f938cc275d56716cb2cf3b5d54
Miniplop <kevin.jean@prismic.io>
2022-03-10 13:22:03 +0100

    create the empty state for the update slice zone modal

--- packages/slice-machine/lib/builders/CustomTypeBuilder/SliceZone/UpdateSliceZoneModal.tsx
+++ packages/slice-machine/lib/builders/CustomTypeBuilder/SliceZone/UpdateSliceZoneModal.tsx
@@ -1,106 +1,54 @@
-import { FieldArray } from "formik";
-import { Checkbox } from "theme-ui";
 import ModalFormCard from "../../../../components/ModalFormCard";
 
 import SliceState from "@lib/models/ui/SliceState";
-import { SharedSlice } from "@lib/models/ui/Slice";
+import UpdateSliceZoneModalEmptyState from "./UpdateSliceZoneModalEmptyState";
+import UpdateSliceZoneModalList from "./UpdateSliceZoneModalList";
 
-import Grid from "@components/Grid";
+interface UpdateSliceModalProps {
+  isOpen: boolean;
+  formId: string;
+  close: () => void;
+  onSubmit: (values: SliceZoneFormValues) => void;
+  availableSlices: ReadonlyArray<SliceState>;
+  slicesInSliceZone: ReadonlyArray<SliceState>;
+}
 
-const UpdateSliceZoneModal = ({
+export type SliceZoneFormValues = {
+  sliceKeys: string[];
+};
+
+const UpdateSliceZoneModal: React.FC<UpdateSliceModalProps> = ({
   isOpen,
   formId,
   close,
   onSubmit,
   availableSlices,
   slicesInSliceZone,
-}: {
-  isOpen: boolean;
-  formId: string;
-  close: () => void;
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  onSubmit: Function;
-  availableSlices: ReadonlyArray<SliceState>;
-  slicesInSliceZone: ReadonlyArray<SliceState>;
 }) => {
+  const projectHasAvailableSlices = !!availableSlices.length;
+
   return (
     <ModalFormCard
       isOpen={isOpen}
       formId={formId}
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      close={() => close()}
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
-      onSubmit={(values: any) => onSubmit(values)}
+      close={close}
+      onSubmit={(values: SliceZoneFormValues) => onSubmit(values)}
       initialValues={{
         sliceKeys: slicesInSliceZone.map((slice) => slice.infos.meta.id),
       }}
       content={{
-        title: "Add a new Slice",
+        title: "Update Slice zone",
       }}
+      omitFooter={!projectHasAvailableSlices}
     >
-      {({ values }) => (
-        <FieldArray
-          name="sliceKeys"
-          render={(arrayHelpers) => {
-            return (
-              <Grid
-                gridTemplateMinPx="200px"
-                elems={availableSlices}
-                defineElementKey={(slice: SliceState) => slice.infos.sliceName}
-                renderElem={(slice: SliceState) => {
-                  return SharedSlice.render({
-                    bordered: true,
-                    displayStatus: false,
-                    thumbnailHeightPx: "220px",
-                    slice,
-                    Wrapper: ({
-                      slice,
-                      children,
-                    }: {
-                      slice: SliceState;
-                      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                      children: any;
-                    }) => {
-                      return (
-                        <div
-                          style={{ cursor: "pointer" }}
-                          onClick={() => {
-                            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-                            const isInSliceZone = values.sliceKeys.includes(
-                              slice.infos.meta.id
-                            );
-                            if (isInSliceZone) {
-                              return arrayHelpers.remove(
-                                // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-                                values.sliceKeys.indexOf(slice.infos.meta.id)
-                              );
-                            }
-                            arrayHelpers.push(slice.infos.meta.id);
-                          }}
-                          key={`${slice.from}-${slice.infos.sliceName}`}
-                        >
-                          {children}
-                        </div>
-                      );
-                    },
-                    CustomStatus: ({ slice }: { slice: SliceState }) => {
-                      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-                      const isInSliceZone = values.sliceKeys.includes(
-                        slice.infos.meta.id
-                      );
-                      return isInSliceZone ? (
-                        <Checkbox value="true" defaultChecked />
-                      ) : (
-                        <Checkbox value="false" />
-                      );
-                    },
-                  });
-                }}
-              />
-            );
-          }}
-        />
-      )}
+      {projectHasAvailableSlices
+        ? ({ values }) => (
+            <UpdateSliceZoneModalList
+              values={values}
+              availableSlices={availableSlices}
+            />
+          )
+        : () => <UpdateSliceZoneModalEmptyState />}
     </ModalFormCard>
   );
 };
