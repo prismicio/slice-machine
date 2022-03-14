51d521238c54694cb6232bee3554ec8184f70fe4
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-03-14 15:27:46 +0100

    cleaning: remove un-needed deps, and remove src directory from build

---
+++ packages/core/src/index.ts
@@ -0,0 +1,6 @@
+export * as FileSystem from "./filesystem";
+export * as Utils from "./utils";
+export * from "./core";
+export * as Models from "./models";
+export * as Libraries from "./libraries";
+export * as Mocks from "./libraries/mocks";
