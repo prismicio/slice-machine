879cc14972a40cedbee16701c7d44c005c999f85
Miniplop <kevin.jean@prismic.io>
2022-03-21 15:22:54 +0100

    Create 2 separated keys for the segment keys + add bump command for releasing into alpha/prod

--- packages/init/siroc.config.js
+++ packages/init/siroc.config.js
@@ -8,8 +8,8 @@ export default defineSirocConfig({
     },
     plugins: [
       replace({
-        "process.env.NEXT_PUBLIC_SEGMENT_KEY": `"${
-          process.env.NEXT_PUBLIC_SEGMENT_KEY ||
+        "process.env.PUBLIC_SM_INIT_SEGMENT_KEY": `"${
+          process.env.PUBLIC_SM_INIT_SEGMENT_KEY ||
           "JfTfmHaATChc4xueS7RcCBsixI71dJIJ"
         }"`,
         preventAssignment: true,
