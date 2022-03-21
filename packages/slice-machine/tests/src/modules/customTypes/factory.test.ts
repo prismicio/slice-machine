d493f7b99c17ef17e84a4ccc9cd2474c807ff7f3
Arnaud Lewis <arnaud.lewis@gmail.com>
2022-03-21 14:16:38 +0100

    fix custom types factory tests

--- packages/slice-machine/tests/src/modules/customTypes/factory.test.ts
+++ packages/slice-machine/tests/src/modules/customTypes/factory.test.ts
@@ -1,37 +1,37 @@
 import "@testing-library/jest-dom";
 
 import { createCustomType } from "@src/modules/customTypes/factory";
-import { CustomType, ObjectTabs } from "@models/common/CustomType";
+import { CustomTypeSM } from "@slicemachine/core/build/src/models/CustomType";
 
 describe("[Custom types factory]", () => {
   describe("[createCustomType]", () => {
     it("should create a custom type with repeatable true", () => {
-      const expectedCustomType: CustomType<ObjectTabs> = {
+      const expectedCustomType: CustomTypeSM = {
         id: "id",
         label: "lama",
         repeatable: true,
         status: true,
-        tabs: {
-          Main: {
+        tabs: [
+          {
             key: "Main",
-            value: {},
+            value: [],
           },
-        },
+        ],
       };
       expect(createCustomType("id", "lama", true)).toEqual(expectedCustomType);
     });
     it("should create a custom type with repeatable false", () => {
-      const expectedCustomType: CustomType<ObjectTabs> = {
+      const expectedCustomType: CustomTypeSM = {
         id: "id",
         label: "lama",
         repeatable: false,
         status: true,
-        tabs: {
-          Main: {
+        tabs: [
+          {
             key: "Main",
-            value: {},
+            value: [],
           },
-        },
+        ],
       };
       expect(createCustomType("id", "lama", false)).toEqual(expectedCustomType);
     });
