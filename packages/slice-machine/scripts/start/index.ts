6fea1b4b3c0a052b117392597917184a28b4e37f
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-03-24 17:41:33 +0100

    cleaning(core): fix start script

--- packages/slice-machine/scripts/start/index.ts
+++ packages/slice-machine/scripts/start/index.ts
@@ -1,5 +1,5 @@
 import path from "path";
-import { Framework } from "@slicemachine/core/build/node-utils";
+import * as NodeUtils from "@slicemachine/core/build/node-utils";
 
 // Don't remove those lines, they resolve aliases
 import { resolveAliases } from "../../lib/env/resolveAliases";
@@ -35,7 +35,7 @@ async function run(): Promise<void> {
   const smNodeModuleDirectory = path.resolve(__dirname, "../../..");
   const packageChangelog = await getPackageChangelog(smNodeModuleDirectory);
 
-  const framework = Framework.defineFramework({
+  const framework = NodeUtils.Framework.defineFramework({
     cwd,
     manifest: manifest.content || undefined,
   });
