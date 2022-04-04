442d073695f44d5a66070b0cda2eede9255543f6
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-04-04 19:43:32 +0200

    linting bin/start

--- packages/slice-machine/bin/start.js
+++ packages/slice-machine/bin/start.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment
 const start = require("../build/scripts/start/index.js");
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 start();
