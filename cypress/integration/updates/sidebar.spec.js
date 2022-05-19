91e1de4d7f9fec6449abf7a6c5dbc890a91bd2bf
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-05-19 14:30:07 +0200

    test(cypress_: increase timeout an flaky test

--- cypress/integration/updates/sidebar.spec.js
+++ cypress/integration/updates/sidebar.spec.js
@@ -76,7 +76,7 @@ describe("update notification", () => {
     });
 
     cy.visit("/");
-    cy.contains("Learn more").should("exist");
+    cy.contains("Learn more", {timeout: 20000}).should("exist");
     cy.get("[data-testid=the-red-dot]").should("not.exist");
   });
 
