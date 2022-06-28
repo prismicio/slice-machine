545b0b835e39b747a8301240f25d84726296d858
Lorenzo <lorenzos@theodo.co.uk>
2022-06-28 10:03:25 +0100

    refactor ErrorTooltip into tsx

--- packages/slice-machine/lib/builders/common/Zone/Card/components/ErrorTooltip.tsx
+++ packages/slice-machine/lib/builders/common/Zone/Card/components/ErrorTooltip.tsx
@@ -3,15 +3,17 @@ import { Fragment } from "react";
 import ReactTooltip from "react-tooltip";
 import { FaRegQuestionCircle } from "react-icons/fa";
 
-const ErrorTooltip = ({ error }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+interface ErrorTooltip {
+  error?: string;
+}
+
+const ErrorTooltip: React.FC<ErrorTooltip> = ({ error }) => {
   if (error) {
     return (
       <Fragment>
         <ReactTooltip type="light" multiline border borderColor={"tomato"} />
         <FaRegQuestionCircle
           color={"tomato"}
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
           data-tip={error}
           style={{
             position: "relative",
