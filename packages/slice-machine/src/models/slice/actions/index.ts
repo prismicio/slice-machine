97255f7a149d3edbe3d28ffd5b655ce540aeb840
Lorenzo <lorenzos@theodo.co.uk>
2022-07-04 15:33:39 +0100

    remove files migrated

--- packages/slice-machine/src/models/slice/actions/index.ts
+++
@@ -1,3 +0,0 @@
-export { ActionType } from "./ActionType";
-export { default as saveSlice } from "./save";
-export { default as pushSlice } from "./push";
--- packages/slice-machine/src/models/slice/variation/actions/index.ts
+++
@@ -1,5 +0,0 @@
-export { ActionType } from "./ActionType";
-
-export { updateWidgetMockConfig } from "./mocks";
-export { deleteWidgetMockConfig } from "./mocks";
-export { generateCustomScreenShot, generateScreenShot } from "./screenshot";
