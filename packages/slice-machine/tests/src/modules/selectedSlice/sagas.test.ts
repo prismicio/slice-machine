71b9905fa46416139a138467b54ffc804c2971e7
Andrew <chengoconnell@gmail.com>
2022-07-20 17:07:39 +0100

    test(): add for generateSliceScreenshotSaga

--- packages/slice-machine/tests/src/modules/selectedSlice/sagas.test.ts
+++ packages/slice-machine/tests/src/modules/selectedSlice/sagas.test.ts
@@ -1,11 +1,17 @@
 import { testSaga } from "redux-saga-test-plan";
-import { pushSliceSaga, saveSliceSaga } from "@src/modules/selectedSlice/sagas";
+import {
+  pushSliceSaga,
+  saveSliceSaga,
+  generateSliceScreenshotSaga,
+} from "@src/modules/selectedSlice/sagas";
 import { getSelectedSliceDummyData } from "./utils";
 import {
+  generateSliceScreenshotCreator,
   pushSliceCreator,
   saveSliceCreator,
 } from "../../../../src/modules/selectedSlice/actions";
 import {
+  generateSliceScreenshotApiClient,
   pushSliceApiClient,
   saveSliceApiClient,
 } from "../../../../src/apiClient";
@@ -109,6 +115,78 @@ describe("[Selected Slice sagas]", () => {
       saga.throw(new Error()).put(
         openToasterCreator({
           message: "Internal Error: Slice was not pushed",
+          type: ToasterType.ERROR,
+        })
+      );
+      saga.next().isDone();
+    });
+  });
+
+  describe("[generateSliceScreenshotSaga]", () => {
+    it("should call the api and dispatch the success action", () => {
+      const mockSetData = jest.fn();
+      const saga = testSaga(
+        generateSliceScreenshotSaga,
+        generateSliceScreenshotCreator.request({
+          _variationId: dummyModelVariationID,
+          component: dummySliceState.component,
+          setData: mockSetData,
+        })
+      );
+
+      saga
+        .next()
+        .call(
+          generateSliceScreenshotApiClient,
+          dummySliceState.component.model.name,
+          dummySliceState.component.from
+        );
+      const response = {
+        screenshots: {
+          dummyModelVariationID: {
+            path: "testScreenshotPath",
+            url: "testScreenshotUrl",
+          },
+        },
+      };
+
+      saga
+        .next({
+          status: 200,
+          data: response,
+        })
+        .put(
+          generateSliceScreenshotCreator.success({
+            screenshots: response.screenshots,
+            component: dummySliceState.component,
+          })
+        );
+
+      saga.next().isDone();
+      expect(mockSetData).toHaveBeenCalledWith({
+        done: true,
+        error: null,
+        loading: false,
+        imageLoading: false,
+        message: "Screenshots were saved to FileSystem",
+        status: 200,
+        warning: false,
+      });
+    });
+    it("should open a error toaster on internal error", () => {
+      const mockSetData = jest.fn();
+      const saga = testSaga(
+        generateSliceScreenshotSaga,
+        generateSliceScreenshotCreator.request({
+          _variationId: dummyModelVariationID,
+          component: dummySliceState.component,
+          setData: mockSetData,
+        })
+      ).next();
+
+      saga.throw(new Error()).put(
+        openToasterCreator({
+          message: "Internal Error: Screenshot not saved",
           type: ToasterType.ERROR,
         })
       );
