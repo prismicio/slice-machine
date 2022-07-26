0ee3823fff83f8a1886dcf109864488871dc617b
Andrew <chengoconnell@gmail.com>
2022-07-26 11:32:01 +0100

    feature(): dont fetch remote cts if local ct folder is empty

--- packages/slice-machine/server/src/api/custom-types/index.ts
+++ packages/slice-machine/server/src/api/custom-types/index.ts
@@ -1,12 +1,9 @@
 import { BackendEnvironment } from "@lib/models/common/Environment";
-import Files from "@lib/utils/files";
-import { CustomTypesPaths } from "@lib/models/paths";
 import {
   CustomTypes,
   CustomTypeSM,
 } from "@slicemachine/core/build/models/CustomType/index";
 import { CustomType } from "@prismicio/types-internal/lib/customtypes/CustomType";
-import * as IO from "../io";
 import { ClientError } from "@slicemachine/client";
 import { getLocalCustomTypes } from "@lib/utils/customTypes";
 
@@ -26,29 +23,13 @@ const fetchRemoteCustomTypes = async (
     });
 };
 
-const saveCustomType = (cts: ReadonlyArray<CustomTypeSM>, cwd: string) => {
-  for (const ct of cts) {
-    IO.CustomType.writeCustomType(
-      CustomTypesPaths(cwd).customType(ct.id).model(),
-      ct
-    );
-  }
-};
-
 export default async function handler(env: BackendEnvironment): Promise<{
   customTypes: ReadonlyArray<CustomTypeSM>;
   remoteCustomTypes: ReadonlyArray<CustomTypeSM>;
 }> {
   const { cwd } = env;
 
-  const pathToCustomTypes = CustomTypesPaths(cwd).value();
-  const folderExists = Files.exists(pathToCustomTypes);
-
   const { remoteCustomTypes } = await fetchRemoteCustomTypes(env);
-
-  if (!folderExists) {
-    saveCustomType(remoteCustomTypes, cwd);
-  }
 
   return {
     customTypes: getLocalCustomTypes(cwd),
