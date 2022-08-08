392d3a283804d2940f71329b70ecc53d5ddb365e
Andrew <chengoconnell@gmail.com>
2022-08-08 15:43:30 +0100

    feature(): update customtypestatus function

--- packages/slice-machine/server/src/api/custom-types/getCustomTypeStatus.ts
+++ packages/slice-machine/server/src/api/custom-types/getCustomTypeStatus.ts
@@ -1,14 +1,12 @@
 import { CustomTypeSM } from "@slicemachine/core/build/models/CustomType";
-import { CustomTypeStatus } from "@src/modules/selectedCustomType/types";
+import { CustomTypeStatus } from "../../../../src/modules/selectedCustomType/types";
 import equal from "fast-deep-equal";
 
 export const getCustomTypeStatus = (
-  localCustomType?: CustomTypeSM,
+  localCustomType: CustomTypeSM,
   remoteCustomType?: CustomTypeSM
 ): CustomTypeStatus => {
-  if (!localCustomType && !remoteCustomType) return CustomTypeStatus.Unknown;
-
-  if (!remoteCustomType) return CustomTypeStatus.New;
+  if (!localCustomType || !remoteCustomType) return CustomTypeStatus.New;
 
   if (!equal(localCustomType, remoteCustomType)) {
     return CustomTypeStatus.Modified;
