3e88958fd19a3a49ff4e7f2ad46acb63be255deb
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-09-14 14:25:38 +0200

    sm-ui(tacking): track clicks on videos when in an empty state

--- packages/slice-machine/components/EmptyState/index.tsx
+++ packages/slice-machine/components/EmptyState/index.tsx
@@ -24,95 +24,97 @@ const EmptyState: React.FunctionComponent<Props> = ({
   videoPublicIdUrl,
   framework,
   version,
-}) => (
-  <Flex sx={{ width: "80%", flexWrap: "wrap", justifyContent: "center" }}>
-    <Flex
-      sx={(theme) => ({
-        flex: 1,
-        alignItems: "center",
-        minWidth: "400px",
-        maxWidth: "70%",
-        border: `1px solid ${theme.colors?.grey02 as string}`,
-      })}
-    >
-      <Video
-        cloudName="dmtf1daqp"
-        controls
-        loop
-        style={{
-          maxWidth: "100%",
-          objectFit: "contain",
-        }}
-        publicId={videoPublicIdUrl}
-        onClick={() => {
-          void Tracking.get().trackClickOnVideoTutorials(
-            framework,
-            version,
-            videoPublicIdUrl
-          );
-        }}
-      />
-    </Flex>
+}) => {
+  return (
+    <Flex sx={{ width: "80%", flexWrap: "wrap", justifyContent: "center" }}>
+      <Flex
+        sx={(theme) => ({
+          flex: 1,
+          alignItems: "center",
+          minWidth: "400px",
+          maxWidth: "70%",
+          border: `1px solid ${theme.colors?.grey02 as string}`,
+        })}
+      >
+        <Video
+          cloudName="dmtf1daqp"
+          controls
+          loop
+          style={{
+            maxWidth: "100%",
+            objectFit: "contain",
+          }}
+          publicId={videoPublicIdUrl}
+          onPlay={() => {
+            void Tracking.get().trackClickOnVideoTutorials(
+              framework,
+              version,
+              videoPublicIdUrl
+            );
+          }}
+        />
+      </Flex>
 
-    <Flex
-      sx={(theme) => ({
-        flexDirection: "column",
-        border: `1px solid ${theme.colors?.grey02 as string}`,
-        flex: 1,
-        minWidth: "400px",
-        maxWidth: "70%",
-      })}
-    >
       <Flex
         sx={(theme) => ({
           flexDirection: "column",
-          p: 4,
-          borderBottom: `1px solid ${theme.colors?.grey02 as string}`,
+          border: `1px solid ${theme.colors?.grey02 as string}`,
+          flex: 1,
+          minWidth: "400px",
+          maxWidth: "70%",
         })}
       >
-        <Heading
-          as="h3"
-          variant={"heading"}
-          sx={{ fontSize: "16px", lineHeight: "24px", mb: 2 }}
+        <Flex
+          sx={(theme) => ({
+            flexDirection: "column",
+            p: 4,
+            borderBottom: `1px solid ${theme.colors?.grey02 as string}`,
+          })}
         >
-          {title}
-        </Heading>
-        <Text variant="xs" sx={{ lineHeight: "24px", fontSize: "13px" }}>
-          {documentationComponent}
-        </Text>
-      </Flex>
-      <Flex
-        sx={{
-          p: 4,
-          alignItems: "center",
-        }}
-      >
-        <Button
-          onClick={onCreateNew}
-          data-cy="empty-state-main-button"
+          <Heading
+            as="h3"
+            variant={"heading"}
+            sx={{ fontSize: "16px", lineHeight: "24px", mb: 2 }}
+          >
+            {title}
+          </Heading>
+          <Text variant="xs" sx={{ lineHeight: "24px", fontSize: "13px" }}>
+            {documentationComponent}
+          </Text>
+        </Flex>
+        <Flex
           sx={{
-            display: "flex",
-            justifyContent: "center",
+            p: 4,
             alignItems: "center",
-            flexShrink: 0,
-            mr: 4,
           }}
         >
-          {isLoading ? <Spinner color="#FFF" size={14} /> : buttonText}
-        </Button>
-        <Text
-          sx={{
-            fontSize: "12px",
-            color: "grey04",
-            maxWidth: "280px",
-          }}
-        >
-          It will be stored locally and you will be able to push it to your
-          repository
-        </Text>
+          <Button
+            onClick={onCreateNew}
+            data-cy="empty-state-main-button"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexShrink: 0,
+              mr: 4,
+            }}
+          >
+            {isLoading ? <Spinner color="#FFF" size={14} /> : buttonText}
+          </Button>
+          <Text
+            sx={{
+              fontSize: "12px",
+              color: "grey04",
+              maxWidth: "280px",
+            }}
+          >
+            It will be stored locally and you will be able to push it to your
+            repository
+          </Text>
+        </Flex>
       </Flex>
     </Flex>
-  </Flex>
-);
+  );
+};
 
 export default EmptyState;
