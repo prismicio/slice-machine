1dd5365123d9bf7a13080b265e511e8df6d29843
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-09-29 14:14:55 +0200

    refactor(ItemHeader): migrate to typescript

--- packages/slice-machine/components/ItemHeader/index.tsx
+++ packages/slice-machine/components/ItemHeader/index.tsx
@@ -1,27 +1,23 @@
+import React from "react";
 import { Flex, Text } from "theme-ui";
+import type { Theme } from "theme-ui";
 
-import SliceMachineIconButton from "@components/SliceMachineIconButton";
-import { FaBars } from "react-icons/fa";
-
-const ItemHeader = ({
-  text,
-  sliceFieldName,
-  theme,
-  WidgetIcon,
-  iconButtonProps = {},
-}) => (
+const ItemHeader: React.FC<{
+  text: React.ReactNode;
+  sliceFieldName: React.ReactNode;
+  theme: Theme;
+  WidgetIcon: React.ElementType;
+}> = ({ text, sliceFieldName, theme, WidgetIcon }) => (
   <Flex sx={{ alignItems: "center" }}>
     <WidgetIcon
       size={28}
       style={{
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-        color: theme.colors.primary,
+        color: theme.colors?.primary,
         marginRight: "8px",
         borderRadius: "4px",
         padding: "4px",
         border: "2px solid",
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-        borderColor: theme.colors.primary,
+        borderColor: theme.colors?.primary,
       }}
     />
     <Text
