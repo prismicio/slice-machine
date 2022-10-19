0b8b58c29763b562dbdc97deb1b55ed085d3293b
Michel CHAU <michel.chau@prismic.io>
2022-10-19 17:27:43 +0200

    Add repo as optional on cypress

--- cypress-setup.sh
+++ cypress-setup.sh
@@ -2,8 +2,8 @@
 # set -x
 if [[ -z "$EMAIL" || -z "$PASSWORD" || -z "$PRISMIC_URL" ]]; then
   echo "[ERROR] Please set EMAIL PASSWORD PRISMIC_URL environments variables"
-  echo 'EMAIL="email@example.com" PASSWORD="guest" PRISMIC_URL="https://prismic.io" npm run test:e2e:dev'
-  echo $EMAIL $PASSWORD $PRISMIC_URL
+  echo 'EMAIL="email@example.com" PASSWORD="guest" PRISMIC_URL="https://prismic.io REPOSITORY="my-repo" npm run test:e2e:dev'
+  echo $EMAIL $PASSWORD $PRISMIC_URL $REPOSITORY
   exit 1
 fi 
 _DATE=$(date +'%y%m%d%H%M%S')
