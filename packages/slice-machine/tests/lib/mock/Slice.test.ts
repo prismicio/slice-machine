809be736dd4d9d2836d49bc1232465c26bbee50b
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-10-25 10:58:27 +0200

    change how shared slice mocks are generated

--- packages/slice-machine/tests/lib/mock/Slice.test.ts
+++ packages/slice-machine/tests/lib/mock/Slice.test.ts
@@ -1,9 +1,108 @@
 import "@testing-library/jest-dom";
 import { WidgetTypes } from "@prismicio/types-internal/lib/customtypes/widgets";
 import { SlicesTypes } from "@prismicio/types-internal/lib/customtypes/widgets/slices";
-import { SliceSM, SliceMock } from "@slicemachine/core/build/models";
+import { SliceSM, Slices } from "@slicemachine/core/build/models";
 import { isRight } from "fp-ts/lib/Either";
 import MockSlice from "../../../lib/mock/Slice";
+import * as t from "io-ts";
+import allFieldSliceModel from "../../../tests/__mocks__/sliceModel";
+
+import {
+  GroupItemContentType,
+  EmptyContent,
+  UIDContent,
+  EmptyContentType,
+  UIDContentType,
+} from "@prismicio/types-internal/lib/documents/widgets";
+import {
+  SeparatorContent,
+  BooleanContent,
+  IntegrationFieldsContent,
+  StructuredTextContent,
+  ImageContent,
+  GeoPointContent,
+  EmbedContent,
+  SeparatorContentType,
+  StructuredTextContentType,
+} from "@prismicio/types-internal/lib/documents/widgets/nestable";
+import { LinkContent } from "@prismicio/types-internal/lib/documents/widgets/nestable/Link";
+import { SharedSliceContentType } from "@prismicio/types-internal/lib/documents/widgets/slices";
+import { FieldContentType } from "@prismicio/types-internal/lib/documents/widgets/nestable/FieldContent";
+import { LinkContentType } from "@prismicio/types-internal/lib/documents/widgets/nestable/Link/LinkContent";
+import { BooleanContentType } from "@prismicio/types-internal/lib/documents/widgets/nestable/BooleanContent";
+
+const SeparatorContentC: t.Type<SeparatorContent> = t.type({
+  __TYPE__: t.literal(SeparatorContentType),
+});
+
+const StructuredTextContentC = t.type({
+  __TYPE__: t.literal(StructuredTextContentType),
+  value: StructuredTextContent,
+});
+
+const FieldTypes: Record<string, null> = [
+  "Text",
+  "Date",
+  "Timestamp",
+  "Color",
+  "Number",
+  "Range",
+  "Select",
+].reduce((acc, curr) => {
+  return { ...acc, [curr]: null };
+}, {}); // this causes some issues
+
+const FieldContentC = t.type({
+  __TYPE__: t.literal(FieldContentType),
+  type: t.keyof(FieldTypes),
+  value: t.string,
+});
+
+const EmptyContentC: t.Type<EmptyContent> = t.type({
+  __TYPE__: t.literal(EmptyContentType),
+  type: t.string,
+});
+
+const SimpleWidgetContent /*: t.Type<SimpleWidgetContentT>*/ = t.union([
+  IntegrationFieldsContent,
+  StructuredTextContentC,
+  ImageContent,
+  t.type({
+    __TYPE__: t.literal(GroupItemContentType),
+    value: GeoPointContent,
+  }),
+  GeoPointContent, // weird that geo has now __TYPE__
+  EmbedContent,
+  t.type({
+    __TYPE__: t.literal(LinkContentType),
+    value: LinkContent,
+  }),
+  t.type({
+    __TYPE__: t.literal(UIDContentType),
+    value: UIDContent,
+  }),
+  SeparatorContentC,
+  FieldContentC,
+  t.type({
+    __TYPE__: t.literal(BooleanContentType),
+    value: BooleanContent,
+  }),
+  EmptyContentC,
+]);
+
+const GroupItemContent /*: t.Type<GroupItemContentT> */ = t.type({
+  __TYPE__: t.literal(GroupItemContentType),
+  value: t.array(t.tuple([t.string, SimpleWidgetContent])),
+});
+
+const SharedSliceContentItem /*: t.Type<SharedSliceContentT> */ = t.type({
+  variation: t.string,
+  primary: t.record(t.string, SimpleWidgetContent),
+  items: t.array(GroupItemContent),
+  __TYPE__: t.literal(SharedSliceContentType),
+});
+
+const SharedSliceContent = t.array(SharedSliceContentItem);
 
 jest.mock("lorem-ipsum", () => {
   return {
@@ -16,18 +115,58 @@ jest.mock("lorem-ipsum", () => {
   };
 });
 
+jest.mock("@prismicio/mocks/lib/generators/utils/slug", () => {
+  return jest.fn().mockReturnValue("Woo");
+});
+
 describe("MockSlice", () => {
+  test("parse primary", () => {
+    const text = {
+      title: {
+        __TYPE__: "StructuredTextContent",
+        value: [{ type: "heading1", content: { text: "Woo" } }],
+      },
+    };
+
+    const got = StructuredTextContentC.decode(text.title);
+    expect(isRight(got)).toBeTruthy();
+
+    const link = {
+      "link-2": {
+        __TYPE__: "LinkContent",
+        value: { url: "http://twitter.com", __TYPE__: "ExternalLink" },
+      },
+    };
+    const linkR = LinkContent.decode(link["link-2"].value);
+    expect(isRight(linkR)).toBeTruthy();
+
+    const geo = {
+      key: {
+        // __TYPE__: GeoPointContentType,
+        position: { lat: 48.8583736, lng: 2.2922926 },
+      },
+    };
+    const geoR = GeoPointContent.decode(geo.key);
+
+    expect(isRight(geoR)).toBeTruthy();
+  });
+
   test("when creating a slice it should return the default mock", () => {
     const wanted = [
       {
+        __TYPE__: "SharedSliceContent",
         variation: "default",
-        slice_type: "some_slice",
-        items: [],
         primary: {
-          title: [{ type: "heading1", text: "RANDOM_VALUE", spans: [] }],
-          description: [{ type: "paragraph", text: "Some text.", spans: [] }],
+          title: {
+            __TYPE__: "StructuredTextContent",
+            value: [{ type: "heading1", content: { text: "Woo" } }],
+          },
+          description: {
+            __TYPE__: "StructuredTextContent",
+            value: [{ type: "paragraph", content: { text: "Some text." } }],
+          },
         },
-        version: "sktwi1xtmkfgx8626",
+        items: [],
       },
     ];
 
@@ -75,32 +214,42 @@ describe("MockSlice", () => {
 
     const result = MockSlice(model, mockConfig);
 
-    // override the randomly generated value since we cannot mock it
-    // @ts-expect-error `result` is typed as unknown[]
-    result[0].primary.title[0].text = "RANDOM_VALUE";
-
     expect(result).toEqual(wanted);
-    expect(isRight(SliceMock.decode(result))).toBeTruthy();
+    const decoded = SharedSliceContent.decode(result);
+    expect(isRight(decoded)).toBeTruthy();
     // needs to be readable by core/mocks/models SliceMock
   });
 
   test("when updating a mock with config", () => {
     const wanted = [
       {
+        __TYPE__: "SharedSliceContent",
         variation: "default",
-        slice_type: "some_slice",
-        items: [{}],
         primary: {
-          title: [{ type: "heading1", text: "RANDOM_VALUE", spans: [] }],
-          description: [{ type: "paragraph", text: "Some text.", spans: [] }],
+          title: {
+            __TYPE__: "StructuredTextContent",
+            value: [{ type: "heading1", content: { text: "Woo" } }],
+          },
+          description: {
+            __TYPE__: "StructuredTextContent",
+            value: [{ type: "paragraph", content: { text: "Some text." } }],
+          },
           image: {
-            dimensions: { width: 900, height: 500 },
-            alt: null,
-            copyright: null,
+            __TYPE__: "ImageContent",
             url: "https://images.unsplash.com/photo-1555169062-013468b47731",
+            origin: {
+              id: "main",
+              url: "https://images.unsplash.com/photo-1555169062-013468b47731",
+              width: 900,
+              height: 500,
+            },
+            width: 900,
+            height: 500,
+            edit: { zoom: 1, crop: { x: 0, y: 0 }, background: "transparent" },
+            thumbnails: {},
           },
         },
-        version: "sktwi1xtmkfgx8626",
+        items: [{ __TYPE__: "GroupItemContent", value: [] }],
       },
     ];
 
@@ -164,12 +313,17 @@ describe("MockSlice", () => {
     };
 
     const result = MockSlice(model, mockConfig);
-
-    // override the randomly generated value since we cannot mock it
-    // @ts-expect-error `result` is typed as unknown[]
-    result[0].primary.title[0].text = "RANDOM_VALUE";
-
     expect(result).toEqual(wanted);
-    expect(isRight(SliceMock.decode(result))).toBeTruthy();
+    const decoded = SharedSliceContent.decode(result);
+    expect(isRight(decoded)).toBeTruthy();
+  });
+
+  test("allFieldSliceModel", () => {
+    const model = Slices.toSM({ ...allFieldSliceModel });
+    const mock = MockSlice(model, {});
+    console.dir({ mock }, { depth: null });
+    const result = SharedSliceContent.decode(mock);
+    console.dir(result, { depth: null });
+    expect(isRight(result)).toBeTruthy();
   });
 });
