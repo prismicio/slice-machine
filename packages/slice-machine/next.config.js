79f741a08c38e888f38a083c7c5a460991d68a4d
Mathieu Dähne <mathieud@theodo.co.uk>
2022-11-22 10:59:22 +0000

    fix sentry config

--- packages/slice-machine/next.config.js
+++ packages/slice-machine/next.config.js
@@ -24,38 +24,75 @@ const RELEASE_NUMBER = pkg.content.version;
 const isStableVersion =
   /^\d+\.\d+\.\d+$/.test(RELEASE_NUMBER) && semver.lte("0.1.0", RELEASE_NUMBER);
 
-const nextPlugins = [
+const nextConfig = {
+  distDir: "./build/client",
+  generateBuildId: () => {
+    // This controls the release number for Sentry
+    return RELEASE_NUMBER;
+  },
+  sentry: {
+    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+    // for client-side builds. (This will be the default starting in
+    // `@sentry/nextjs` version 8.0.0.) See
+    // https://webpack.js.org/configuration/devtool/ and
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+    // for more information.
+    hideSourceMaps: true,
+
+    // This prevents a crash when doing the static export
+    // There may be a way to completely disable the server part in which case this would become redundant
+    // https://github.com/getsentry/sentry-javascript/issues/6088#issuecomment-1296797294
+    autoInstrumentServerFunctions: false,
+
+    // We need all the sourcemap uploaded to Sentry to get the complete trace
+    // Not just the "pages"
+    // Probably because of the static export
+    widenClientFileUpload: true,
+  },
+  publicRuntimeConfig: {
+    sentryEnvironment: isStableVersion ? process.env.NODE_ENV : "alpha",
+  },
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+let exportedConfig = withPlugins(
   [
-    withCustomBabelConfigFile,
-    {
-      babelConfigFile: path.resolve("./babel.next.config.js"),
-      webpack5: false,
-      env: {
-        // overridden by start script
-        CWD: path.resolve("./tests/project"),
-      },
-      webpack: (config, { isServer }) => {
-        if (!isServer) {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-          config.node = { fs: "empty", child_process: "empty" };
-        }
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        config.module.rules.push({
-          test: /\.svg$/,
-          issuer: {
-            test: /\.(js|ts)x?$/,
-          },
-          use: ["@svgr/webpack"],
-        });
+    [
+      withCustomBabelConfigFile,
+      {
+        babelConfigFile: path.resolve("./babel.next.config.js"),
+        webpack5: false,
+        env: {
+          // overridden by start script
+          CWD: path.resolve("./tests/project"),
+        },
+        webpack: (config, { isServer }) => {
+          if (!isServer) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            config.node = { fs: "empty", child_process: "empty" };
+          }
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+          config.module.rules.push({
+            test: /\.svg$/,
+            issuer: {
+              test: /\.(js|ts)x?$/,
+            },
+            use: ["@svgr/webpack"],
+          });
 
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        return config;
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+          return config;
+        },
       },
-    },
+    ],
+    withVideos,
   ],
-  withVideos,
-];
+  nextConfig
+);
 
+// The Sentry plugin does not work with next-compose-plugins
+// The main config is not passed / handled
+// It must be initialised separately
 if (process.env.NODE_ENV !== "development") {
   if (!process.env.SENTRY_PROJECT || !process.env.SENTRY_AUTH_TOKEN) {
     console.warn("⚠️ Creating a production build with no Sentry config");
@@ -80,33 +117,15 @@ if (process.env.NODE_ENV !== "development") {
 
       // For all available options, see:
       // https://github.com/getsentry/sentry-webpack-plugin#options.
+
+      // The Sentry webpack plugin always ignores some files when uploading sourcemaps
+      // We actually need them (because of the static export?) to get the complete trace in Sentry
+      ignore: [],
     };
-    nextPlugins.push([withSentryConfig, sentryWebpackPluginOptions]);
+
+    exportedConfig = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
   }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-module.exports = withPlugins(nextPlugins, {
-  distDir: "./build/client",
-  generateBuildId: () => {
-    // This controls the release number for Sentry
-    return RELEASE_NUMBER;
-  },
-  sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
-    hideSourceMaps: true,
-
-    // This prevents a crash when doing the static export
-    // There may be a way to completely disable the server part in which case this would become redundant
-    // https://github.com/getsentry/sentry-javascript/issues/6088#issuecomment-1296797294
-    autoInstrumentServerFunctions: false,
-  },
-  publicRuntimeConfig: {
-    sentryEnvironment: isStableVersion ? process.env.NODE_ENV : "alpha",
-  },
-});
+module.exports = exportedConfig;
