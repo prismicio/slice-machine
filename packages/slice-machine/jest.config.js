d0feef596af3bacd12692386ffb8f371e5efce32
Baptiste Morelle <bapmrl@users.noreply.github.com>
2022-11-23 11:02:55 +0100

    use babel-jest

--- packages/slice-machine/jest.config.js
+++ packages/slice-machine/jest.config.js
@@ -1,16 +1,4 @@
-const nextJest = require("next/jest");
-
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
-});
-
-// Add any custom config to be passed to Jest
-const customJestConfig = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ["node_modules", "<rootDir>/"],
+module.exports = {
   clearMocks: true,
   collectCoverage: true,
   globals: { appRoot: ".." },
@@ -22,8 +10,18 @@ const customJestConfig = {
     "^@src/(.*)$": "<rootDir>/src/$1",
     "^@tests/(.*)$": "<rootDir>/tests/$1",
     "^@utils/(.*)$": "<rootDir>/lib/utils/$1",
+    "^components/(.*)$": "<rootDir>/components/$1",
+    "^lib/(.*)$": "<rootDir>/lib/$1",
+    "^tests/(.*)$": "<rootDir>/tests/$1",
+  },
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": [
+      "babel-jest",
+      {
+        presets: ["next/babel"],
+        plugins: ["@babel/plugin-proposal-private-methods"],
+      },
+    ],
   },
 };
-
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
