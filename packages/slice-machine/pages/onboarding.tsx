b71ec46b7b0b1ba9802827bb9a08682a657e37f1
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-11-29 13:49:34 +0100

    lint: onboarding fix linter error

--- packages/slice-machine/pages/onboarding.tsx
+++ packages/slice-machine/pages/onboarding.tsx
@@ -202,7 +202,7 @@ function idFromStep(
   }
 }
 
-function handleTracking(props: { step: number; maxSteps: number }): void {
+function useTracking(props: { step: number; maxSteps: number }): void {
   const state = useRef(props);
 
   useEffect(() => {
@@ -248,7 +248,7 @@ export default function Onboarding(): JSX.Element {
     step: 0,
   });
 
-  handleTracking({
+  useTracking({
     ...state,
     maxSteps: STEPS.length,
   });
