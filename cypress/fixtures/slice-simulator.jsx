0311145a8f4222b20b205375c21201624147ba0c
MarcMcIntosh <marcmcintosh1987@gmail.com>
2022-12-05 15:35:25 +0100

    test-e2e: add test for saving mocks from the editor

---
+++ cypress/fixtures/slice-simulator.jsx
@@ -0,0 +1,12 @@
+import { SliceSimulator } from "@prismicio/slice-simulator-react";
+import { SliceZone } from "@prismicio/react";
+
+import { components } from "../slices";
+
+const SliceSimulatorPage = () => (<SliceSimulator
+  // The "sliceZone" prop should be a function receiving slices and rendering them using your "SliceZone" component.
+  sliceZone={(props) => <SliceZone {...props} components={components} />}
+  state={{}}
+/>);
+
+export default SliceSimulatorPage;
