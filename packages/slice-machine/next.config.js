6505a53aad350fe0bb3b2c5088b00b617fd10ebb
Mathieu Dähne <mathieud@theodo.co.uk>
2022-12-12 18:04:30 +0100

    fix after merge

--- packages/slice-machine/next.config.js
+++ packages/slice-machine/next.config.js
@@ -1,10 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
 // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
 const { withSentryConfig } = require("@sentry/nextjs");
 // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
 const NodeUtils = require("@slicemachine/core/build/node-utils");
 // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
 const semver = require("semver");
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+
 const pkg = NodeUtils.retrieveJsonPackage(path.resolve(__dirname));
 
 /**
@@ -22,25 +24,6 @@ const isStableVersion =
 let nextConfig = {
   distDir: "build/client",
   swcMinify: true,
-  sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
-    hideSourceMaps: true,
-
-    // This prevents a crash when doing the static export
-    // There may be a way to completely disable the server part in which case this would become redundant
-    // https://github.com/getsentry/sentry-javascript/issues/6088#issuecomment-1296797294
-    autoInstrumentServerFunctions: false,
-
-    // We need all the sourcemap uploaded to Sentry to get the complete trace
-    // Not just the "pages"
-    // Probably because of the static export
-    widenClientFileUpload: true,
-  },
   publicRuntimeConfig: {
     sentryEnvironment: isStableVersion ? process.env.NODE_ENV : "alpha",
   },
@@ -74,7 +57,31 @@ if (process.env.NODE_ENV !== "development") {
       configFile: "sentry-next.properties",
     };
 
-    nextConfig = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+    nextConfig = withSentryConfig(
+      {
+        ...nextConfig,
+        sentry: {
+          // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+          // for client-side builds. (This will be the default starting in
+          // `@sentry/nextjs` version 8.0.0.) See
+          // https://webpack.js.org/configuration/devtool/ and
+          // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+          // for more information.
+          hideSourceMaps: true,
+
+          // This prevents a crash when doing the static export
+          // There may be a way to completely disable the server part in which case this would become redundant
+          // https://github.com/getsentry/sentry-javascript/issues/6088#issuecomment-1296797294
+          autoInstrumentServerFunctions: false,
+
+          // We need all the sourcemap uploaded to Sentry to get the complete trace
+          // Not just the "pages"
+          // Probably because of the static export
+          widenClientFileUpload: true,
+        },
+      },
+      sentryWebpackPluginOptions
+    );
   }
 }
 
