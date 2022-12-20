8dd351c70fb528adf94a30a1e6f44dad73cd15b5
Baptiste Morelle <bapmrl@users.noreply.github.com>
2022-12-20 18:33:16 +0100

    refactor: call useElementSize in Header

--- packages/slice-machine/src/hooks/useElementSize.ts
+++ packages/slice-machine/src/hooks/useElementSize.ts
@@ -5,17 +5,17 @@ import {
   useRef,
 } from "react";
 
-export function useElementSize(
-  callback: (size: ResizeObserverSize) => void,
+export function useElementSize<E extends Element>(
+  callback: (size: ResizeObserverSize, element: E) => void,
   deps: DependencyList
-): RefCallback<Element> {
+): RefCallback<E> {
   const resizeObserverRef = useRef<ResizeObserver>();
-  return useCallback((element: Element | null) => {
+  return useCallback((element: E | null) => {
     resizeObserverRef.current?.disconnect();
     if (element === null) return;
     resizeObserverRef.current = new ResizeObserver(([entry]) => {
       const { height, width } = entry.contentRect;
-      callback({ blockSize: height, inlineSize: width });
+      callback({ blockSize: height, inlineSize: width }, element);
     });
     resizeObserverRef.current.observe(element);
   }, deps);
