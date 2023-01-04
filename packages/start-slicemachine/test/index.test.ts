8442a8c86f77be087f2018391d9a245ab670f8e6
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-01-04 11:54:20 -1000

    test: add dummy test for `start-slicemachine`

---
+++ packages/start-slicemachine/test/index.test.ts
@@ -0,0 +1,8 @@
+import { it, expect } from "vitest";
+
+import * as lib from "../src";
+
+// TODO: Dummy test, meant to be removed when real tests come in
+it("exports something", () => {
+	expect(lib).toBeTruthy();
+});
