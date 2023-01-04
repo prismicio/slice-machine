f1c78c356be9728950627bbd12760f4aeb8bd0aa
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-01-03 16:21:11 -1000

    test: update tests to reflect latest changes

--- packages/manager/test/SliceMachineManager-simulator-readSliceSimulatorSetupSteps.test.ts
+++ packages/manager/test/SliceMachineManager-simulator-readSliceSimulatorSetupSteps.test.ts
@@ -17,28 +17,32 @@ it("returns Slice Simulator set up steps with validation status", async () => {
 				return [
 					{
 						title: "valid",
+						description: "valid desc",
 						body: "foo",
 						validate: async () => void 0,
 					},
 					{
 						title: "invalid-one-error",
+						description: "invalid-one-error desc",
 						body: "bar",
 						validate: async () => {
-							return { type: "Error", title: "title", message: "message" };
+							return { title: "title", message: "message" };
 						},
 					},
 					{
 						title: "invalid-multiple-errors",
+						description: "invalid-multiple-errors desc",
 						body: "baz",
 						validate: async () => {
 							return [
-								{ type: "Error", title: "title1", message: "message1" },
-								{ type: "Error", title: "title2", message: "message2" },
+								{ title: "title1", message: "message1" },
+								{ title: "title2", message: "message2" },
 							];
 						},
 					},
 					{
 						title: "no-validator",
+						description: "no-validator desc",
 						body: "qux",
 					},
 				];
@@ -59,17 +63,18 @@ it("returns Slice Simulator set up steps with validation status", async () => {
 		steps: [
 			{
 				title: "valid",
+				description: "valid desc",
 				body: "<p>foo</p>",
 				isComplete: true,
 				validationMessages: [],
 			},
 			{
 				title: "invalid-one-error",
+				description: "invalid-one-error desc",
 				body: "<p>bar</p>",
 				isComplete: false,
 				validationMessages: [
 					{
-						type: "Error",
 						title: "title",
 						message: "<p>message</p>",
 					},
@@ -77,16 +82,15 @@ it("returns Slice Simulator set up steps with validation status", async () => {
 			},
 			{
 				title: "invalid-multiple-errors",
+				description: "invalid-multiple-errors desc",
 				body: "<p>baz</p>",
 				isComplete: false,
 				validationMessages: [
 					{
-						type: "Error",
 						title: "title1",
 						message: "<p>message1</p>",
 					},
 					{
-						type: "Error",
 						title: "title2",
 						message: "<p>message2</p>",
 					},
@@ -94,7 +98,57 @@ it("returns Slice Simulator set up steps with validation status", async () => {
 			},
 			{
 				title: "no-validator",
+				description: "no-validator desc",
 				body: "<p>qux</p>",
+				isComplete: undefined,
+				validationMessages: [],
+			},
+		],
+		errors: [],
+	});
+});
+
+it("supports optional descriptions", async () => {
+	const adapter = createTestPlugin({
+		setup: ({ hook }) => {
+			hook("slice-simulator:setup:read", () => {
+				return [
+					{
+						title: "with description",
+						description: "with description desc",
+						body: "foo",
+					},
+					{
+						title: "without description",
+						body: "bar",
+					},
+				];
+			});
+		},
+	});
+	const cwd = await createTestProject({ adapter });
+	const manager = createSliceMachineManager({
+		nativePlugins: { [adapter.meta.name]: adapter },
+		cwd,
+	});
+
+	await manager.plugins.initPlugins();
+
+	const res = await manager.simulator.readSliceSimulatorSetupSteps();
+
+	expect(res).toStrictEqual({
+		steps: [
+			{
+				title: "with description",
+				description: "with description desc",
+				body: "<p>foo</p>",
+				isComplete: undefined,
+				validationMessages: [],
+			},
+			{
+				title: "without description",
+				description: undefined,
+				body: "<p>bar</p>",
 				isComplete: undefined,
 				validationMessages: [],
 			},
@@ -110,6 +164,7 @@ it("supports Markdown with code syntax highlighting in a step's body", async () 
 				return [
 					{
 						title: "markdown-test",
+						description: "markdown-test desc",
 						body: `
 # Heading 1
 ## Heading 2
@@ -138,6 +193,7 @@ const foo = "bar";
 		steps: [
 			{
 				title: "markdown-test",
+				description: "markdown-test desc",
 				body: `
 <h1>Heading 1</h1>
 <h2>Heading 2</h2>
@@ -160,10 +216,10 @@ it("supports Markdown with code syntax highlighting in a step's body", async () 
 				return [
 					{
 						title: "markdown-test",
+						description: "markdown-test desc",
 						body: "body",
 						validate: () => {
 							return {
-								type: "Error",
 								title: "invalid",
 								message: `
 # Heading 1
@@ -195,11 +251,11 @@ const foo = "bar";
 		steps: [
 			{
 				title: "markdown-test",
+				description: "markdown-test desc",
 				body: "<p>body</p>",
 				isComplete: false,
 				validationMessages: [
 					{
-						type: "Error",
 						title: "invalid",
 						message: `
 <h1>Heading 1</h1>
