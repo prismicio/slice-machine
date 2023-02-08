5476fd87164ce085ba0a95d99cd878fd75a1d8c3
Mathieu Dähne <mathieud@theodo.co.uk>
2023-02-08 16:19:12 +0000

    stop on error and send to Sentry

--- packages/slice-machine/server/src/api/push-changes.ts
+++ packages/slice-machine/server/src/api/push-changes.ts
@@ -37,6 +37,7 @@ import { PushChangesPayload } from "../../../lib/models/common/TransactionalPush
 import { purge } from "./services/uploadScreenshotClient";
 import { uploadScreenshots as uploadScreenshotsClient } from "./services/sliceService";
 import { compareScreenshots } from "../../../lib/models/common/ModelStatus/compareSliceModels";
+import * as Sentry from "@sentry/node";
 
 type TransactionalPushBody = {
   body: PushChangesPayload;
@@ -49,7 +50,6 @@ export default async function handler({
 }: TransactionalPushBody): Promise<
   ApiResult<{ status: number; body: Limit | null }>
 > {
-  debugger;
   const { cwd, client, manifest } = env;
 
   if (!manifest.libraries)
@@ -75,8 +75,6 @@ export default async function handler({
     cwd,
     manifest.libraries
   );
-
-  debugger;
 
   /* -- Assemble the models together and compute their statuses -- */
   const slicesModels: ReadonlyArray<LocalOrRemoteSlice> =
@@ -112,133 +110,130 @@ export default async function handler({
     };
   };
 
-  const newScreenshots = await Promise.allSettled(
-    slicesWithStatus.map(async (m) => {
-      switch (m.status) {
-        case ModelStatus.New: {
-          const payload = Slices.fromSM(await uploadAndUpdate(m.model.local));
-          return {
-            type: ChangeTypes.SLICE_INSERT,
-            id: payload.id,
-            payload,
-          };
-        }
-        case ModelStatus.Deleted: {
-          const { err: purgeError } = await purge(env, getModelId(m.model)); // throw this?
-          if (purgeError) throw purgeError;
-          return {
-            type: ChangeTypes.SLICE_DELETE,
-            id: m.model.remote.id,
-            payload: { id: m.model.remote.id },
-          };
-        }
-        case ModelStatus.Modified: {
-          debugger;
-          let sliceModel = m.model.local;
-          if (!compareScreenshots(m.model.remote, m.model.localScreenshots)) {
-            const { err: purgeError } = await purge(env, getModelId(m.model)); // throw this?
-            if (purgeError) throw purgeError;
-            sliceModel = await uploadAndUpdate(m.model.local);
-          } else {
-            sliceModel = {
-              ...sliceModel,
-              variations: sliceModel.variations.map((v) => ({
-                ...v,
-                imageUrl: m.model.remote.variations.find((vv) => vv.id === v.id)
-                  ?.imageUrl,
-              })),
+  try {
+    const newScreenshots = await Promise.all(
+      slicesWithStatus.map(async (m) => {
+        switch (m.status) {
+          case ModelStatus.New: {
+            const payload = Slices.fromSM(await uploadAndUpdate(m.model.local));
+            return {
+              type: ChangeTypes.SLICE_INSERT,
+              id: payload.id,
+              payload,
             };
           }
-          console.log(sliceModel);
-          const payload = Slices.fromSM(sliceModel);
-          return {
-            type: ChangeTypes.SLICE_UPDATE,
-            id: payload.id,
-            payload,
-          };
+          case ModelStatus.Deleted: {
+            const { err: purgeError } = await purge(env, getModelId(m.model)); // throw this?
+            if (purgeError) throw purgeError;
+            return {
+              type: ChangeTypes.SLICE_DELETE,
+              id: m.model.remote.id,
+              payload: { id: m.model.remote.id },
+            };
+          }
+          case ModelStatus.Modified: {
+            let sliceModel = m.model.local;
+            if (!compareScreenshots(m.model.remote, m.model.localScreenshots)) {
+              const { err: purgeError } = await purge(env, getModelId(m.model)); // throw this?
+              if (purgeError) throw purgeError;
+              sliceModel = await uploadAndUpdate(m.model.local);
+            } else {
+              sliceModel = {
+                ...sliceModel,
+                variations: sliceModel.variations.map((v) => ({
+                  ...v,
+                  imageUrl: m.model.remote.variations.find(
+                    (vv) => vv.id === v.id
+                  )?.imageUrl,
+                })),
+              };
+            }
+            const payload = Slices.fromSM(sliceModel);
+            return {
+              type: ChangeTypes.SLICE_UPDATE,
+              id: payload.id,
+              payload,
+            };
+          }
+          default:
+            return undefined;
         }
-        default:
-          return undefined;
-      }
-    })
-  );
+      })
+    );
 
-  const isFulfilled = <T>(
-    promiseResult: PromiseSettledResult<T>
-  ): promiseResult is PromiseFulfilledResult<T> =>
-    promiseResult.status === "fulfilled";
-
-  const sliceChanges = newScreenshots
-    .filter(isFulfilled)
-    .map((r) => r.value)
-    .filter(
+    const sliceChanges = newScreenshots.filter(
       (m): m is SliceInsertChange | SliceUpdateChange | SliceDeleteChange =>
         m !== undefined
     );
 
-  const customTypeChanges = customTypeModels.reduce(
-    (
-      acc: (
-        | CustomTypeInsertChange
-        | CustomTypeUpdateChange
-        | CustomTypeDeleteChange
-      )[],
-      customType: LocalOrRemoteCustomType
-    ) => {
-      // assessing the user is connected if we went that far in the processing
-      const statusResult = computeModelStatus(customType, true);
+    const customTypeChanges = customTypeModels.reduce(
+      (
+        acc: (
+          | CustomTypeInsertChange
+          | CustomTypeUpdateChange
+          | CustomTypeDeleteChange
+        )[],
+        customType: LocalOrRemoteCustomType
+      ) => {
+        // assessing the user is connected if we went that far in the processing
+        const statusResult = computeModelStatus(customType, true);
 
-      switch (statusResult.status) {
-        case ModelStatus.New: {
-          const payload = CustomTypes.fromSM(statusResult.model.local);
-          const customTypeInsert: CustomTypeInsertChange = {
-            type: ChangeTypes.CUSTOM_TYPE_INSERT,
-            id: payload.id,
-            payload,
-          };
-          return [...acc, customTypeInsert];
+        switch (statusResult.status) {
+          case ModelStatus.New: {
+            const payload = CustomTypes.fromSM(statusResult.model.local);
+            const customTypeInsert: CustomTypeInsertChange = {
+              type: ChangeTypes.CUSTOM_TYPE_INSERT,
+              id: payload.id,
+              payload,
+            };
+            return [...acc, customTypeInsert];
+          }
+
+          case ModelStatus.Modified: {
+            const payload = CustomTypes.fromSM(statusResult.model.local);
+            const customTypeUpdate: CustomTypeUpdateChange = {
+              type: ChangeTypes.CUSTOM_TYPE_UPDATE,
+              id: payload.id,
+              payload,
+            };
+            return [...acc, customTypeUpdate];
+          }
+
+          case ModelStatus.Deleted: {
+            const payload = CustomTypes.fromSM(statusResult.model.remote);
+            const customTypeDelete: CustomTypeDeleteChange = {
+              type: ChangeTypes.CUSTOM_TYPE_DELETE,
+              id: payload.id,
+              payload: { id: payload.id },
+            };
+            return [...acc, customTypeDelete];
+          }
+
+          default: {
+            return acc;
+          }
         }
+      },
+      []
+    );
 
-        case ModelStatus.Modified: {
-          const payload = CustomTypes.fromSM(statusResult.model.local);
-          const customTypeUpdate: CustomTypeUpdateChange = {
-            type: ChangeTypes.CUSTOM_TYPE_UPDATE,
-            id: payload.id,
-            payload,
-          };
-          return [...acc, customTypeUpdate];
-        }
+    const newBody: BulkBody = {
+      confirmDeleteDocuments: body.confirmDeleteDocuments,
+      changes: [...sliceChanges, ...customTypeChanges],
+    };
 
-        case ModelStatus.Deleted: {
-          const payload = CustomTypes.fromSM(statusResult.model.remote);
-          const customTypeDelete: CustomTypeDeleteChange = {
-            type: ChangeTypes.CUSTOM_TYPE_DELETE,
-            id: payload.id,
-            payload: { id: payload.id },
-          };
-          return [...acc, customTypeDelete];
-        }
+    return client
+      .bulk(newBody)
+      .then((potentialLimit: Limit | null) => ({
+        status: 200,
+        body: potentialLimit,
+      }))
+      .catch((error: ClientError) => onError(error.message, error.status));
+  } catch (e) {
+    console.error("An error happened while pushing your changes");
+    console.error(e);
+    Sentry.captureException(e);
 
-        default: {
-          return acc;
-        }
-      }
-    },
-    []
-  );
-
-  const newBody: BulkBody = {
-    confirmDeleteDocuments: body.confirmDeleteDocuments,
-    changes: [...sliceChanges, ...customTypeChanges],
-  };
-
-  console.log(sliceChanges);
-
-  return client
-    .bulk(newBody)
-    .then((potentialLimit: Limit | null) => ({
-      status: 200,
-      body: potentialLimit,
-    }))
-    .catch((error: ClientError) => onError(error.message, error.status));
+    return onError("failed", 500);
+  }
 }
