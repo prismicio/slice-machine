21349e5285bde65c9d21d63d55bf237acef7c95a
Amaury Cahuet <amaury.cahuet@gmail.com>
2023-02-09 18:16:46 +0100

    reduce type

--- packages/slice-machine/server/src/api/push-changes.ts
+++ packages/slice-machine/server/src/api/push-changes.ts
@@ -254,56 +254,48 @@ async function buildChanges(
       change !== undefined
   );
 
-  const customTypeChanges = customTypeModels.reduce(
-    (
-      acc: (
-        | CustomTypeInsertChange
-        | CustomTypeUpdateChange
-        | CustomTypeDeleteChange
-      )[],
-      customType: LocalOrRemoteCustomType
-    ) => {
-      // assessing the user is connected if we went that far in the processing
-      const statusResult = computeModelStatus(customType, true);
+  const customTypeChanges = customTypeModels.reduce<
+    (CustomTypeInsertChange | CustomTypeUpdateChange | CustomTypeDeleteChange)[]
+  >((acc, customType) => {
+    // assessing the user is connected if we went that far in the processing
+    const statusResult = computeModelStatus(customType, true);
 
-      switch (statusResult.status) {
-        case ModelStatus.New: {
-          const payload = CustomTypes.fromSM(statusResult.model.local);
-          const customTypeInsert: CustomTypeInsertChange = {
-            type: ChangeTypes.CUSTOM_TYPE_INSERT,
-            id: payload.id,
-            payload,
-          };
-          return [...acc, customTypeInsert];
-        }
-
-        case ModelStatus.Modified: {
-          const payload = CustomTypes.fromSM(statusResult.model.local);
-          const customTypeUpdate: CustomTypeUpdateChange = {
-            type: ChangeTypes.CUSTOM_TYPE_UPDATE,
-            id: payload.id,
-            payload,
-          };
-          return [...acc, customTypeUpdate];
-        }
-
-        case ModelStatus.Deleted: {
-          const payload = CustomTypes.fromSM(statusResult.model.remote);
-          const customTypeDelete: CustomTypeDeleteChange = {
-            type: ChangeTypes.CUSTOM_TYPE_DELETE,
-            id: payload.id,
-            payload: { id: payload.id },
-          };
-          return [...acc, customTypeDelete];
-        }
-
-        default: {
-          return acc;
-        }
+    switch (statusResult.status) {
+      case ModelStatus.New: {
+        const payload = CustomTypes.fromSM(statusResult.model.local);
+        const customTypeInsert: CustomTypeInsertChange = {
+          type: ChangeTypes.CUSTOM_TYPE_INSERT,
+          id: payload.id,
+          payload,
+        };
+        return [...acc, customTypeInsert];
       }
-    },
-    []
-  );
+
+      case ModelStatus.Modified: {
+        const payload = CustomTypes.fromSM(statusResult.model.local);
+        const customTypeUpdate: CustomTypeUpdateChange = {
+          type: ChangeTypes.CUSTOM_TYPE_UPDATE,
+          id: payload.id,
+          payload,
+        };
+        return [...acc, customTypeUpdate];
+      }
+
+      case ModelStatus.Deleted: {
+        const payload = CustomTypes.fromSM(statusResult.model.remote);
+        const customTypeDelete: CustomTypeDeleteChange = {
+          type: ChangeTypes.CUSTOM_TYPE_DELETE,
+          id: payload.id,
+          payload: { id: payload.id },
+        };
+        return [...acc, customTypeDelete];
+      }
+
+      default: {
+        return acc;
+      }
+    }
+  }, []);
 
   return [...sliceChanges, ...customTypeChanges];
 }
