9e3873dc036011a71f6fd8105df41fd1426eb490
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-02-24 16:01:22 -1000

    chore(start-slicemachine): add development server reloading

--- packages/start-slicemachine/vite.config.ts
+++ packages/start-slicemachine/vite.config.ts
@@ -13,9 +13,16 @@ export default defineConfig({
 			entry: {
 				index: "./src/index.ts",
 				cli: "./src/cli.ts",
+				"bin/start-slicemachine": "./src/bin/start-slicemachine.ts",
 			},
 		},
 		rollupOptions: {
+			// Listing `nodemon` as external prevents it from being
+			// bundled. Note that `nodemon` is listed under
+			// devDependencies and is used in
+			// `./src/bin/start-slicemachine.ts`, which would
+			// normally prompt Vite to bundle the dependency.
+			external: ["nodemon"],
 			plugins: [
 				// @ts-expect-error - Type mismatch
 				renameNodeModules("_node_modules"),
