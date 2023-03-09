9ffb725ab59a6b248722f30d815276e4ce23b2eb
Hugo Villain <hugo.p.villain@gmail.com>
2023-03-09 11:09:02 +0100

    migrate deprecated mocks and cts

--- packages/start-slicemachine/src/lib/migrate.ts
+++ packages/start-slicemachine/src/lib/migrate.ts
@@ -4,6 +4,7 @@ import * as fsSync from "node:fs";
 import { SliceMachineManager } from "@slicemachine/manager";
 import { SharedSliceContent, Document } from "@prismicio/types-internal/lib/content";
 import {
+	DocumentMock,
 	SharedSliceMock,
   } from "@prismicio/mocks";
 
@@ -16,71 +17,77 @@ const createPathToDeprecatedLibrary = (cwd: string) => path.join(cwd, '.slicemac
 const createPathToSlicesAssets = (cwd: string) => path.join(createPathToDeprecatedLibrary(cwd), 'assets', 'slices');
 const createPathToCustomTypesAssets = (cwd: string) => path.join(createPathToDeprecatedLibrary(cwd), 'assets', 'customtypes');
 
-const safeUnlink = (pathToUnlink: string) => {
+const safeUnlink = (pathToUnlink: string, type: "file" | "folder") => {
 	try {
-		fsSync.unlinkSync(pathToUnlink)
+		if (type === "file") {
+			fsSync.unlinkSync(pathToUnlink)
+		} else {
+			fsSync.rmdirSync(pathToUnlink, { recursive: true })
+		}
 	}
 	catch(_) {
+		console.log({ _ })
+	}
+}
+
+const migrateMockFile = (
+	targetPathToMocks: string,
+	deprecatedPathToMocks: string,
+	validator: (str: string) => boolean,
+	generate: () => Record<string, unknown>
+) => {
+	try {
+		const rawMockContent = (() => {
+			if (fsSync.existsSync(targetPathToMocks)) {
+				return fsSync.readFileSync(targetPathToMocks, 'utf-8');
+			}
+			if (fsSync.existsSync(deprecatedPathToMocks)) {
+				return fsSync.readFileSync(deprecatedPathToMocks, 'utf-8');
+			}
+			return null;
+		})();
+
+		if (rawMockContent === null || !validator(rawMockContent)) {
+			const regeneratedMocks = generate();
+			fsSync.writeFileSync(targetPathToMocks, JSON.stringify(regeneratedMocks, null, 2));
+		} else {
+			fsSync.writeFileSync(targetPathToMocks, rawMockContent);
+		}
+	} catch(error) {
 
 	}
 }
 
-// const migrateJSONFile = (targetPath, deprecatedPath, decoder) => {
-
-// }
-
 export const migrate = async (manager: SliceMachineManager) => {
-	console.log("\n", "migration started!!\n")
-
 	try {
 		const allSlices = await manager.slices.readAllSlices(); 
+		const sharedSlices = allSlices.models.reduce((o, slice) => ({ ...o, [slice.model.id]: slice.model }), {})
 		allSlices.models.forEach((c) => {
 			const targetPathToMocks = path.join(manager.cwd, c.libraryID, c.model.name, MOCKS_FILE_NAME);
 			const deprecatedPathToMocks = path.join(createPathToSlicesAssets(manager.cwd), c.libraryID, c.model.name, MOCKS_FILE_NAME);
-			try {
-				const rawMockContent = (() => {
-					if (fsSync.existsSync(targetPathToMocks)) {
-						return fsSync.readFileSync(targetPathToMocks, 'utf-8');
-					}
-					if (fsSync.existsSync(deprecatedPathToMocks)) {
-						const rawMockContent = fsSync.readFileSync(deprecatedPathToMocks, 'utf-8');
-						return rawMockContent;
-					}
-					return null;
-				})();
-		
-				if (rawMockContent === null || ComponentMocks.decode(rawMockContent)._tag === "Left") {
-					const regeneratedMocks = SharedSliceMock.generate(c.model);
-					fsSync.writeFileSync(targetPathToMocks, JSON.stringify(regeneratedMocks, null, 2));
-				} else {
-					fsSync.writeFileSync(targetPathToMocks, rawMockContent);
-				}
-			} catch(error) {
-
-			}
+			migrateMockFile(
+				targetPathToMocks,
+				deprecatedPathToMocks,
+				(str: string) => ComponentMocks.decode(str)._tag === "Right",
+				() => SharedSliceMock.generate(c.model)
+			)
 		});
 
 		const allCustomTypes = await manager.customTypes.readAllCustomTypes();
-		console.log({ allCustomTypes })
-
-		// USE Document.decode(...) with same logic
+		allCustomTypes.models.forEach((c) => {
+			const targetPathToMocks = path.join(manager.cwd, 'customtypes', c.model.id, MOCKS_FILE_NAME);
+			const deprecatedPathToMocks = path.join(createPathToCustomTypesAssets(manager.cwd), c.model.id, MOCKS_FILE_NAME);
+			migrateMockFile(
+				targetPathToMocks,
+				deprecatedPathToMocks,
+				(str: string) => Document.decode(str)._tag === "Right",
+				() => DocumentMock.generate(c.model, sharedSlices)
+			)
+		});
 	} catch(error) {
 
 	} finally {
-		safeUnlink(path.join(createPathToDeprecatedLibrary(manager.cwd), 'assets'));
-		safeUnlink(path.join(createPathToDeprecatedLibrary(manager.cwd), MOCK_CONFIG_FILE_NAME));
+		safeUnlink(path.join(createPathToDeprecatedLibrary(manager.cwd), 'assets'), "folder");
+		safeUnlink(path.join(createPathToDeprecatedLibrary(manager.cwd), MOCK_CONFIG_FILE_NAME), "file");
 	}
-
-	
 }
-
-/**
-const pathToDeprecatedMocksConfig = path.join(createPathToDeprecatedLibrary(manager.cwd), MOCK_CONFIG_FILE_NAME);
-const pathToSliceMockConfig = path.join(manager.cwd, c.libraryID, c.model.name, MOCK_CONFIG_FILE_NAME);
-if (fsSync.existsSync(pathToDeprecatedMocksConfig)) {
-	fsSync.unlinkSync(pathToDeprecatedMocksConfig)
-}
-if (fsSync.existsSync(pathToSliceMockConfig)) {
-	fsSync.unlinkSync(pathToSliceMockConfig)
-}
- */
