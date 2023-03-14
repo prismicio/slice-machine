e4f42b2050cc87d8b805deed42fd761d308e380e
mdeclercq <mat.declercq@gmail.com>
2023-03-14 16:04:41 +0100

    trying to fixing puppeteer installation in ci

--- cypress-setup.sh
+++ cypress-setup.sh
@@ -25,7 +25,7 @@ fi
 rm -rf e2e-projects/cypress-next-app \
 && npx --yes create-next-app e2e-projects/cypress-next-app \
 && npx --yes vite-node ./cypress/plugins/addAuth.ts -- ${EMAIL} ${PASSWORD} ${PRISMIC_URL} \
-&& npx --yes vite-node ./cypress/plugins/createRepo.ts -- "${_PRISMIC_REPO}" "${PASSWORD}" "${PRISMIC_URL}"  \
+&& npx --yes vite-node ./cypress/plugins/createRepo.ts -- "${_PRISMIC_REPO}" "${PASSWORD}" "${PRISMIC_URL}" \
 && npm --workspaces pack --pack-destination ./e2e-projects/cypress-next-app \
 && cd e2e-projects/cypress-next-app \
 && npm i *.tgz \
