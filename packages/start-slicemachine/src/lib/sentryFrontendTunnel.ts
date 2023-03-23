d7238befde7c7b3887e17944e61ffd926c650de3
lihbr <lihbr@users.noreply.github.com>
2023-03-23 13:08:12 +0100

    build(start-slicemachine): support websocket for development (SMX-99)

--- packages/start-slicemachine/src/lib/sentryFrontendTunnel.ts
+++ packages/start-slicemachine/src/lib/sentryFrontendTunnel.ts
@@ -1,39 +1,35 @@
 // We tunnel the Sentry calls via the h3 server to avoid ad blockers
 // adapted from https://github.com/getsentry/examples/blob/0e2f17c5914f28ccae931e51801b7d9760d34cbe/tunneling/nextjs/pages/api/tunnel.js
 
-import { H3Event, readRawBody } from "h3";
-import fetch from "node-fetch";
+import type { RequestHandler } from "express";
 import * as Sentry from "@sentry/node";
+import fetch from "node-fetch";
 
-export const sentryFrontendTunnel = async (
-	event: H3Event,
-): Promise<Record<string, never>> => {
+export const sentryFrontendTunnel: RequestHandler = async (req, res) => {
 	try {
-		const envelope = await readRawBody(event);
+		const envelope = req.body;
 
-		if (!envelope) {
-			return {};
-		}
+		if (envelope) {
+			const pieces = envelope.split("\n");
+			const header = JSON.parse(pieces[0]);
+			const { host, pathname } = new URL(header.dsn);
+			const projectId =
+				(pathname?.endsWith("/") ? pathname.slice(0, -1) : pathname) ?? "";
+			const sentryUrl = `https://${host}/api/${projectId}/envelope/`;
 
-		const pieces = envelope.split("\n");
-		const header = JSON.parse(pieces[0]);
-		const { host, pathname } = new URL(header.dsn);
-		const projectId =
-			(pathname?.endsWith("/") ? pathname.slice(0, -1) : pathname) ?? "";
-		const sentryUrl = `https://${host}/api/${projectId}/envelope/`;
+			const response = await fetch(sentryUrl, {
+				method: "POST",
+				body: envelope,
+			});
 
-		const response = await fetch(sentryUrl, {
-			method: "POST",
-			body: envelope,
-		});
-
-		if (!response.ok) {
-			const errorMessage = await response.text();
-			throw new Error(errorMessage);
+			if (!response.ok) {
+				const errorMessage = await response.text();
+				throw new Error(errorMessage);
+			}
 		}
 	} catch (e) {
 		Sentry.captureException(e);
 	} finally {
-		return {};
+		return res.status(202).send();
 	}
 };
