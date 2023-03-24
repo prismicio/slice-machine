59b5e17e1251cdf918f27b680da1564903f5d011
Baptiste Morelle <bapmrl@users.noreply.github.com>
2023-03-24 17:26:23 +0100

    chore: remove e2e-projects/next2

--- e2e-projects/next2/slices/Image/index.js
+++
@@ -1,7 +0,0 @@
-const Image = ({ slice }) => {
-  return (
-    <section>This is the Image Slice (variation: {slice.variation})</section>
-  );
-};
-
-export default Image;
--- e2e-projects/next2/slices/Quote/index.js
+++
@@ -1,7 +0,0 @@
-const Quote = ({ slice }) => {
-  return (
-    <section>This is the Quote Slice (variation: {slice.variation})</section>
-  );
-};
-
-export default Quote;
