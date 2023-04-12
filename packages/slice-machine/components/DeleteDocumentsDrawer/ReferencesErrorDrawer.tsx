193e4553685fe9a940f2c4304de101e3853fb284
Daniel Belo Goncalves <47107427+danigo99@users.noreply.github.com>
2023-04-12 19:18:39 +0100

    bring delete drawers back (#907)

--- packages/slice-machine/components/DeleteDocumentsDrawer/ReferencesErrorDrawer.tsx
+++ packages/slice-machine/components/DeleteDocumentsDrawer/ReferencesErrorDrawer.tsx
@@ -1,93 +1,91 @@
-// import React from "react";
-// import { Text } from "theme-ui";
-// import useSliceMachineActions from "@src/modules/useSliceMachineActions";
-// import { Button } from "@components/Button";
-// import { useSelector } from "react-redux";
-// import { SliceMachineStoreType } from "@src/redux/type";
-// import { ModalKeysEnum } from "@src/modules/modal/types";
-// import { isModalOpen } from "@src/modules/modal";
-// import { getModelId, hasLocal } from "@lib/models/common/ModelData";
-// import { CustomTypesReferencesCard } from "./AssociatedDocumentsCard";
-// import { ToasterType } from "@src/modules/toaster";
-// import { selectAllCustomTypes } from "@src/modules/availableCustomTypes";
-// import { SliceMachineDrawerUI } from "@components/SliceMachineDrawer";
+import React from "react";
+import { Text } from "theme-ui";
+import useSliceMachineActions from "@src/modules/useSliceMachineActions";
+import { Button } from "@components/Button";
+import { useSelector } from "react-redux";
+import { SliceMachineStoreType } from "@src/redux/type";
+import { ModalKeysEnum } from "@src/modules/modal/types";
+import { isModalOpen } from "@src/modules/modal";
+import { getModelId, hasLocal } from "@lib/models/common/ModelData";
+import { CustomTypesReferencesCard } from "./AssociatedDocumentsCard";
+import { ToasterType } from "@src/modules/toaster";
+import { selectAllCustomTypes } from "@src/modules/availableCustomTypes";
+import { SliceMachineDrawerUI } from "@components/SliceMachineDrawer";
 
-// export const ReferencesErrorDrawer: React.FunctionComponent<{
-//   pushChanges: (confirmDeleteDocuments: boolean) => void;
-// }> = ({ pushChanges }) => {
-//   const { isOpen, modalData, localCustomTypes } = useSelector(
-//     (store: SliceMachineStoreType) => ({
-//       isOpen: isModalOpen(store, ModalKeysEnum.REFERENCES_MISSING_DRAWER),
-//       localCustomTypes: selectAllCustomTypes(store).filter(hasLocal),
-//       modalData: store.pushChanges,
-//     })
-//   );
-//   const { closeModals, openToaster } = useSliceMachineActions();
+export const ReferencesErrorDrawer: React.FunctionComponent<{
+  pushChanges: (confirmDeleteDocuments: boolean) => void;
+}> = ({ pushChanges }) => {
+  const { isOpen, modalData, localCustomTypes } = useSelector(
+    (store: SliceMachineStoreType) => ({
+      isOpen: isModalOpen(store, ModalKeysEnum.REFERENCES_MISSING_DRAWER),
+      localCustomTypes: selectAllCustomTypes(store).filter(hasLocal),
+      modalData: store.pushChanges,
+    })
+  );
+  const { closeModals, openToaster } = useSliceMachineActions();
 
-//   if (!isOpen) return null;
+  if (!isOpen) return null;
 
-//   if (modalData?.details === undefined) {
-//     openToaster("No change data", ToasterType.ERROR);
-//     return null;
-//   }
+  if (modalData?.details === undefined) {
+    openToaster("No change data", ToasterType.ERROR);
+    return null;
+  }
 
-//   const hasMoreThanOne = modalData?.details.customTypes.length > 1;
+  const hasMoreThanOne = modalData?.details.customTypes.length > 1;
 
-//   const associatedDocumentsCards = modalData.details.customTypes.map(
-//     (customTypeDetail) => {
-//       const customType = localCustomTypes.find(
-//         (customType) => getModelId(customType) === customTypeDetail.id
-//       );
-//       if (customType === undefined) return null;
+  const associatedDocumentsCards = modalData.details.customTypes.map(
+    (customTypeDetail) => {
+      const customType = localCustomTypes.find(
+        (customType) => getModelId(customType) === customTypeDetail.id
+      );
+      if (customType === undefined) return null;
 
-//       return (
-//         <CustomTypesReferencesCard
-//           name={customType.local.label ?? customType.local.id}
-//           id={customType.local.id}
-//         />
-//       );
-//     }
-//   );
+      return (
+        <CustomTypesReferencesCard
+          name={customType.local.label ?? customType.local.id}
+          id={customType.local.id}
+        />
+      );
+    }
+  );
 
-//   return (
-//     <SliceMachineDrawerUI
-//       isOpen={isOpen}
-//       title={"Missing Slices"}
-//       footer={
-//         <>
-//           <Button
-//             label="Try again"
-//             variant="primary"
-//             onClick={() => {
-//               closeModals();
-//               pushChanges(false);
-//             }}
-//             sx={{
-//               fontWeight: "bold",
-//               color: "white",
-//               borderRadius: 6,
-//               width: "100%",
-//             }}
-//           />
-//         </>
-//       }
-//       explanations={
-//         <>
-//           <Text sx={{ fontWeight: "bold", mb: 1, lineHeight: "24px" }}>
-//             You have{hasMoreThanOne ? "" : " a"} Custom Type
-//             {hasMoreThanOne ? "s" : ""} that reference{" "}
-//             {hasMoreThanOne ? "" : "s"} one or more missing Slices
-//           </Text>
-//           <Text sx={{ mb: 24 }}>
-//             Before pushing, remove references to any missing Slices by opening{" "}
-//             the affected Custom Type{hasMoreThanOne ? "s" : ""} and clicking the{" "}
-//             <b>Save</b> button.
-//           </Text>
-//           {associatedDocumentsCards}
-//         </>
-//       }
-//     />
-//   );
-// };
-
-export const ReferencesErrorDrawer = () => <></>;
+  return (
+    <SliceMachineDrawerUI
+      isOpen={isOpen}
+      title={"Missing Slices"}
+      footer={
+        <>
+          <Button
+            label="Try again"
+            variant="primary"
+            onClick={() => {
+              closeModals();
+              pushChanges(false);
+            }}
+            sx={{
+              fontWeight: "bold",
+              color: "white",
+              borderRadius: 6,
+              width: "100%",
+            }}
+          />
+        </>
+      }
+      explanations={
+        <>
+          <Text sx={{ fontWeight: "bold", mb: 1, lineHeight: "24px" }}>
+            You have{hasMoreThanOne ? "" : " a"} Custom Type
+            {hasMoreThanOne ? "s" : ""} that reference{" "}
+            {hasMoreThanOne ? "" : "s"} one or more missing Slices
+          </Text>
+          <Text sx={{ mb: 24 }}>
+            Before pushing, remove references to any missing Slices by opening{" "}
+            the affected Custom Type{hasMoreThanOne ? "s" : ""} and clicking the{" "}
+            <b>Save</b> button.
+          </Text>
+          {associatedDocumentsCards}
+        </>
+      }
+    />
+  );
+};
