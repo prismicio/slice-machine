ec28c372514d763a5cc7d9240f79ccb5c7541780
Baptiste Morelle <bapmrl@users.noreply.github.com>
2023-04-17 18:32:13 +0200

    fix: deleteScreenshotFolder (#923)

--- packages/manager/src/managers/screenshots/ScreenshotsManager.ts
+++ packages/manager/src/managers/screenshots/ScreenshotsManager.ts
@@ -296,7 +296,9 @@ export class ScreenshotsManager extends BaseManager {
 		args: ScreenshotsManagerDeleteScreenshotFolderArgs,
 	): Promise<void> {
 		const res = await this._fetch({
-			body: { sliceId: args.sliceID },
+			// We're sending `args.sliceID` as `sliceName` because it's inconsistently
+			// named in the ACL Provider API.
+			body: { sliceName: args.sliceID },
 			method: "POST",
 			url: new URL("delete-folder", API_ENDPOINTS.AwsAclProvider),
 		});
