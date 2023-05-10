a4c4dbe899687f07c69df252e13c925f073e65d6
Baptiste Morelle <bapmrl@users.noreply.github.com>
2023-05-10 18:04:55 +0200

    install storybook

--- packages/slice-machine/.eslintrc.js
+++ packages/slice-machine/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "eslint-config-prettier",
+    "plugin:storybook/recommended",
   ],
   ignorePatterns: ["build", "templates", "**/tests/**", "helpers/**"],
   rules: {
