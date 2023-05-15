55b1f61b2a64b275f2b6b2ebb2025bd926af5671
Baptiste Morelle <bapmrl@users.noreply.github.com>
2023-05-15 12:23:34 +0200

    remove unnecessary deps and default stories

--- packages/slice-machine/.storybook/main.ts
+++ packages/slice-machine/.storybook/main.ts
@@ -1,11 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-essentials"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
