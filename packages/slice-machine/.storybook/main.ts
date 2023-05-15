13a4d4e47102eb4b48000c0cc24aac7c1bed9e3b
Baptiste Morelle <bapmrl@users.noreply.github.com>
2023-05-15 12:25:19 +0200

    only accept .ts?(x) stories

--- packages/slice-machine/.storybook/main.ts
+++ packages/slice-machine/.storybook/main.ts
@@ -1,13 +1,10 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-essentials"],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+  docs: { autodocs: "tag" },
+  framework: "@storybook/react-vite",
+  stories: ["../src/**/*.stories.ts?(x)"],
 };
+
 export default config;
