831e146f865a572f0c4c9131acfd38675e39dcb1
Baptiste Morelle <bapmrl@users.noreply.github.com>
2023-05-17 11:55:42 +0200

    use disableTelemetry rather than env variable

--- packages/slice-machine/.storybook/main.ts
+++ packages/slice-machine/.storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   addons: ["@storybook/addon-essentials"],
+  core: { disableTelemetry: true },
   docs: { autodocs: "tag" },
   framework: "@storybook/react-vite",
   stories: ["../src/**/*.stories.ts?(x)"],
