1eabb29b9a34bf6410c96e062fcdfc834c927f31
Baptiste Morelle <bapmrl@users.noreply.github.com>
2023-05-17 12:15:43 +0200

    fix unused clsx dep

--- packages/slice-machine/src/components/PageLayout/PageLayout.tsx
+++ packages/slice-machine/src/components/PageLayout/PageLayout.tsx
@@ -1,3 +1,4 @@
+import { clsx } from "clsx";
 import type { FC, ReactNode } from "react";
 import { Text } from "@prismicio/editor-ui";
 
@@ -5,13 +6,15 @@ import * as styles from "./PageLayout.css";
 
 type PageLayoutProps = Readonly<{
   children?: ReactNode;
+  className?: string;
 }>;
 
 export const PageLayout: FC<PageLayoutProps> = ({
   children,
+  className,
   ...otherProps
 }) => (
-  <div {...otherProps} className={styles.root}>
+  <div {...otherProps} className={clsx(styles.root, className)}>
     <Text>PageLayout</Text>
     {children}
   </div>
