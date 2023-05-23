e93dc4d42a667cdc039bb7691cbea58fe6d7d10d
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2023-05-23 10:55:07 +0200

    feat(slice-machine-ui): fix types for Table story

--- packages/slice-machine/src/components/Table/Table.stories.tsx
+++ packages/slice-machine/src/components/Table/Table.stories.tsx
@@ -1,3 +1,4 @@
+import { FC } from "react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -18,89 +19,88 @@ import { TableRow } from "./TableRow";
 
 type Story = StoryObj<typeof meta>;
 
-const meta = {
-  component: Table,
-  render: () => {
-    const data = Array.from({ length: 10 }, (_, index) => ({
-      id: index,
-      name:
-        index === 3 || index === 7
-          ? `The very very long page name that is annoying ${index}`
-          : `My name ${index}`,
-      apiId:
-        index === 5 || index === 7
-          ? `very_very_very_long_api_id_${index}`
-          : `api_id_${index}`,
-      limit: index % 2 ? "repeatable" : "single",
-    }));
+const TableExample: FC = () => {
+  const data = Array.from({ length: 10 }, (_, index) => ({
+    id: index,
+    name:
+      index === 3 || index === 7
+        ? `The very very long page name that is annoying ${index}`
+        : `My name ${index}`,
+    apiId:
+      index === 5 || index === 7
+        ? `very_very_very_long_api_id_${index}`
+        : `api_id_${index}`,
+    limit: index % 2 ? "repeatable" : "single",
+  }));
 
-    return (
-      <Table>
-        <TableHead>
-          <TableRow>
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>
+            <Icon name="fieldList" />
+          </TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>API ID</TableCell>
+          <TableCell>Limit</TableCell>
+          <TableCell />
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data.map(({ id, name, apiId, limit }) => (
+          <TableRow
+            key={id}
+            onClick={() => {
+              console.log(`Table row clicked for ${name}`);
+            }}
+          >
             <TableCell>
-              <Icon name="fieldList" />
+              {limit === "single" ? (
+                <Icon name="unique" />
+              ) : (
+                <Icon name="reusable" />
+              )}
             </TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>API ID</TableCell>
-            <TableCell>Limit</TableCell>
-            <TableCell />
+            <TableCell>
+              <span style={{ fontWeight: 600 }}>{name}</span>
+            </TableCell>
+            <TableCell>{apiId}</TableCell>
+            <TableCell>{limit === "single" ? "Unique" : "Reusable"}</TableCell>
+            <TableCell>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <IconButton icon="kebabDots" />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuItem
+                    startIcon={<IconEditor name="edit" />}
+                    onSelect={() => {
+                      console.log(`Rename clicked for ${name}`);
+                    }}
+                  >
+                    <Text>Rename</Text>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    startIcon={<IconEditor color="tomato11" name="delete" />}
+                    onSelect={() => {
+                      console.log(`Remove clicked for ${name}`);
+                    }}
+                  >
+                    <Text color="tomato11">Remove</Text>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
           </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map(({ id, name, apiId, limit }) => (
-            <TableRow
-              key={id}
-              onClick={() => {
-                console.log(`Table row clicked for ${name}`);
-              }}
-            >
-              <TableCell>
-                {limit === "single" ? (
-                  <Icon name="unique" />
-                ) : (
-                  <Icon name="reusable" />
-                )}
-              </TableCell>
-              <TableCell>
-                <span style={{ fontWeight: 600 }}>{name}</span>
-              </TableCell>
-              <TableCell>{apiId}</TableCell>
-              <TableCell>
-                {limit === "single" ? "Unique" : "Reusable"}
-              </TableCell>
-              <TableCell>
-                <DropdownMenu>
-                  <DropdownMenuTrigger>
-                    <IconButton icon="kebabDots" />
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuItem
-                      startIcon={<IconEditor name="edit" />}
-                      onSelect={() => {
-                        console.log(`Rename clicked for ${name}`);
-                      }}
-                    >
-                      <Text>Rename</Text>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      startIcon={<IconEditor color="tomato11" name="delete" />}
-                      onSelect={() => {
-                        console.log(`Remove clicked for ${name}`);
-                      }}
-                    >
-                      <Text color="tomato11">Remove</Text>
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    );
-  },
-} satisfies Meta<typeof Table>;
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+const meta = {
+  component: TableExample,
+} satisfies Meta<typeof TableExample>;
 
 export default meta;
 
