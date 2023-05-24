c2cb3e582154edade5a655e17289d177002b17de
MarcMcIntosh <marcmcintosh1987@gmail.com>
2023-05-24 18:07:10 +0200

    page-types: add seo meta data to custom-type

--- packages/slice-machine/src/modules/availableCustomTypes/factory.ts
+++ packages/slice-machine/src/modules/availableCustomTypes/factory.ts
@@ -1,36 +1,75 @@
-import { CustomTypeSM, TabFields } from "@lib/models/common/CustomType";
+import { CustomTypeSM, TabSM } from "@lib/models/common/CustomType";
+
+const DEFAULT_SEO_TAB: TabSM = {
+  key: "Metadata",
+  value: [
+    {
+      key: "meta_title",
+      value: {
+        type: "Text",
+        config: {
+          label: "Meta Title",
+          placeholder:
+            "A title of the page used for social media and search engines",
+        },
+      },
+    },
+    {
+      key: "meta_description",
+      value: {
+        type: "StructuredText",
+        config: {
+          label: "Meta Description",
+          placeholder: "A brief summary of the page",
+        },
+      },
+    },
+    {
+      key: "meta_image",
+      value: {
+        type: "Image",
+        config: {
+          label: "Meta Image",
+          constraint: {
+            width: 2400,
+            height: 1260,
+          },
+        },
+      },
+    },
+  ],
+};
 
 export const createCustomType = (
   id: string,
   label: string,
   repeatable: boolean
 ): CustomTypeSM => {
-  const defaultMainTabValue: TabFields = [];
+  const mainTab: TabSM = {
+    key: "Main",
+    value: repeatable
+      ? [
+          {
+            key: "uid",
+            value: {
+              type: "UID",
+              config: {
+                label: "UID",
+              },
+            },
+          },
+        ]
+      : [],
+  };
 
-  // All new custom type should have a default UID field non-editable
-  if (repeatable) {
-    defaultMainTabValue.push({
-      key: "uid",
-      value: {
-        type: "UID",
-        config: {
-          label: "UID",
-        },
-      },
-    });
-  }
+  const tabs: TabSM[] = [mainTab, DEFAULT_SEO_TAB];
 
   return {
     id,
     label,
     format: "custom",
     repeatable,
-    tabs: [
-      {
-        key: "Main",
-        value: defaultMainTabValue,
-      },
-    ],
+    tabs,
     status: true,
   };
 };
