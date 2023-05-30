7dee521aa61f9bf28aa8bc6521fc5601eeeef322
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2023-05-30 16:14:28 +0200

    style(slice-machine-ui): improve components props management with children (#988)

--- packages/slice-machine/src/components/Breadcrumb/Breadcrumb.tsx
+++ packages/slice-machine/src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,10 +1,6 @@
 import { Text } from "@prismicio/editor-ui";
-import { FC, ReactNode } from "react";
+import { FC, PropsWithChildren } from "react";
 
-type BreadcrumbProps = Readonly<{
-  children: ReactNode;
-}>;
-
-export const Breadcrumb: FC<BreadcrumbProps> = ({ children }) => {
+export const Breadcrumb: FC<PropsWithChildren> = ({ children }) => {
   return <Text color="grey11">{children}</Text>;
 };
