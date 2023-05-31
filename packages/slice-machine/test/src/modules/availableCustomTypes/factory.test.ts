d9a78820927ec777e4992c88a41271f1c4a9bccf
Marc McIntosh <marcmcintosh1987@gmail.com>
2023-05-31 17:20:20 +0200

    refactor(create-custom-type): return custom-type from types-internal (#989)

--- packages/slice-machine/test/src/modules/availableCustomTypes/factory.test.ts
+++ packages/slice-machine/test/src/modules/availableCustomTypes/factory.test.ts
@@ -6,30 +6,24 @@ describe("[Custom types factory]", () => {
     it("should create a custom type with repeatable true", () => {
       expect(createCustomType("id", "lama", true, "custom"))
         .toMatchInlineSnapshot(`
-        {
-          "format": "custom",
-          "id": "id",
-          "label": "lama",
-          "repeatable": true,
-          "status": true,
-          "tabs": [
-            {
-              "key": "Main",
-              "value": [
-                {
-                  "key": "uid",
-                  "value": {
-                    "config": {
-                      "label": "UID",
-                    },
-                    "type": "UID",
+          {
+            "format": "custom",
+            "id": "id",
+            "json": {
+              "Main": {
+                "uid": {
+                  "config": {
+                    "label": "UID",
                   },
+                  "type": "UID",
                 },
-              ],
+              },
             },
-          ],
-        }
-      `);
+            "label": "lama",
+            "repeatable": true,
+            "status": true,
+          }
+        `);
     });
 
     it("should create a custom type with repeatable false", () => {
@@ -37,20 +31,17 @@ describe("[Custom types factory]", () => {
       expect(result.id).toBe("id");
       expect(result.label).toBe("lama");
       expect(result.format).toBe("custom");
-      expect(result.tabs).not.toHaveLength(0);
+      expect(result.json).toHaveProperty("Main");
       expect(result).toMatchInlineSnapshot(`
         {
           "format": "custom",
           "id": "id",
+          "json": {
+            "Main": {},
+          },
           "label": "lama",
           "repeatable": false,
           "status": true,
-          "tabs": [
-            {
-              "key": "Main",
-              "value": [],
-            },
-          ],
         }
       `);
     });
@@ -58,78 +49,113 @@ describe("[Custom types factory]", () => {
     it("should create a page type with a slice zone", () => {
       const result = createCustomType("🥪", "label", true, "page");
       expect(result.format).toBe("page");
-      expect(result.tabs).not.toHaveLength(0);
-      const [main] = result.tabs;
-      expect(main).toHaveProperty("sliceZone");
-
+      expect(result.json).toHaveProperty("Main");
+      expect(result.json.Main).toHaveProperty("slices");
+      expect(result.json).toHaveProperty("SEO & Metadata");
       expect(result).toMatchInlineSnapshot(`
         {
           "format": "page",
           "id": "🥪",
+          "json": {
+            "Main": {
+              "slices": {
+                "config": {
+                  "choices": {},
+                },
+                "fieldset": "Slice Zone",
+                "type": "Slices",
+              },
+              "uid": {
+                "config": {
+                  "label": "UID",
+                },
+                "type": "UID",
+              },
+            },
+            "SEO & Metadata": {
+              "meta_description": {
+                "config": {
+                  "label": "Meta Description",
+                  "placeholder": "A brief summary of the page",
+                },
+                "type": "StructuredText",
+              },
+              "meta_image": {
+                "config": {
+                  "constraint": {
+                    "height": 1260,
+                    "width": 2400,
+                  },
+                  "label": "Meta Image",
+                  "thumbnails": [],
+                },
+                "type": "Image",
+              },
+              "meta_title": {
+                "config": {
+                  "label": "Meta Title",
+                  "placeholder": "A title of the page used for social media and search engines",
+                },
+                "type": "Text",
+              },
+            },
+          },
           "label": "label",
           "repeatable": true,
           "status": true,
-          "tabs": [
-            {
-              "key": "Main",
-              "sliceZone": {
-                "key": "slices",
-                "value": [],
-              },
-              "value": [
-                {
-                  "key": "uid",
-                  "value": {
-                    "config": {
-                      "label": "UID",
-                    },
-                    "type": "UID",
-                  },
-                },
-              ],
-            },
-            {
-              "key": "SEO & Metadata",
-              "value": [
-                {
-                  "key": "meta_title",
-                  "value": {
-                    "config": {
-                      "label": "Meta Title",
-                      "placeholder": "A title of the page used for social media and search engines",
-                    },
-                    "type": "Text",
-                  },
-                },
-                {
-                  "key": "meta_description",
-                  "value": {
-                    "config": {
-                      "label": "Meta Description",
-                      "placeholder": "A brief summary of the page",
-                    },
-                    "type": "StructuredText",
-                  },
-                },
-                {
-                  "key": "meta_image",
-                  "value": {
-                    "config": {
-                      "constraint": {
-                        "height": 1260,
-                        "width": 2400,
-                      },
-                      "label": "Meta Image",
-                      "thumbnails": [],
-                    },
-                    "type": "Image",
-                  },
-                },
-              ],
-            },
-          ],
         }
       `);
     });
+  });
+
+  it("when non repeatable page type is should contain Main with a slice-zone, no uid, and a SEO tab", () => {
+    const result = createCustomType("foo", "bar", false, "page");
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "format": "page",
+        "id": "foo",
+        "json": {
+          "Main": {
+            "slices": {
+              "config": {
+                "choices": {},
+              },
+              "fieldset": "Slice Zone",
+              "type": "Slices",
+            },
+          },
+          "SEO & Metadata": {
+            "meta_description": {
+              "config": {
+                "label": "Meta Description",
+                "placeholder": "A brief summary of the page",
+              },
+              "type": "StructuredText",
+            },
+            "meta_image": {
+              "config": {
+                "constraint": {
+                  "height": 1260,
+                  "width": 2400,
+                },
+                "label": "Meta Image",
+                "thumbnails": [],
+              },
+              "type": "Image",
+            },
+            "meta_title": {
+              "config": {
+                "label": "Meta Title",
+                "placeholder": "A title of the page used for social media and search engines",
+              },
+              "type": "Text",
+            },
+          },
+        },
+        "label": "bar",
+        "repeatable": false,
+        "status": true,
+      }
+    `);
   });
 });
