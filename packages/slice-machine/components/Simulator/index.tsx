bce642bcb7d70dc806b0c7eff4688c7e4a4574cf
MarcMcIntosh <marcmcintosh1987@gmail.com>
2023-06-01 15:42:58 +0200

    chore: update oembed and upsplash urls to use prismic.io endpoints

--- packages/slice-machine/components/Simulator/index.tsx
+++ packages/slice-machine/components/Simulator/index.tsx
@@ -308,7 +308,7 @@ export default Simulator;
 // TODO(DT-1333): change this config depending on the environment.
 const SHARED_SLICE_EDITOR_CONFIG = {
   embeds: {
-    url: "https://jntc6tzga1.execute-api.us-east-1.amazonaws.com/beta",
+    url: "https://oembed.pismic.io",
   },
-  unsplash: { url: "https://unsplash.wroom.io" },
+  unsplash: { url: "https://unsplash.prismic.io" },
 };
