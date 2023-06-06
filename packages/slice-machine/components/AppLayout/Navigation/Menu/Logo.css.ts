70459b9929013859ceab88c92d703fc6a9ca9c1b
MarcMcIntosh <marcmcintosh1987@gmail.com>
2023-06-06 15:00:24 +0200

    refactor: no need for sprinkles yet

--- packages/slice-machine/components/AppLayout/Navigation/Menu/Logo.css.ts
+++ packages/slice-machine/components/AppLayout/Navigation/Menu/Logo.css.ts
@@ -1,23 +1,17 @@
-import { sprinkles } from "@prismicio/editor-ui";
 import { style } from "@vanilla-extract/css";
 
 export const container = style({ padding: "0 14px" });
 
-export const icon = style([
-  sprinkles({
-    marginBottom: 32,
-  }),
-]);
+export const icon = style({
+  marginBottom: 32,
+});
 
-export const flex = style([
-  sprinkles({
-    display: "flex",
-    justifyContent: "space-between",
-  }),
-]);
+export const flex = style({
+  display: "flex",
+  justifyContent: "space-between",
+});
 
 export const title = style({
-  // TODO: sprinkles doesn't do fonts or colors and style is over ridden by some global selector (probably theme-ui), so this needs to go inline
   fontWeight: 500,
   fontSize: "18px",
   lineHeight: "32px",
@@ -26,7 +20,6 @@ export const title = style({
 });
 
 export const repoUrl = style({
-  // TODO: same as above
   fontStyle: "normal",
   fontWeight: 400,
   fontSize: "12px",
