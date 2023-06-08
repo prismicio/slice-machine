ea9f543a7be48083b2c2d4ebd1cc1d3062f6b563
Marc McIntosh <marcmcintosh1987@gmail.com>
2023-06-08 14:41:37 +0200

    Mm/dt 1333 (#995): update editor config endpoints
    Co-authored-by: MarcMcIntosh <MarcMcIntosh@users.noreply.github.com>
    Co-authored-by: Xavier Rutayisire <xavier.rutayisire@gmail.com>
    Co-authored-by: xrutayisire <xrutayisire@users.noreply.github.com>

--- packages/manager/test/SliceMachineManager-getState.test.ts
+++ packages/manager/test/SliceMachineManager-getState.test.ts
@@ -1,5 +1,38 @@
-import { it } from "vitest";
+import { it, expect } from "vitest";
+import { createSliceMachineManager } from "../src";
+import { createTestProject } from "./__testutils__/createTestProject";
+import { createTestPlugin } from "./__testutils__/createTestPlugin";
 
-it.todo("returns global Slice Machine state", () => {
-	// TODO
+it("returns global Slice Machine state", async () => {
+	const adapter = await createTestPlugin();
+	const cwd = await createTestProject({ adapter });
+	const manager = createSliceMachineManager({
+		cwd,
+		nativePlugins: {
+			[adapter.meta.name]: adapter,
+		},
+	});
+	await manager.plugins.initPlugins();
+	const result = await manager.getState();
+
+	expect(result.env.endpoints).toStrictEqual({
+		AwsAclProvider:
+			"https://0yyeb2g040.execute-api.us-east-1.amazonaws.com/prod/",
+		PrismicOembed: "https://oembed.prismic.io/",
+		PrismicAuthentication: "https://auth.prismic.io/",
+		PrismicModels: "https://customtypes.prismic.io/",
+		PrismicUser: "https://user.internal-prismic.io/",
+		PrismicWroom: "https://prismic.io/",
+		PrismicUnsplash: "https://unsplash.prismic.io/",
+	});
+	expect(result.clientError).toStrictEqual({
+		message: "__stub__",
+		name: "__stub__",
+		reason: "__stub__",
+		status: 401,
+	});
+	expect(result.libraries).toStrictEqual([]);
+	expect(result.customTypes).toStrictEqual([]);
+	expect(result.remoteCustomTypes).toStrictEqual([]);
+	expect(result.remoteSlices).toStrictEqual([]);
 });
--- packages/slice-machine/test/src/modules/__fixtures__/serverState.ts
+++ packages/slice-machine/test/src/modules/__fixtures__/serverState.ts
@@ -20,6 +20,16 @@ export const dummyServerState: Pick<
       updateAvailable: false,
       versions: [],
     },
+    endpoints: {
+      PrismicWroom: "https://prismic.io/",
+      PrismicAuthentication: "https://auth.prismic.io/",
+      PrismicModels: "https://customtypes.prismic.io/",
+      PrismicUser: "https://user.internal-prismic.io/",
+      AwsAclProvider:
+        "https://0yyeb2g040.execute-api.us-east-1.amazonaws.com/prod/",
+      PrismicOembed: "https://oembed.prismic.io/",
+      PrismicUnsplash: "https://unsplash.prismic.io/",
+    },
     shortId: "shortId",
     supportsSliceSimulator: true,
   },
