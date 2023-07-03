8ad8ae4e31bbbb20ccbcbd8019ab4ca818f4c1f9
Baptiste Morelle <bapmrl@users.noreply.github.com>
2023-07-03 12:18:18 +0200

    minor tweaks

--- packages/slice-machine/src/components/CodeBlock/CodeBlock.css.ts
+++ packages/slice-machine/src/components/CodeBlock/CodeBlock.css.ts
@@ -1,31 +1,31 @@
-import { style } from "@vanilla-extract/css";
-import { colors, sprinkles, vars } from "@prismicio/editor-ui";
+import { colors, sprinkles } from "@prismicio/editor-ui";
 
 export const root = sprinkles({
+  all: "unset",
+  display: "revert",
   borderWidth: 1,
   borderRadius: 4,
   borderStyle: "solid",
   borderColor: colors.grey6,
 });
 
-export const header = style([
-  sprinkles({
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    borderTopRightRadius: 4,
-    borderTopLeftRadius: 4,
-    borderBottomStyle: "solid",
-    borderBottomWidth: 1,
-    backgroundColor: colors.grey2,
-    borderBottomColor: colors.grey6,
-  }),
-  {
-    padding: `${vars.space[4]} ${vars.space[10]}`,
-  },
-]);
+export const header = sprinkles({
+  all: "unset",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  borderTopRightRadius: 4,
+  borderTopLeftRadius: 4,
+  borderBottomStyle: "solid",
+  borderBottomWidth: 1,
+  backgroundColor: colors.grey2,
+  borderBottomColor: colors.grey6,
+  paddingBlock: 4,
+  paddingInline: 10,
+});
 
 export const fileInfo = sprinkles({
+  all: "unset",
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
--- packages/slice-machine/src/components/CodeBlock/CodeBlock.tsx
+++ packages/slice-machine/src/components/CodeBlock/CodeBlock.tsx
@@ -1,9 +1,9 @@
-import { CSSProperties, ReactNode, useState } from "react";
+import { type ReactNode, useEffect, useState } from "react";
 
 import * as styles from "./CodeBlock.css";
 import { Text, IconButton } from "@prismicio/editor-ui";
 
-import theme from "./theme";
+import { theme } from "./theme";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 import { JavaScript } from "@src/icons/JavaScript";
@@ -45,12 +45,20 @@ const FileIcon = ({ fileName }: { fileName: string }) => {
 const Copy = ({ code }: { code: string }) => {
   const [isCopied, setIsCopied] = useState(false);
 
+  useEffect(() => {
+    if (isCopied) {
+      const timeoutId = setTimeout(() => {
+        setIsCopied(false);
+      }, 1200);
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [isCopied]);
+
   const copy = (): void => {
     void navigator.clipboard.writeText(code).then(() => {
       setIsCopied(true);
-      setTimeout(() => {
-        setIsCopied(false);
-      }, 1200);
     });
   };
   return (
@@ -95,7 +103,7 @@ export const CodeBlock = ({
             borderBottomRightRadius: "4px",
           }}
           children={String(code).replace(/\n$/, "")}
-          style={theme as { [key: string]: CSSProperties }}
+          style={theme}
           language={match[1]}
           PreTag="div"
         />
