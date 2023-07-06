541e80298e57407fd70af6af2d8e08a32c9aad42
Marc McIntosh <marcmcintosh1987@gmail.com>
2023-07-06 14:03:53 +0200

    Mm/academy link (#1030)
    Co-authored-by: Xavier Rutayisire <xavier.rutayisire@gmail.com>

--- packages/slice-machine/lib/consts.ts
+++ packages/slice-machine/lib/consts.ts
@@ -12,9 +12,13 @@ export const API_ID_REGEX = /^[A-Za-z0-9]+(?:_[A-Za-z0-9]+)*$/;
 export const API_ID_RETRO_COMPATIBLE_REGEX =
   /^[A-Za-z0-9]+(?:[_-][A-Za-z0-9]+)*$/;
 
+export const PRISMIC_ACADEMY_URL =
+  "https://prismic.io/academy/prismic-and-nextjs";
+
 // VIDEOS
 export const VIDEO_YOUTUBE_PLAYLIST_LINK =
   "https://youtube.com/playlist?list=PLUVZjQltoA3wnaQudcqQ3qdZNZ6hyfyhH";
+
 export const VIDEO_WHAT_ARE_SLICES = "placeholders/What_are_Slices_mrvome";
 export const VIDEO_WHAT_ARE_CUSTOM_TYPES = "placeholders/CUSTOM_TYPE__0.5.0";
 
