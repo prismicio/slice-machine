1a4f0df3853e0c5c822254bd13d816f22bf03a5a
hypervillain <hugo.p.villain@gmail.com>
2023-07-24 12:18:17 +0200

    skip auth on fetch

--- packages/manager/src/managers/prismicRepository/PrismicRepositoryManager.ts
+++ packages/manager/src/managers/prismicRepository/PrismicRepositoryManager.ts
@@ -134,12 +134,11 @@ export class PrismicRepositoryManager extends BaseManager {
 			`./app/dashboard/repositories/${args.domain}/exists`,
 			API_ENDPOINTS.PrismicWroom,
 		);
-		const res = await fetch(url.toString(), {
-			method: "GET",
-			headers: {
-				"User-Agent": SLICE_MACHINE_USER_AGENT,
-			},
+		const res = await this._fetch({
+			url,
+			skipAuthentication: true,
 		});
+
 		const text = await res.text();
 
 		if (res.ok) {
@@ -448,8 +447,16 @@ export class PrismicRepositoryManager extends BaseManager {
 		body?: unknown;
 		userAgent?: PrismicRepositoryUserAgents;
 		repository?: string;
+		skipAuthentication?: boolean;
 	}): Promise<Response> {
-		const cookies = await this.user.getAuthenticationCookies();
+		let cookies;
+		try {
+			cookies = await this.user.getAuthenticationCookies();
+		} catch (e) {
+			if (!args.skipAuthentication) {
+				throw e;
+			}
+		}
 
 		const extraHeaders: Record<string, string> = {};
 
