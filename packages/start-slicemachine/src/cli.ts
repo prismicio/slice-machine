f69471a3a7aac577cc05ae87ebe2264a22833ccd
Hugo Villain <hugo.p.villain@gmail.com>
2023-07-28 15:47:49 +0200

    Run SliceMachine after init (#1078)

--- packages/start-slicemachine/src/cli.ts
+++ packages/start-slicemachine/src/cli.ts
@@ -18,7 +18,7 @@ const args = mri<Args>(process.argv.slice(2), {
 		version: "v",
 	},
 	default: {
-		open: false,
+		open: true,
 		help: false,
 		version: false,
 	},
@@ -31,7 +31,7 @@ Usage:
     start-slicemachine [options...]
 
 Options:
-    --open         Open Slice Machine automatically
+    --no-open      Don't open Slice Machine automatically
     --port, -p     Specify the port on which to run Slice Machine
     --help, -h     Show help text
     --version, -v  Show version
