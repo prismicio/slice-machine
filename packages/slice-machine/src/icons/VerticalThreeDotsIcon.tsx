3ce99ce809039cfb72c07be7757bd8e8131a2d33
MarcMcIntosh <marcmcintosh1987@gmail.com>
2023-08-10 18:12:21 +0200

    typo(slicemahcine-ui): fix typo in VerticalThreeDots svg

--- packages/slice-machine/src/icons/VerticalThreeDotsIcon.tsx
+++ packages/slice-machine/src/icons/VerticalThreeDotsIcon.tsx
@@ -9,8 +9,8 @@ export const VerticalThreeDotsIcon: FC<SVGProps<SVGSVGElement>> = (props) => (
     {...props}
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M15 11C15 10.4477 15.4477 10 16 10C16.5523 10 17 10.4477 17 11C17 11.5523 16.5523 12 16 12C15.4477 12 15 11.5523 15 11ZM15 16C15 15.4477 15.4477 15 16 15C16.5523 15 17 15.4477 17 16C17 16.5523 16.5523 17 16 17C15.4477 17 15 16.5523 15 16ZM16 20C15.4477 20 15 20.4477 15 21C15 21.5523 15.4477 22 16 22C16.5523 22 17 21.5523 17 21C17 20.4477 16.5523 20 16 20Z"
     />
   </svg>
