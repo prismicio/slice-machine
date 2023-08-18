9574f6e0e511af5bf7428c1a71929c9513c5afe2
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-08-17 17:09:37 -1000

    chore: add `svelte-kit` e2e project

---
+++ e2e-projects/svelte-kit/src/lib/slices/Text/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Content, SliceZone } from '@prismicio/client';
+
+	export let slice: Content.TextSlice;
+	export let slices: SliceZone;
+	export let index: number;
+	export let context: unknown;
+</script>
+
+<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+	Placeholder component for {slice.slice_type} (variation: {slice.variation}) Slices
+</section>
