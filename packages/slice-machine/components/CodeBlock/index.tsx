a14701dfc1f475d17b1bab7ad3bb93af94e3e304
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-08-24 14:42:05 -1000

    fix: remove explicit `svelte` language support in code blocks

--- packages/slice-machine/components/CodeBlock/index.tsx
+++ packages/slice-machine/components/CodeBlock/index.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import { type ThemeUIStyleObject, Flex, useThemeUI } from "theme-ui";
 import hljs from "highlight.js";
 
-import { svelte } from "@lib/hljs/svelte";
-
 const DEFAULT_LANGUAGES = ["javascript", "bash", "xml", "html", "json"];
-
-hljs.registerLanguage("svelte", svelte);
 
 const CodeBlock: React.FC<{
   children: string;
@@ -17,6 +13,7 @@ const CodeBlock: React.FC<{
   Header?: React.FC;
 }> = ({ children, lang, sx, codeStyle, codeClass, Header }) => {
   const { theme } = useThemeUI();
+
   // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   const text = lang
     ? hljs.highlight(children, { language: lang === "vue" ? "html" : lang })
