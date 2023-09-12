1b108a4c787de063456d81c7378339a64d67ba9d
Hugo Villain <hugo.p.villain@gmail.com>
2023-09-12 16:55:50 +0200

    feat(Slice Templates): Hero Slice Template
    Co-authored-by: Xavier Rutayisire <xavier.rutayisire@gmail.com>

--- packages/adapter-next/src/sliceTemplates/CustomerLogos/index.ts
+++ packages/adapter-next/src/sliceTemplates/CustomerLogos/index.ts
@@ -259,270 +259,271 @@ export const createComponentContents = (
 
 	if (isTypeScriptProject) {
 		return stripIndent`
-            import { Content, isFilled, asText } from "@prismicio/client";
-            import {
-                SliceComponentProps,
-                PrismicLink,
-                PrismicImage,
-            } from "@prismicio/react";
-            
-            export type ${pascalName}Props =
-            SliceComponentProps<Content.${pascalName}Slice>;
-            
-            const ${pascalName} = ({ slice }: ${pascalName}Props): JSX.Element => {
-                return (
-                    <section
-                        data-slice-type={slice.slice_type}
-                        data-slice-variation={slice.variation}
-                        className="essential-slice es-bounded es-customer-logos"
-                    >
-                        <div className="es-bounded-content es-customer-logos__content">
-                            {isFilled.richText(slice.primary.eyebrowHeadline) && (
-                                <h2 className="es-customer-logos__heading">
-                                    {asText(slice.primary.eyebrowHeadline)}
-                                </h2>
-                            )}
-                            {slice.items.length > 0 && (
-                                <ul className="es-customer-logos__logos">
-                                    {slice.items.map(
-                                    (item) =>
-                                        isFilled.image(item.image) && (
-                                            <li key={item.image.url} className="es-customer-logos__logo">
-                                                <PrismicLink
-                                                    field={item.link}
-                                                    className="es-customer-logos__link"
-                                                >
-                                                    <PrismicImage
-                                                        field={item.image}
-                                                        height={26}
-                                                        width={160}
-                                                        className="es-customer-logos__logo__link__image"
-                                                    />
-                                                </PrismicLink>
-                                            </li>
-                                        )
-                                    )}
-                                </ul>
-                            )}
-                            <PrismicLink
-                                field={slice.primary.callToActionLink}
-                                className="es-customer-logos__button"
-                            >
-                                {slice.primary.callToActionLabel || "Learn more..."}
-                            </PrismicLink>
-                        </div>
-                        <style>
-                            {\`
-                        .es-bounded {
-                            margin: 0px;
-                            min-width: 0px;
-                            position: relative;
-                            padding: 8vw 1.25rem;
-                        }
+			import { Content, isFilled, asText } from "@prismicio/client";
+			import { PrismicNextLink, PrismicNextImage } from "@prismicio/next";
+			import { SliceComponentProps } from "@prismicio/react";
+			
+			/**
+			 * Props for \`${pascalName}\`.
+			 */
+			export type ${pascalName}Props = SliceComponentProps<Content.${pascalName}Slice>;
+			
+			/**
+			 * Component for "${model.name}" Slices.
+			 */
+			const ${pascalName} = ({ slice }: ${pascalName}Props): JSX.Element => {
+				return (
+					<section
+						data-slice-type={slice.slice_type}
+						data-slice-variation={slice.variation}
+						className="es-bounded es-customer-logos"
+					>
+						<div className="es-bounded-content es-customer-logos__content">
+							{isFilled.richText(slice.primary.eyebrowHeadline) && (
+								<h2 className="es-customer-logos__heading">
+									{asText(slice.primary.eyebrowHeadline)}
+								</h2>
+							)}
+							{slice.items.length > 0 && (
+								<ul className="es-customer-logos__logos">
+									{slice.items.map(
+										(item) =>
+											isFilled.image(item.image) && (
+												<li key={item.image.url} className="es-customer-logos__logo">
+													<PrismicNextLink
+														field={item.link}
+														className="es-customer-logos__link"
+													>
+														<PrismicNextImage
+															field={item.image}
+															height={26}
+															width={160}
+															className="es-customer-logos__logo__link__image"
+														/>
+													</PrismicNextLink>
+												</li>
+											)
+									)}
+								</ul>
+							)}
+							<PrismicNextLink
+								field={slice.primary.callToActionLink}
+								className="es-customer-logos__button"
+							>
+								{slice.primary.callToActionLabel || "Learn more..."}
+							</PrismicNextLink>
+						</div>
+						<style>
+							{\`
+						.es-bounded {
+							margin: 0px;
+							min-width: 0px;
+							position: relative;
+							padding: 8vw 1.25rem;
+						}
 
-                        .es-bounded-content {
-                            min-width: 0px;
-                            max-width: 90%;
-                            margin: 0px auto;
-                            font-family: system-ui, sans-serif;
-                        }
+						.es-bounded-content {
+							min-width: 0px;
+							max-width: 90%;
+							margin: 0px auto;
+							font-family: system-ui, sans-serif;
+						}
 
-                        .es-customer-logos {
-                            background-color: #f4f0ec;
-                            color: #333;
-                        }
-                        
-                        .es-customer-logos__content {
-                            display: grid;
-                            gap: 2rem;
-                            justify-items: center;
-                        }
-                        
-                        .es-customer-logos__heading {
-                            color: #8592e0;
-                            font-size: 1.5rem;
-                            font-weight: 500;
-                            text-align: center;
-                        }
-                        
-                        .es-customer-logos__logos {
-                            display: grid;
-                            grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
-                            grid-column-gap: 1.25rem;
-                            grid-row-gap: 2rem;
-                            align-items: center;
-                            list-style-type: none;
-                            width: 100%;
-                        }
-                        
-                        @media (min-width: 1200px) {
-                            .es-customer-logos__logos {
-                                margin-left: -3rem;
-                            }
-                        }
-                        
-                        .es-customer-logos__logo {
-                            margin: 0;
-                            display: flex;
-                            justify-content: center;
-                        }
-                        
-                        @media (min-width: 1200px) {
-                            .es-customer-logos__logo {
-                                margin-left: 3rem;
-                            }
-                        }
-                        
-                        .es-customer-logos__logo__link__image {
-                            max-width: 10rem;
-                        }
-                        
-                        .es-customer-logos__button {
-                            justify-self: center;
-                        }
-                    \`}
-                        </style>
-                    </section>
-                );
-            };
-            
-            export default ${pascalName};
-        `;
+						.es-customer-logos {
+							background-color: #f4f0ec;
+							color: #333;
+						}
+						
+						.es-customer-logos__content {
+							display: grid;
+							gap: 2rem;
+							justify-items: center;
+						}
+						
+						.es-customer-logos__heading {
+							color: #8592e0;
+							font-size: 1.5rem;
+							font-weight: 500;
+							text-align: center;
+						}
+						
+						.es-customer-logos__logos {
+							display: grid;
+							grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+							grid-column-gap: 1.25rem;
+							grid-row-gap: 2rem;
+							align-items: center;
+							list-style-type: none;
+							width: 100%;
+						}
+						
+						@media (min-width: 1200px) {
+							.es-customer-logos__logos {
+								margin-left: -3rem;
+							}
+						}
+						
+						.es-customer-logos__logo {
+							margin: 0;
+							display: flex;
+							justify-content: center;
+						}
+						
+						@media (min-width: 1200px) {
+							.es-customer-logos__logo {
+								margin-left: 3rem;
+							}
+						}
+						
+						.es-customer-logos__logo__link__image {
+							max-width: 10rem;
+						}
+						
+						.es-customer-logos__button {
+							justify-self: center;
+							text-decoration: underline;
+						}
+					\`}
+						</style>
+					</section>
+				);
+			};
+			
+			export default ${pascalName};
+
+		`;
 	}
 
 	return stripIndent`
-        import { isFilled, asText } from "@prismicio/client";
-        import {
-            PrismicLink,
-            PrismicImage,
-        } from "@prismicio/react";
-        
-        /**
-         * @typedef {import("@prismicio/client").Content.${pascalName}Slice} ${pascalName}Slice
-         * @typedef {import("@prismicio/react").SliceComponentProps<${pascalName}Slice>} ${pascalName}Props
-         * @param {${pascalName}Props}
-         */
-        
-        const ${pascalName} = ({ slice }) => {
-            return (
-                <section
-                    data-slice-type={slice.slice_type}
-                    data-slice-variation={slice.variation}
-                    className="essential-slice es-bounded es-customer-logos"
-                >
-                    <div className="es-bounded-content es-customer-logos__content">
-                        {isFilled.richText(slice.primary.eyebrowHeadline) && (
-                            <h2 className="es-customer-logos__heading">
-                                {asText(slice.primary.eyebrowHeadline)}
-                            </h2>
-                        )}
-                        {slice.items.length > 0 && (
-                            <ul className="es-customer-logos__logos">
-                                {slice.items.map(
-                                (item) =>
-                                    isFilled.image(item.image) && (
-                                    <li key={item.image.url} className="es-customer-logos__logo">
-                                        <PrismicLink
-                                        field={item.link}
-                                        className="es-customer-logos__link"
-                                        >
-                                        <PrismicImage
-                                            field={item.image}
-                                            height={26}
-                                            width={160}
-                                            className="es-customer-logos__logo__link__image"
-                                        />
-                                        </PrismicLink>
-                                    </li>
-                                    )
-                                )}
-                            </ul>
-                        )}
-                        <PrismicLink
-                            field={slice.primary.callToActionLink}
-                            className="es-customer-logos__button"
-                        >
-                            {slice.primary.callToActionLabel || "Learn more..."}
-                        </PrismicLink>
-                    </div>
-                    <style>
-                        {\`
-                    .es-bounded {
-                        margin: 0px;
-                        min-width: 0px;
-                        position: relative;
-                        padding: 8vw 1.25rem;
-                    }
+		import { isFilled, asText } from "@prismicio/client";
+		import { PrismicNextLink, PrismicNextImage } from "@prismicio/next";
+		
+		/**
+		 * @typedef {import("@prismicio/client").Content.${pascalName}Slice} ${pascalName}Slice
+		 * @typedef {import("@prismicio/react").SliceComponentProps<${pascalName}Slice>} ${pascalName}Props
+		 * @param {${pascalName}Props}
+		 */
+		const ${pascalName} = ({ slice }) => {
+			return (
+				<section
+					data-slice-type={slice.slice_type}
+					data-slice-variation={slice.variation}
+					className="es-bounded es-customer-logos"
+				>
+					<div className="es-bounded-content es-customer-logos__content">
+						{isFilled.richText(slice.primary.eyebrowHeadline) && (
+							<h2 className="es-customer-logos__heading">
+								{asText(slice.primary.eyebrowHeadline)}
+							</h2>
+						)}
+						{slice.items.length > 0 && (
+							<ul className="es-customer-logos__logos">
+								{slice.items.map(
+									(item) =>
+										isFilled.image(item.image) && (
+											<li key={item.image.url} className="es-customer-logos__logo">
+												<PrismicNextLink
+													field={item.link}
+													className="es-customer-logos__link"
+												>
+													<PrismicNextImage
+														field={item.image}
+														height={26}
+														width={160}
+														className="es-customer-logos__logo__link__image"
+													/>
+												</PrismicNextLink>
+											</li>
+										)
+								)}
+							</ul>
+						)}
+						<PrismicNextLink
+							field={slice.primary.callToActionLink}
+							className="es-customer-logos__button"
+						>
+							{slice.primary.callToActionLabel || "Learn more..."}
+						</PrismicNextLink>
+					</div>
+					<style>
+						{\`
+					.es-bounded {
+						margin: 0px;
+						min-width: 0px;
+						position: relative;
+						padding: 8vw 1.25rem;
+					}
 
-                    .es-bounded-content {
-                        min-width: 0px;
-                        max-width: 90%;
-                        margin: 0px auto;
-                        font-family: system-ui, sans-serif;
-                    }
+					.es-bounded-content {
+						min-width: 0px;
+						max-width: 90%;
+						margin: 0px auto;
+						font-family: system-ui, sans-serif;
+					}
 
-                    .es-customer-logos {
-                        background-color: #f4f0ec;
-                        color: #333;
-                    }
-                    
-                    .es-customer-logos__content {
-                        display: grid;
-                        gap: 2rem;
-                        justify-items: center;
-                    }
-                    
-                    .es-customer-logos__heading {
-                        color: #8592e0;
-                        font-size: 1.5rem;
-                        font-weight: 500;
-                        text-align: center;
-                    }
-                    
-                    .es-customer-logos__logos {
-                        display: grid;
-                        grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
-                        grid-column-gap: 1.25rem;
-                        grid-row-gap: 2rem;
-                        align-items: center;
-                        list-style-type: none;
-                        width: 100%;
-                    }
-                    
-                    @media (min-width: 1200px) {
-                        .es-customer-logos__logos {
-                            margin-left: -3rem;
-                        }
-                    }
-                    
-                    .es-customer-logos__logo {
-                        margin: 0;
-                        display: flex;
-                        justify-content: center;
-                    }
-                    
-                    @media (min-width: 1200px) {
-                        .es-customer-logos__logo {
-                            margin-left: 3rem;
-                        }
-                    }
-                    
-                    .es-customer-logos__logo__link__image {
-                        max-width: 10rem;
-                    }
-                    
-                    .es-customer-logos__button {
-                        justify-self: center;
-                    }
-                \`}
-                    </style>
-                </section>
-            );
-        };
-        
-        export default ${pascalName};
-        
-    `;
+					.es-customer-logos {
+						background-color: #f4f0ec;
+						color: #333;
+					}
+					
+					.es-customer-logos__content {
+						display: grid;
+						gap: 2rem;
+						justify-items: center;
+					}
+					
+					.es-customer-logos__heading {
+						color: #8592e0;
+						font-size: 1.5rem;
+						font-weight: 500;
+						text-align: center;
+					}
+					
+					.es-customer-logos__logos {
+						display: grid;
+						grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+						grid-column-gap: 1.25rem;
+						grid-row-gap: 2rem;
+						align-items: center;
+						list-style-type: none;
+						width: 100%;
+					}
+					
+					@media (min-width: 1200px) {
+						.es-customer-logos__logos {
+							margin-left: -3rem;
+						}
+					}
+					
+					.es-customer-logos__logo {
+						margin: 0;
+						display: flex;
+						justify-content: center;
+					}
+					
+					@media (min-width: 1200px) {
+						.es-customer-logos__logo {
+							margin-left: 3rem;
+						}
+					}
+					
+					.es-customer-logos__logo__link__image {
+						max-width: 10rem;
+					}
+					
+					.es-customer-logos__button {
+						justify-self: center;
+						text-decoration: underline;
+					}
+				\`}
+					</style>
+				</section>
+			);
+		};
+		
+		export default ${pascalName};
+		
+	`;
 };
 
 export const model: SharedSlice = {
