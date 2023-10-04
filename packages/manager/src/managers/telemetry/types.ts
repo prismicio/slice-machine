70d567422403d46a9facfd92e4e14db66bfd7010
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2023-10-04 11:43:39 +0200

    feat(side menu): Add Invite team side menu link (#1155)

--- packages/manager/src/managers/telemetry/types.ts
+++ packages/manager/src/managers/telemetry/types.ts
@@ -10,6 +10,7 @@ export const SegmentEventType = {
 	sliceSimulator_open: "slice-simulator:open",
 	sliceSimulator_isNotRunning: "slice-simulator:is-not-running",
 	pageView: "page-view",
+	users_invite_button_clicked: "users-invite-button-clicked",
 	openVideoTutorials: "open-video-tutorials",
 	customType_created: "custom-type:created",
 	customType_fieldAdded: "custom-type:field-added",
@@ -37,6 +38,8 @@ export const HumanSegmentEventType = {
 	[SegmentEventType.sliceSimulator_isNotRunning]:
 		"SliceMachine Slice Simulator is not running",
 	[SegmentEventType.pageView]: "SliceMachine Page View",
+	[SegmentEventType.users_invite_button_clicked]:
+		"Slice Machine Users Invite Button Clicked",
 	[SegmentEventType.openVideoTutorials]: "SliceMachine Open Video Tutorials",
 	[SegmentEventType.customType_created]: "SliceMachine Custom Type Created",
 	[SegmentEventType.customType_fieldAdded]:
@@ -123,6 +126,10 @@ type OpenPageSnippetSegmentEvent = SegmentEvent<
 type CopyPageSnippetSegmentEvent = SegmentEvent<
 	typeof SegmentEventType.copy_page_snippet,
 	{ framework: string }
+>;
+
+type UsersInviteButtonClickedSegmentEvent = SegmentEvent<
+	typeof SegmentEventType.users_invite_button_clicked
 >;
 
 type OpenVideoTutorialsSegmentEvent = SegmentEvent<
@@ -230,4 +237,5 @@ export type SegmentEvents =
 	| ChangesLimitReachSegmentEvent
 	| EditorWidgetUsedSegmentEvent
 	| OpenPageSnippetSegmentEvent
-	| CopyPageSnippetSegmentEvent;
+	| CopyPageSnippetSegmentEvent
+	| UsersInviteButtonClickedSegmentEvent;
