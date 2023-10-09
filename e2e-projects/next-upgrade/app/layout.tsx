c78e6915d9894fd35d94c2e491b069b9e753582d
lihbr <lihbr@users.noreply.github.com>
2023-10-09 11:30:30 +0200

    refactor(e2e-projects): review simplification

--- e2e-projects/next-upgrade/app/layout.tsx
+++ e2e-projects/next-upgrade/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 // eslint-disable-next-line react-refresh/only-export-components
