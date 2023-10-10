b7e0b1e8933f814278c2bbd98891c8949da3590b
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-10-10 09:35:21 -1000

    fix(adapter-next): prevent slice template hydration errors due to CSS

--- packages/adapter-next/public/AlternateGrid/javascript.jsx
+++ packages/adapter-next/public/AlternateGrid/javascript.jsx
@@ -78,158 +78,159 @@ const PascalNameToReplace = ({ slice }) => {
 					)}
 				</div>
 			</div>
+			<style
+				dangerouslySetInnerHTML={{
+					__html: `
+            .es-bounded {
+                margin: 0px;
+                min-width: 0px;
+                position: relative;
+                padding: 8vw 1.25rem;
+            }
 
-			<style>
-				{`
-					.es-bounded {
-							margin: 0px;
-							min-width: 0px;
-							position: relative;
-							padding: 8vw 1.25rem;
-					}
+            .es-alternate-grid {
+                font-family: system-ui, sans-serif;
+                background-color: #fff;
+                color: #333;
+            }
 
-					.es-alternate-grid {
-							font-family: system-ui, sans-serif;
-							background-color: #fff;
-							color: #333;
-					}
-					
-					.es-alternate-grid__content {
-							display: grid;
-							gap: 1.5rem;
-							grid-auto-flow: dense;
-					}
-					
-					@media (min-width: 640px) {
-							.es-alternate-grid__content--with-image {
-									grid-template-columns: repeat(2, 1fr);
-							}
-					}
-					
-					@media (min-width: 1200px) {
-							.es-alternate-grid__content--with-image {
-									grid-template-columns: repeat(2, 1fr);
-							}
-					}
-					
-					.es-alternate-grid__image {
-							width: auto;
-							height: auto;
-							max-width: 100%;
-							align-self: center;
-					}
-					
-					.es-alternate-grid__image--left {
-							order: 1;
-					}
+            .es-alternate-grid__content {
+                display: grid;
+                gap: 1.5rem;
+                grid-auto-flow: dense;
+            }
 
-					.es-alternate-grid__image--left + div {
-							order: 2;
-					}
-					
-					.es-alternate-grid__image--right{
-							order: 2;
-					}
+            @media (min-width: 640px) {
+                .es-alternate-grid__content--with-image {
+            	grid-template-columns: repeat(2, 1fr);
+                }
+            }
 
-					.es-alternate-grid__image--right + div {
-							order: 1;
-					}
+            @media (min-width: 1200px) {
+                .es-alternate-grid__content--with-image {
+            	grid-template-columns: repeat(2, 1fr);
+                }
+            }
 
-					.es-alternate-grid__primary-content {
-							display: grid;
-							gap: 2rem;
-					}
-					
-					.es-alternate-grid__primary-content__intro {
-							display: grid;
-							gap: 0.5rem;
-					}
-					
-					.es-alternate-grid__primary-content__intro__eyebrow {
-							color: #8592e0;
-							font-size: 1.15rem;
-							font-weight: 500;
-							margin: 0;
-					}
-					
-					.es-alternate-grid__primary-content__intro__headline {
-							font-size: 1.625rem;
-							font-weight: 700;
-					}
+            .es-alternate-grid__image {
+                width: auto;
+                height: auto;
+                max-width: 100%;
+                align-self: center;
+            }
 
-					.es-alternate-grid__primary-content__intro__headline * {
-							margin: 0;
-					}
-					
-					@media (min-width: 640px) {
-							.es-alternate-grid__primary-content__intro__headline {
-									font-size: 2rem;
-							}
-					}
-					
-					@media (min-width: 1024px) {
-							.es-alternate-grid__primary-content__intro__headline {
-									font-size: 2.5rem;
-							}
-					}
-					
-					@media (min-width: 1200px) {
-							.es-alternate-grid__primary-content__intro__headline {
-									font-size: 2.75rem;
-							}
-					}
-					
-					.es-alternate-grid__primary-content__intro__description {
-							font-size: 1.15rem;
-							max-width: 38rem;
-					}
+            .es-alternate-grid__image--left {
+                order: 1;
+            }
 
-					.es-alternate-grid__primary-content__intro__description > p {
-							margin: 0;
-					}
-					
-					@media (min-width: 1200px) {
-							.es-alternate-grid__primary-content__intro__description {
-									font-size: 1.4rem;
-							}
-					}
-					
-					.es-alternate-grid__primary-content__items {
-							display: grid;
-							gap: 2rem;
-					}
-					
-					@media (min-width: 640px) {
-							.es-alternate-grid__primary-content__items {
-									grid-template-columns: repeat(2, 1fr);
-							}
-					}
-					
-					.es-alternate-grid__item {
-							display: grid;
-							align-content: start;
-					}
-					
-					.es-alternate-grid__item__heading {
-							font-weight: 700;
-							font-size: 1.17rem;
-							margin-top: 0;
-							margin-bottom: .5rem;
-					}
-					
-					.es-alternate-grid__item__heading * {
-							margin: 0;
-					}
-					
-					.es-alternate-grid__item__description {
-							font-size: 0.9rem;
-					}
-					
-					.es-alternate-grid__item__description * {
-							margin: 0;
-					}
-			`}
-			</style>
+            .es-alternate-grid__image--left + div {
+                order: 2;
+            }
+
+            .es-alternate-grid__image--right{
+                order: 2;
+            }
+
+            .es-alternate-grid__image--right + div {
+                order: 1;
+            }
+
+            .es-alternate-grid__primary-content {
+                display: grid;
+                gap: 2rem;
+            }
+
+            .es-alternate-grid__primary-content__intro {
+                display: grid;
+                gap: 0.5rem;
+            }
+
+            .es-alternate-grid__primary-content__intro__eyebrow {
+                color: #8592e0;
+                font-size: 1.15rem;
+                font-weight: 500;
+                margin: 0;
+            }
+
+            .es-alternate-grid__primary-content__intro__headline {
+                font-size: 1.625rem;
+                font-weight: 700;
+            }
+
+            .es-alternate-grid__primary-content__intro__headline * {
+                margin: 0;
+            }
+
+            @media (min-width: 640px) {
+                .es-alternate-grid__primary-content__intro__headline {
+            	font-size: 2rem;
+                }
+            }
+
+            @media (min-width: 1024px) {
+                .es-alternate-grid__primary-content__intro__headline {
+            	font-size: 2.5rem;
+                }
+            }
+
+            @media (min-width: 1200px) {
+                .es-alternate-grid__primary-content__intro__headline {
+            	font-size: 2.75rem;
+                }
+            }
+
+            .es-alternate-grid__primary-content__intro__description {
+                font-size: 1.15rem;
+                max-width: 38rem;
+            }
+
+            .es-alternate-grid__primary-content__intro__description > p {
+                margin: 0;
+            }
+
+            @media (min-width: 1200px) {
+                .es-alternate-grid__primary-content__intro__description {
+            	font-size: 1.4rem;
+                }
+            }
+
+            .es-alternate-grid__primary-content__items {
+                display: grid;
+                gap: 2rem;
+            }
+
+            @media (min-width: 640px) {
+                .es-alternate-grid__primary-content__items {
+            	grid-template-columns: repeat(2, 1fr);
+                }
+            }
+
+            .es-alternate-grid__item {
+                display: grid;
+                align-content: start;
+            }
+
+            .es-alternate-grid__item__heading {
+                font-weight: 700;
+                font-size: 1.17rem;
+                margin-top: 0;
+                margin-bottom: .5rem;
+            }
+
+            .es-alternate-grid__item__heading * {
+                margin: 0;
+            }
+
+            .es-alternate-grid__item__description {
+                font-size: 0.9rem;
+            }
+
+            .es-alternate-grid__item__description * {
+                margin: 0;
+            }
+          `,
+				}}
+			/>
 		</section>
 	);
 };
--- packages/adapter-next/public/CustomerLogos/javascript.jsx
+++ packages/adapter-next/public/CustomerLogos/javascript.jsx
@@ -48,83 +48,84 @@ const PascalNameToReplace = ({ slice }) => {
 					{slice.primary.callToActionLabel || "Learn more..."}
 				</PrismicNextLink>
 			</div>
+			<style
+				dangerouslySetInnerHTML={{
+					__html: `
+            .es-bounded {
+              margin: 0px;
+              min-width: 0px;
+              position: relative;
+              padding: 8vw 1.25rem;
+            }
 
-			<style>
-				{`
-					.es-bounded {
-							margin: 0px;
-							min-width: 0px;
-							position: relative;
-							padding: 8vw 1.25rem;
-					}
+            .es-bounded__content {
+              min-width: 0px;
+              max-width: 90%;
+              margin: 0px auto;
+            }
 
-					.es-bounded__content {
-							min-width: 0px;
-							max-width: 90%;
-							margin: 0px auto;
-					}
+            .es-customer-logos {
+              font-family: system-ui, sans-serif;
+              background-color: #f4f0ec;
+              color: #333;
+            }
 
-					.es-customer-logos {
-							font-family: system-ui, sans-serif;
-							background-color: #f4f0ec;
-							color: #333;
-					}
-					
-					.es-customer-logos__content {
-							display: grid;
-							gap: 2rem;
-							justify-items: center;
-					}
-					
-					.es-customer-logos__heading {
-							color: #8592e0;
-							font-size: 1.5rem;
-							font-weight: 500;
-							text-align: center;
-					}
+            .es-customer-logos__content {
+              display: grid;
+              gap: 2rem;
+              justify-items: center;
+            }
 
-					.es-customer-logos__heading * {
-						margin: 0;
-					}			
-					
-					.es-customer-logos__logos {
-							display: grid;
-							grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
-							grid-column-gap: 1.25rem;
-							grid-row-gap: 2rem;
-							align-items: center;
-							list-style-type: none;
-							width: 100%;
-					}
-					
-					@media (min-width: 1200px) {
-							.es-customer-logos__logos {
-									margin-left: -3rem;
-							}
-					}
-					
-					.es-customer-logos__logo {
-							margin: 0;
-							display: flex;
-							justify-content: center;
-					}
-					
-					@media (min-width: 1200px) {
-							.es-customer-logos__logo {
-									margin-left: 3rem;
-							}
-					}
-					
-					.es-customer-logos__logo__link__image {
-							max-width: 10rem;
-					}
-					
-					.es-customer-logos__button {
-							justify-self: center;
-							text-decoration: underline;
-					}
-			`}
-			</style>
+            .es-customer-logos__heading {
+              color: #8592e0;
+              font-size: 1.5rem;
+              font-weight: 500;
+              text-align: center;
+            }
+
+            .es-customer-logos__heading * {
+            	margin: 0;
+            }
+
+            .es-customer-logos__logos {
+              display: grid;
+              grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+              grid-column-gap: 1.25rem;
+              grid-row-gap: 2rem;
+              align-items: center;
+              list-style-type: none;
+              width: 100%;
+            }
+
+            @media (min-width: 1200px) {
+              .es-customer-logos__logos {
+            		margin-left: -3rem;
+              }
+            }
+
+            .es-customer-logos__logo {
+              margin: 0;
+              display: flex;
+              justify-content: center;
+            }
+
+            @media (min-width: 1200px) {
+              .es-customer-logos__logo {
+            		margin-left: 3rem;
+              }
+            }
+
+            .es-customer-logos__logo__link__image {
+              max-width: 10rem;
+            }
+
+            .es-customer-logos__button {
+              justify-self: center;
+              text-decoration: underline;
+            }
+          `,
+				}}
+			/>
 		</section>
 	);
 };
