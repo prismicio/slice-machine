165397e0d03bf3851788874ff1ad35fee0ec1ee6
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-10-10 22:02:13 -1000

    fix(adapter-next): prevent slice template hydration errors due to CSS (DT-1673) (#1163)

--- packages/adapter-next/public/CallToAction/javascript.jsx
+++ packages/adapter-next/public/CallToAction/javascript.jsx
@@ -46,89 +46,90 @@ const PascalNameToReplace = ({ slice }) => {
 					</PrismicNextLink>
 				)}
 			</div>
+			<style
+				dangerouslySetInnerHTML={{
+					__html: `
+            .es-bounded {
+              padding: 8vw 2rem;
+            }
 
-			<style>
-				{`
-					.es-bounded {
-						padding: 8vw 2rem;
-					}
-					
-					.es-bounded__content {
-						margin-left: auto;
-						margin-right: auto;
-					}
-					
-					@media screen and (min-width: 640px) {
-						.es-bounded__content {
-							max-width: 90%;
-						}
-					}
-					
-					@media screen and (min-width: 896px) {
-						.es-bounded__content {
-							max-width: 80%;
-						}
-					}
-					
-					@media screen and (min-width: 1280px) {
-						.es-bounded__content {
-							max-width: 75%;
-						}
-					}
-					
-					.es-call-to-action {
-						font-family: system-ui, sans-serif;
-						background-color: #fff;
-						color: #333;
-					}
-					
-					.es-call-to-action__image {
-						max-width: 14rem;
-						height: auto;
-						width: auto;
-						justify-self: ${alignment};
-					}
-					
-					.es-call-to-action__content {
-						display: grid;
-						gap: 1rem;
-						justify-items: ${alignment};
-					}
-					
-					.es-call-to-action__content__heading {
-						font-size: 2rem;
-						font-weight: 700;
-						text-align: ${alignment};
-					}
+            .es-bounded__content {
+              margin-left: auto;
+              margin-right: auto;
+            }
 
-					.es-call-to-action__content__heading * {
-						margin: 0;
-					}
-					
-					.es-call-to-action__content__paragraph {
-						font-size: 1.15rem;
-						max-width: 38rem;
-						text-align: ${alignment};
-					}
-					
-					.es-call-to-action__button {
-						justify-self: ${alignment};
-						border-radius: 0.25rem;
-						display: inline-block;
-						font-size: 0.875rem;
-						line-height: 1.3;
-						padding: 1rem 2.625rem;
-						text-align: ${alignment};
-						transition: background-color 100ms linear;
-						background-color: #16745f;
-						color: #fff;
-					}
-					
-					.es-call-to-action__button:hover {
-						background-color: #0d5e4c;
-					}
-				`}
-			</style>
+            @media screen and (min-width: 640px) {
+              .es-bounded__content {
+                max-width: 90%;
+              }
+            }
+
+            @media screen and (min-width: 896px) {
+              .es-bounded__content {
+                max-width: 80%;
+              }
+            }
+
+            @media screen and (min-width: 1280px) {
+              .es-bounded__content {
+                max-width: 75%;
+              }
+            }
+
+            .es-call-to-action {
+              font-family: system-ui, sans-serif;
+              background-color: #fff;
+              color: #333;
+            }
+
+            .es-call-to-action__image {
+              max-width: 14rem;
+              height: auto;
+              width: auto;
+              justify-self: ${alignment};
+            }
+
+            .es-call-to-action__content {
+              display: grid;
+              gap: 1rem;
+              justify-items: ${alignment};
+            }
+
+            .es-call-to-action__content__heading {
+              font-size: 2rem;
+              font-weight: 700;
+              text-align: ${alignment};
+            }
+
+            .es-call-to-action__content__heading * {
+              margin: 0;
+            }
+
+            .es-call-to-action__content__paragraph {
+              font-size: 1.15rem;
+              max-width: 38rem;
+              text-align: ${alignment};
+            }
+
+            .es-call-to-action__button {
+              justify-self: ${alignment};
+              border-radius: 0.25rem;
+              display: inline-block;
+              font-size: 0.875rem;
+              line-height: 1.3;
+              padding: 1rem 2.625rem;
+              text-align: ${alignment};
+              transition: background-color 100ms linear;
+              background-color: #16745f;
+              color: #fff;
+            }
+
+            .es-call-to-action__button:hover {
+              background-color: #0d5e4c;
+            }
+          `,
+				}}
+			/>
 		</section>
 	);
 };
