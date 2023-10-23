cc61ae69b0717301361bac1f7ffb4072a854b6bd
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2023-10-23 17:33:56 +0200

    [1.19.0] fix(init): Rework init end message (#1186)

--- packages/init/src/SliceMachineInitProcess.ts
+++ packages/init/src/SliceMachineInitProcess.ts
@@ -198,106 +198,38 @@ export class SliceMachineInitProcess {
 		console.log(
 			`\n${chalk.bgGreen(` ${chalk.bold.white("Slice Machine")} `)} ${chalk.dim(
 				"→",
-			)} Initialization successful!`,
+			)} Initialization successful!
+			
+Continue with next steps in Slice Machine.
+`,
 		);
 
 		try {
-			const runSmCommand = this.context.projectInitialization?.patchedScript
-				? await getRunScriptCommand({
-						agent: this.context.packageManager || "npm",
-						script: "slicemachine",
-				  })
-				: await getExecuteCommand({
-						agent: this.context.packageManager || "npm",
-						script: "start-slicemachine",
-				  });
+			if (this.options.startSlicemachine) {
+				const runSmCommand = this.context.projectInitialization?.patchedScript
+					? await getRunScriptCommand({
+							agent: this.context.packageManager || "npm",
+							script: "slicemachine",
+					  })
+					: await getExecuteCommand({
+							agent: this.context.packageManager || "npm",
+							script: "start-slicemachine",
+					  });
 
-			const runProjectCommand = await getRunScriptCommand({
-				agent: this.context.packageManager || "npm",
-				script: "dev",
-			});
-
-			const apiEndpoints = this.manager.getAPIEndpoints();
-			const wroomHost = new URL(apiEndpoints.PrismicWroom).host;
-
-			const dashboardURL = new URL(
-				`https://${this.context.repository.domain}.${wroomHost}`,
-			)
-				.toString()
-				.replace(/\/$/, "");
-			const apiURL = new URL(
-				"./api/v2",
-				`https://${this.context.repository.domain}.cdn.${wroomHost}`,
-			).toString();
-
-			// We prefer to manually allow console logs despite the app being a CLI to catch wild/unwanted console logs better
-			// eslint-disable-next-line no-console
-			console.log(`
-  YOUR REPOSITORY
-    Page Builder         ${chalk.cyan(dashboardURL)}
-    API                  ${chalk.cyan(apiURL)}
-
-  RESOURCES
-    Documentation        ${chalk.cyan(
-			this.context.framework.prismicDocumentation,
-		)}
-    Getting help         ${chalk.cyan("https://community.prismic.io")}
-
-  GETTING STARTED
-    Run Slice Machine    ${chalk.cyan(runSmCommand)}
-    Run your project     ${chalk.cyan(runProjectCommand)}
-	`);
-
-			if (this.options.starter && this.options.repository) {
-				const { openDashboard } = await prompt<boolean, "openDashboard">({
-					type: "confirm",
-					name: "openDashboard",
-					message: "Would you like to open your repository?",
-					initial: true,
-				});
-
-				if (openDashboard) {
-					open(dashboardURL);
-				}
-			} else if (this.options.startSlicemachine) {
-				const pkgJSONPath = path.join(this.manager.cwd, "package.json");
-				const pkg = JSON.parse(await fs.readFile(pkgJSONPath, "utf-8"));
-				const scripts = pkg.scripts || {};
-
-				const finalSmCommand = (() => {
-					if (
-						scripts["dev"] &&
-						typeof scripts["dev"] === "string" &&
-						scripts["dev"].includes(":slicemachine")
-					) {
-						return {
-							command: runProjectCommand,
-							message: `Would you like to run your project and Slice Machine concurrently (${runProjectCommand})?`,
-						};
-					}
-
-					return {
-						command: runSmCommand,
-						message: `Would you like to run Slice Machine (${runSmCommand})?`,
-					};
-				})();
 				const { startSlicemachine } = await prompt<
 					boolean,
 					"startSlicemachine"
 				>({
 					type: "confirm",
 					name: "startSlicemachine",
-					message: finalSmCommand.message,
+					message: `Run Slice Machine (${chalk.cyan(runSmCommand)})?`,
 					initial: true,
 				});
 
 				if (startSlicemachine) {
-					const commandReturnValue = await execaCommand(
-						finalSmCommand.command,
-						{
-							env: { FORCE_COLOR: "true" },
-						},
-					).pipeStdout?.(process.stdout);
+					const commandReturnValue = await execaCommand(runSmCommand, {
+						env: { FORCE_COLOR: "true" },
+					}).pipeStdout?.(process.stdout);
 					if (commandReturnValue !== undefined) {
 						// eslint-disable-next-line no-console
 						console.log(commandReturnValue.stdout);
