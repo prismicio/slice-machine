49d6e493e9bf176e1bc2618bba52fcb835c48193
lihbr <lihbr@users.noreply.github.com>
2023-10-24 18:06:41 +0200

    fix(adapter-nuxt): nuxt 3.8 compatibility

--- packages/adapter-nuxt/src/hooks/slice-create.ts
+++ packages/adapter-nuxt/src/hooks/slice-create.ts
@@ -41,7 +41,7 @@ const createComponentFile = async ({
 	} else if (isTypeScriptProject) {
 		contents = stripIndent`
 			<script setup lang="ts">
-			import { Content } from "@prismicio/client";
+			import { type Content } from "@prismicio/client";
 
 			// The array passed to \`getSliceComponentProps\` is purely optional.
 			// Consider it as a visual hint for you when templating your slice.
