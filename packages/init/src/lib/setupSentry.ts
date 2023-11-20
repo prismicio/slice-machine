5f06b57d11ff6451d160c88345f1fbf60863d7fc
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2023-11-20 14:03:51 +0100

    chore(sentry): Prevent Sentry error for quota reached

--- packages/init/src/lib/setupSentry.ts
+++ packages/init/src/lib/setupSentry.ts
@@ -19,8 +19,8 @@ export function setupSentry(): void {
 		environment: isStableVersion
 			? import.meta.env.MODE || "production"
 			: "alpha",
-		// Increase the default truncation length of 250 to 12500 (x50)
+		// Increase the default truncation length of 250 to 2500 (x10)
 		// to have enough details in Sentry
-		maxValueLength: 12_500,
+		maxValueLength: 2_500,
 	});
 }
