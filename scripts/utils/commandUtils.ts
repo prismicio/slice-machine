82e1f8e186823900a342750ecd6b5cefcf0f3de0
Baptiste Morelle <bapmrl@users.noreply.github.com>
2023-12-05 17:49:14 +0100

    chore: add publish script

---
+++ scripts/utils/commandUtils.ts
@@ -0,0 +1,56 @@
+import chalk from "chalk";
+import {
+  execa,
+  type ExecaChildProcess,
+  type Options as ExecaOptions,
+  type ExecaReturnValue,
+} from "execa";
+
+export class CommandError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export function handleUncaughtException(error: Error): void {
+  if (error instanceof CommandError) {
+    console.error(chalk.stderr.red(error.message));
+    process.exitCode = 2;
+  } else {
+    console.error(error);
+    process.exitCode = 1;
+  }
+}
+
+export function exec(
+  file: string,
+  args?: string[],
+  options?: ExecaOptions & { dryRun?: false },
+): ExecaChildProcess;
+export async function exec(
+  file: string,
+  args?: string[],
+  options?: ExecaOptions & { dryRun: boolean },
+): Promise<void>;
+export async function exec(
+  file: string,
+  args?: string[],
+  options: ExecaOptions & { dryRun?: boolean } = {},
+): Promise<ExecaReturnValue | void> {
+  const { dryRun, ...execaOptions } = options;
+  if (dryRun === true) {
+    console.log(chalk.blue(escapeCommand(file, args)));
+  } else {
+    return await execa(file, args, execaOptions);
+  }
+}
+
+// Adapted from: https://github.com/sindresorhus/execa/blob/ec380db676f8380320b5c41eb444135220e241ca/lib/command.js#L24.
+function escapeCommand(file: string, args: string[] = []): string {
+  return [file, ...args].map(escapeArg).join(" ");
+}
+
+// Adapted from: https://github.com/sindresorhus/execa/blob/ec380db676f8380320b5c41eb444135220e241ca/lib/command.js#L12-L20.
+function escapeArg(arg: string): string {
+  return /^[\w-.]+$/.test(arg) ? arg : `"${arg.replace(/"/g, '\\"')}"`;
+}
