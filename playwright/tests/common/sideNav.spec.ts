85da77e0d3c4dc34b79e408c99d796404e314c95
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2023-12-27 14:49:19 +0100

    test(playwright): Remove describe in test files (#1256)

--- playwright/tests/common/sideNav.spec.ts
+++ playwright/tests/common/sideNav.spec.ts
@@ -3,146 +3,138 @@ import { expect } from "@playwright/test";
 import { test } from "../../fixtures";
 import { generateLibraries, generateTypes } from "../../mocks";
 
-test.describe("Side nav", () => {
-  test.run()(
-    "I can navigate through all menu entries",
-    async ({
-      sliceMachinePage,
-      pageTypesTablePage,
-      customTypesTablePage,
-      slicesListPage,
-      changesPage,
-      changelogPage,
-    }) => {
-      await sliceMachinePage.gotoDefaultPage();
+test.run()(
+  "I can navigate through all menu entries",
+  async ({
+    sliceMachinePage,
+    pageTypesTablePage,
+    customTypesTablePage,
+    slicesListPage,
+    changesPage,
+    changelogPage,
+  }) => {
+    await sliceMachinePage.gotoDefaultPage();
 
-      await pageTypesTablePage.menu.pageTypesLink.click();
-      await expect(pageTypesTablePage.breadcrumbLabel).toBeVisible();
-      expect(await sliceMachinePage.page.title()).toContain(
-        "Page types - Slice Machine",
-      );
+    await pageTypesTablePage.menu.pageTypesLink.click();
+    await expect(pageTypesTablePage.breadcrumbLabel).toBeVisible();
+    expect(await sliceMachinePage.page.title()).toContain(
+      "Page types - Slice Machine",
+    );
 
-      await customTypesTablePage.menu.customTypesLink.click();
-      await expect(customTypesTablePage.breadcrumbLabel).toBeVisible();
-      expect(await sliceMachinePage.page.title()).toContain(
-        "Custom types - Slice Machine",
-      );
+    await customTypesTablePage.menu.customTypesLink.click();
+    await expect(customTypesTablePage.breadcrumbLabel).toBeVisible();
+    expect(await sliceMachinePage.page.title()).toContain(
+      "Custom types - Slice Machine",
+    );
 
-      await slicesListPage.menu.slicesLink.click();
-      await expect(slicesListPage.breadcrumbLabel).toBeVisible();
-      expect(await sliceMachinePage.page.title()).toContain(
-        "Slices - Slice Machine",
-      );
+    await slicesListPage.menu.slicesLink.click();
+    await expect(slicesListPage.breadcrumbLabel).toBeVisible();
+    expect(await sliceMachinePage.page.title()).toContain(
+      "Slices - Slice Machine",
+    );
 
-      await changesPage.menu.changesLink.click();
-      await expect(changesPage.breadcrumbLabel).toBeVisible();
-      expect(await sliceMachinePage.page.title()).toContain(
-        "Changes - Slice Machine",
-      );
+    await changesPage.menu.changesLink.click();
+    await expect(changesPage.breadcrumbLabel).toBeVisible();
+    expect(await sliceMachinePage.page.title()).toContain(
+      "Changes - Slice Machine",
+    );
 
-      await changelogPage.menu.changelogLink.click();
-      await expect(changelogPage.breadcrumbLabel).toBeVisible();
-      expect(await sliceMachinePage.page.title()).toContain(
-        "Changelog - Slice Machine",
-      );
-    },
-  );
+    await changelogPage.menu.changelogLink.click();
+    await expect(changelogPage.breadcrumbLabel).toBeVisible();
+    expect(await sliceMachinePage.page.title()).toContain(
+      "Changelog - Slice Machine",
+    );
+  },
+);
 
-  test.run()(
-    "I access the changelog from Slice Machine version",
-    async ({ pageTypesTablePage, changelogPage, procedures }) => {
-      procedures.mock(
-        "versions.getRunningSliceMachineVersion",
-        () => "1.0.42",
-        { execute: false },
-      );
+test.run()(
+  "I access the changelog from Slice Machine version",
+  async ({ pageTypesTablePage, changelogPage, procedures }) => {
+    procedures.mock("versions.getRunningSliceMachineVersion", () => "1.0.42", {
+      execute: false,
+    });
 
-      await pageTypesTablePage.goto();
-      await pageTypesTablePage.menu.getAppVersion("1.0.42").click();
+    await pageTypesTablePage.goto();
+    await pageTypesTablePage.menu.getAppVersion("1.0.42").click();
 
-      await expect(changelogPage.breadcrumbLabel).toBeVisible();
-    },
-  );
+    await expect(changelogPage.breadcrumbLabel).toBeVisible();
+  },
+);
 
-  test.run()(
-    "I can see the updates available warning and access changelog from it",
-    async ({ pageTypesTablePage, changelogPage, procedures }) => {
-      procedures.mock(
-        "versions.checkIsSliceMachineUpdateAvailable",
-        () => true,
-        { execute: false },
-      );
+test.run()(
+  "I can see the updates available warning and access changelog from it",
+  async ({ pageTypesTablePage, changelogPage, procedures }) => {
+    procedures.mock("versions.checkIsSliceMachineUpdateAvailable", () => true, {
+      execute: false,
+    });
 
-      await pageTypesTablePage.goto();
-      await expect(pageTypesTablePage.menu.updatesAvailableTitle).toBeVisible();
-      await pageTypesTablePage.menu.updatesAvailableButton.click();
+    await pageTypesTablePage.goto();
+    await expect(pageTypesTablePage.menu.updatesAvailableTitle).toBeVisible();
+    await pageTypesTablePage.menu.updatesAvailableButton.click();
 
-      await expect(changelogPage.breadcrumbLabel).toBeVisible();
-    },
-  );
+    await expect(changelogPage.breadcrumbLabel).toBeVisible();
+  },
+);
 
-  test.run()(
-    "I cannot see the updates available warning",
-    async ({ pageTypesTablePage, procedures }) => {
-      procedures.mock("versions.checkIsAdapterUpdateAvailable", () => false, {
-        execute: false,
-      });
+test.run()(
+  "I cannot see the updates available warning",
+  async ({ pageTypesTablePage, procedures }) => {
+    procedures.mock("versions.checkIsAdapterUpdateAvailable", () => false, {
+      execute: false,
+    });
 
-      procedures.mock(
-        "versions.checkIsSliceMachineUpdateAvailable",
-        () => false,
-        { execute: false },
-      );
+    procedures.mock(
+      "versions.checkIsSliceMachineUpdateAvailable",
+      () => false,
+      { execute: false },
+    );
 
-      await pageTypesTablePage.goto();
-      await expect(pageTypesTablePage.menu.appVersion).toBeVisible();
-      await expect(
-        pageTypesTablePage.menu.updatesAvailableTitle,
-      ).not.toBeVisible();
-    },
-  );
+    await pageTypesTablePage.goto();
+    await expect(pageTypesTablePage.menu.appVersion).toBeVisible();
+    await expect(
+      pageTypesTablePage.menu.updatesAvailableTitle,
+    ).not.toBeVisible();
+  },
+);
 
-  test.run({
-    onboarded: false,
-    reduxStorage: {
-      lastSyncChange: new Date(new Date().getTime() - 3600000).getTime(),
-    },
-  })(
-    "I can close the tutorial video tooltip and it stays close",
-    async ({ sliceMachinePage, procedures }) => {
-      const libraries = generateLibraries({ slicesCount: 1 });
+test.run({
+  onboarded: false,
+  reduxStorage: {
+    lastSyncChange: new Date(new Date().getTime() - 3600000).getTime(),
+  },
+})(
+  "I can close the tutorial video tooltip and it stays close",
+  async ({ sliceMachinePage, procedures }) => {
+    const libraries = generateLibraries({ slicesCount: 1 });
 
-      // We mock a page type with a slice that is a requirement for the review dialog
-      procedures.mock("getState", ({ data }) => ({
-        ...(data as Record<string, unknown>),
-        libraries,
-        customTypes: generateTypes({ typesCount: 1, libraries }),
-        remoteCustomTypes: [],
-        remoteSlices: [],
-        clientError: undefined,
-      }));
+    // We mock a page type with a slice that is a requirement for the review dialog
+    procedures.mock("getState", ({ data }) => ({
+      ...(data as Record<string, unknown>),
+      libraries,
+      customTypes: generateTypes({ typesCount: 1, libraries }),
+      remoteCustomTypes: [],
+      remoteSlices: [],
+      clientError: undefined,
+    }));
 
-      await sliceMachinePage.gotoDefaultPage();
+    await sliceMachinePage.gotoDefaultPage();
 
-      // We close the in app guide and review dialogs that are requirements for the tutorial tooltip display
-      await sliceMachinePage.inAppGuideDialog.closeButton.click();
-      await sliceMachinePage.reviewDialog.closeButton.click();
+    // We close the in app guide and review dialogs that are requirements for the tutorial tooltip display
+    await sliceMachinePage.inAppGuideDialog.closeButton.click();
+    await sliceMachinePage.reviewDialog.closeButton.click();
 
-      // Then tutorial tooltip open after the review dialog
-      await expect(
-        sliceMachinePage.menu.tutorialVideoTooltipTitle,
-      ).toBeVisible();
-      await sliceMachinePage.menu.tutorialVideoTooltipCloseButton.click();
-      await expect(
-        sliceMachinePage.menu.tutorialVideoTooltipTitle,
-      ).not.toBeVisible();
+    // Then tutorial tooltip open after the review dialog
+    await expect(sliceMachinePage.menu.tutorialVideoTooltipTitle).toBeVisible();
+    await sliceMachinePage.menu.tutorialVideoTooltipCloseButton.click();
+    await expect(
+      sliceMachinePage.menu.tutorialVideoTooltipTitle,
+    ).not.toBeVisible();
 
-      await sliceMachinePage.page.reload();
-      await expect(sliceMachinePage.menu.pageTypesLink).toBeVisible();
+    await sliceMachinePage.page.reload();
+    await expect(sliceMachinePage.menu.pageTypesLink).toBeVisible();
 
-      await expect(
-        sliceMachinePage.menu.tutorialVideoTooltipTitle,
-      ).not.toBeVisible();
-    },
-  );
-});
+    await expect(
+      sliceMachinePage.menu.tutorialVideoTooltipTitle,
+    ).not.toBeVisible();
+  },
+);
--- playwright/tests/slices/sliceBuilder.spec.ts
+++ playwright/tests/slices/sliceBuilder.spec.ts
@@ -3,105 +3,92 @@ import { expect } from "@playwright/test";
 import { test } from "../../fixtures";
 import { generateRandomId } from "../../utils/generateRandomId";
 
-test.describe("Slice builder", () => {
-  test.run()(
-    "I can add a new variation",
-    async ({ slice, sliceBuilderPage }) => {
-      await sliceBuilderPage.goto(slice.name);
-      await expect(sliceBuilderPage.variationCards).toHaveCount(1);
+test.run()("I can add a new variation", async ({ slice, sliceBuilderPage }) => {
+  await sliceBuilderPage.goto(slice.name);
+  await expect(sliceBuilderPage.variationCards).toHaveCount(1);
 
-      await sliceBuilderPage.openAddVariationDialog();
-      const variationName = `Variation ${generateRandomId()}`;
-      await sliceBuilderPage.addVariationDialog.addVariation(variationName);
+  await sliceBuilderPage.openAddVariationDialog();
+  const variationName = `Variation ${generateRandomId()}`;
+  await sliceBuilderPage.addVariationDialog.addVariation(variationName);
 
-      await expect(sliceBuilderPage.variationCards).toHaveCount(2);
-      await expect(
-        sliceBuilderPage.getVariationCard(slice.name, variationName),
-      ).toBeVisible();
-    },
-  );
-
-  test.run()(
-    "I can rename a variation",
-    async ({ slice, sliceBuilderPage }) => {
-      await sliceBuilderPage.goto(slice.name);
-
-      const defaultVariationName = "Default";
-      await sliceBuilderPage.openVariationActionMenu(
-        slice.name,
-        defaultVariationName,
-        "Rename",
-      );
-      const newVariationName = `${defaultVariationName}Renamed`;
-      await sliceBuilderPage.renameVariationDialog.renameVariation(
-        newVariationName,
-      );
-
-      await expect(sliceBuilderPage.variationCards).toHaveCount(1);
-      await expect(
-        sliceBuilderPage.getVariationCard(slice.name, defaultVariationName),
-      ).not.toBeVisible();
-      await expect(
-        sliceBuilderPage.getVariationCard(slice.name, newVariationName),
-      ).toBeVisible();
-    },
-  );
-
-  test.run()(
-    "I can delete a variation",
-    async ({ slice, sliceBuilderPage }) => {
-      await sliceBuilderPage.goto(slice.name);
-      await sliceBuilderPage.openAddVariationDialog();
-      const variationName = `Variation ${generateRandomId()}`;
-      await sliceBuilderPage.addVariationDialog.addVariation(variationName);
-
-      await sliceBuilderPage.openVariationActionMenu(
-        slice.name,
-        variationName,
-        "Delete",
-      );
-      await sliceBuilderPage.deleteVariationDialog.deleteVariation();
-
-      await expect(sliceBuilderPage.variationCards).toHaveCount(1);
-      await expect(
-        sliceBuilderPage.getVariationCard(slice.name, variationName),
-      ).not.toBeVisible();
-    },
-  );
-
-  test.run()(
-    "I can add a static field to the builder",
-    async ({ slice, sliceBuilderPage }) => {
-      await sliceBuilderPage.goto(slice.name);
-
-      await expect(sliceBuilderPage.staticZoneListItem).toHaveCount(0);
-
-      await sliceBuilderPage.addStaticField(
-        "Rich Text",
-        "Description",
-        "description",
-      );
-
-      await expect(sliceBuilderPage.staticZoneListItem).toHaveCount(1);
-    },
-  );
-
-  test.run({ onboarded: false })(
-    "I can close the simulator tooltip and it stays close",
-    async ({ slice, sliceBuilderPage }) => {
-      await sliceBuilderPage.goto(slice.name);
-
-      // Simulator tooltip should open automatically
-      await expect(sliceBuilderPage.simulateTooltipTitle).toBeVisible();
-      await sliceBuilderPage.simulateTooltipCloseButton.click();
-      await expect(sliceBuilderPage.simulateTooltipTitle).not.toBeVisible();
-
-      await sliceBuilderPage.page.reload();
-      await expect(
-        sliceBuilderPage.getBreadcrumbLabel(slice.name),
-      ).toBeVisible();
-
-      await expect(sliceBuilderPage.simulateTooltipTitle).not.toBeVisible();
-    },
-  );
+  await expect(sliceBuilderPage.variationCards).toHaveCount(2);
+  await expect(
+    sliceBuilderPage.getVariationCard(slice.name, variationName),
+  ).toBeVisible();
 });
+
+test.run()("I can rename a variation", async ({ slice, sliceBuilderPage }) => {
+  await sliceBuilderPage.goto(slice.name);
+
+  const defaultVariationName = "Default";
+  await sliceBuilderPage.openVariationActionMenu(
+    slice.name,
+    defaultVariationName,
+    "Rename",
+  );
+  const newVariationName = `${defaultVariationName}Renamed`;
+  await sliceBuilderPage.renameVariationDialog.renameVariation(
+    newVariationName,
+  );
+
+  await expect(sliceBuilderPage.variationCards).toHaveCount(1);
+  await expect(
+    sliceBuilderPage.getVariationCard(slice.name, defaultVariationName),
+  ).not.toBeVisible();
+  await expect(
+    sliceBuilderPage.getVariationCard(slice.name, newVariationName),
+  ).toBeVisible();
+});
+
+test.run()("I can delete a variation", async ({ slice, sliceBuilderPage }) => {
+  await sliceBuilderPage.goto(slice.name);
+  await sliceBuilderPage.openAddVariationDialog();
+  const variationName = `Variation ${generateRandomId()}`;
+  await sliceBuilderPage.addVariationDialog.addVariation(variationName);
+
+  await sliceBuilderPage.openVariationActionMenu(
+    slice.name,
+    variationName,
+    "Delete",
+  );
+  await sliceBuilderPage.deleteVariationDialog.deleteVariation();
+
+  await expect(sliceBuilderPage.variationCards).toHaveCount(1);
+  await expect(
+    sliceBuilderPage.getVariationCard(slice.name, variationName),
+  ).not.toBeVisible();
+});
+
+test.run()(
+  "I can add a static field to the builder",
+  async ({ slice, sliceBuilderPage }) => {
+    await sliceBuilderPage.goto(slice.name);
+
+    await expect(sliceBuilderPage.staticZoneListItem).toHaveCount(0);
+
+    await sliceBuilderPage.addStaticField(
+      "Rich Text",
+      "Description",
+      "description",
+    );
+
+    await expect(sliceBuilderPage.staticZoneListItem).toHaveCount(1);
+  },
+);
+
+test.run({ onboarded: false })(
+  "I can close the simulator tooltip and it stays close",
+  async ({ slice, sliceBuilderPage }) => {
+    await sliceBuilderPage.goto(slice.name);
+
+    // Simulator tooltip should open automatically
+    await expect(sliceBuilderPage.simulateTooltipTitle).toBeVisible();
+    await sliceBuilderPage.simulateTooltipCloseButton.click();
+    await expect(sliceBuilderPage.simulateTooltipTitle).not.toBeVisible();
+
+    await sliceBuilderPage.page.reload();
+    await expect(sliceBuilderPage.getBreadcrumbLabel(slice.name)).toBeVisible();
+
+    await expect(sliceBuilderPage.simulateTooltipTitle).not.toBeVisible();
+  },
+);
