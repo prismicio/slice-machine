28789c0be1c87df49059717838813e7dee03cdfe
Baptiste Morelle <bapmrl@users.noreply.github.com>
2024-01-22 12:03:50 +0100

    feat(slice-machine): add FieldSet component

---
+++ packages/slice-machine/src/components/FieldSet/FieldSet.tsx
@@ -0,0 +1,72 @@
+import { Text } from "@prismicio/editor-ui";
+import type { FC, PropsWithChildren, ReactNode } from "react";
+
+import styles from "./FieldSet.module.css";
+
+export const FieldSet: FC<PropsWithChildren> = (props) => (
+  <div {...props} className={styles.root} />
+);
+
+export const FieldSetLegend: FC<PropsWithChildren> = ({
+  children,
+  ...otherProps
+}) => (
+  <div {...otherProps} className={styles.legend}>
+    <Text color="grey11" component="span" noWrap variant="smallBold">
+      {children}
+    </Text>
+  </div>
+);
+
+export const FieldSetHeader: FC<PropsWithChildren> = (props) => (
+  <div {...props} className={styles.header} />
+);
+
+export const FieldSetContent: FC<PropsWithChildren> = (props) => (
+  <div {...props} className={styles.content} />
+);
+
+export const FieldSetList: FC<PropsWithChildren> = (props) => (
+  <div {...props} className={styles.list} />
+);
+
+type FieldSetListItemProps = PropsWithChildren<{ action?: ReactNode }>;
+
+export const FieldSetListItem: FC<FieldSetListItemProps> = ({
+  action,
+  children,
+  ...otherProps
+}) => (
+  <div {...otherProps} className={styles.listItem}>
+    <Text
+      className={styles.listItemText}
+      component="span"
+      noWrap
+      variant="bold"
+    >
+      {children}
+    </Text>
+    {action}
+  </div>
+);
+
+type FieldSetFooterProps = PropsWithChildren<{ action?: ReactNode }>;
+
+export const FieldSetFooter: FC<FieldSetFooterProps> = ({
+  action,
+  children,
+  ...otherProps
+}) => (
+  <div {...otherProps} className={styles.footer}>
+    <Text
+      className={styles.footerText}
+      color="grey11"
+      component="span"
+      noWrap
+      variant="small"
+    >
+      {children}
+    </Text>
+    {action}
+  </div>
+);
