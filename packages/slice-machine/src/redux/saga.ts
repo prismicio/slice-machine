d8c6687feec5b281b9b70102a4bf757083f240e6
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2024-01-29 17:28:49 +0100

    refactor(custom types): Remove createCustomTypeSaga

--- packages/slice-machine/src/redux/saga.ts
+++ packages/slice-machine/src/redux/saga.ts
@@ -1,7 +1,6 @@
 import { fork } from "redux-saga/effects";
 
 import { watchSimulatorSagas } from "@src/modules/simulator";
-import { watchAvailableCustomTypesSagas } from "@src/modules/availableCustomTypes";
 import { watchSliceSagas } from "@src/modules/slices";
 import { watchToasterSagas } from "@src/modules/toaster";
 import { watchChangelogSagas } from "@src/modules/environment";
@@ -9,7 +8,6 @@ import { watchChangelogSagas } from "@src/modules/environment";
 // Single entry point to start all Sagas at once
 export default function* rootSaga() {
   yield fork(watchSimulatorSagas);
-  yield fork(watchAvailableCustomTypesSagas);
   yield fork(watchSliceSagas);
   yield fork(watchToasterSagas);
   yield fork(watchChangelogSagas);
