68903d5cc4c4f2d000f0875889d2651b00f0d1c3
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2024-01-31 16:51:21 -1000

    docs: add comment about `null` owner type

--- packages/manager/src/managers/git/types.ts
+++ packages/manager/src/managers/git/types.ts
@@ -2,6 +2,9 @@ export type Owner = {
 	provider: "gitHub";
 	id: string;
 	name: string;
+	// If type is null, the owner's type could not be determined. This can
+	// happen if a Git provider uses an owner type that we do not support.
+	// Owners with a null type should still be usable like any other owner.
 	type: "user" | "team" | null;
 };
 
