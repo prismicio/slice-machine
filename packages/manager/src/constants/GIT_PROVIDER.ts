0555f495f43975b4f59667fb16ce52f8c8a70be6
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2024-02-22 00:28:29 +0100

    feat(tracking): Add tracking on slice machine start

--- packages/manager/src/constants/GIT_PROVIDER.ts
+++ packages/manager/src/constants/GIT_PROVIDER.ts
@@ -1,5 +1,7 @@
 export const GIT_PROVIDER = {
 	GitHub: "gitHub",
+	GitLab: "gitLab",
+	Bitbucket: "bitbucket",
 } as const;
 
 export type GitProvider = (typeof GIT_PROVIDER)[keyof typeof GIT_PROVIDER];
