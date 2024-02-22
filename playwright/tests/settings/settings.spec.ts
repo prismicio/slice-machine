c99b97a2b630dce44cd710918f6d1ef13114b5c3
Baptiste Morelle <bapmrl@users.noreply.github.com>
2024-02-22 16:31:54 +0100

    test: refactor test options

--- playwright/tests/settings/settings.spec.ts
+++ playwright/tests/settings/settings.spec.ts
@@ -13,127 +13,122 @@ import {
   UnauthorizedError,
 } from "../../utils";
 
-test.run()(
-  "I can install the GitHub app",
-  async ({ procedures, settingsPage }) => {
-    procedures.mock("git.fetchLinkedRepos", () => [], { execute: false });
-    procedures.mock("git.fetchOwners", () => [], { execute: false });
+test("I can install the GitHub app", async ({ procedures, settingsPage }) => {
+  procedures.mock("git.fetchLinkedRepos", () => [], { execute: false });
+  procedures.mock("git.fetchOwners", () => [], { execute: false });
 
-    await settingsPage.goto();
+  await settingsPage.goto();
 
-    procedures.mock(
-      "git.getProviderAppInstallURL",
-      () => providerAppInstallURL,
-      { execute: false },
-    );
+  procedures.mock("git.getProviderAppInstallURL", () => providerAppInstallURL, {
+    execute: false,
+  });
 
-    const gitHubPagePromise = settingsPage.page.context().waitForEvent("page");
-    await settingsPage.installGitHubButton.click();
-    const gitHubPage = await gitHubPagePromise;
-    await gitHubPage.waitForLoadState();
-  },
-);
+  const gitHubPagePromise = settingsPage.page.context().waitForEvent("page");
+  await settingsPage.installGitHubButton.click();
+  const gitHubPage = await gitHubPagePromise;
+  await gitHubPage.waitForLoadState();
+});
 
-test.run()(
-  "I can connect a GitHub repository to my Prismic repository",
-  async ({ procedures, settingsPage }) => {
-    procedures.mock("git.fetchLinkedRepos", () => [], { execute: false });
-    procedures.mock("git.fetchOwners", () => gitOwners, { execute: false });
-    procedures.mock("git.fetchRepos", () => gitRepos, { execute: false });
+test("I can connect a GitHub repository to my Prismic repository", async ({
+  procedures,
+  settingsPage,
+}) => {
+  procedures.mock("git.fetchLinkedRepos", () => [], { execute: false });
+  procedures.mock("git.fetchOwners", () => gitOwners, { execute: false });
+  procedures.mock("git.fetchRepos", () => gitRepos, { execute: false });
 
-    await settingsPage.goto();
+  await settingsPage.goto();
 
-    const firstGitRepo = gitRepos[0];
-    await settingsPage.getConnectGitRepoButton(firstGitRepo.name).click();
+  const firstGitRepo = gitRepos[0];
+  await settingsPage.getConnectGitRepoButton(firstGitRepo.name).click();
 
-    procedures.mock("git.linkRepo", () => undefined, { execute: false });
-    procedures.mock("git.updateWriteAPIToken", () => undefined, {
-      execute: false,
-    });
-    procedures.mock("git.fetchLinkedRepos", () => [firstGitRepo], {
-      execute: false,
-    });
+  procedures.mock("git.linkRepo", () => undefined, { execute: false });
+  procedures.mock("git.updateWriteAPIToken", () => undefined, {
+    execute: false,
+  });
+  procedures.mock("git.fetchLinkedRepos", () => [firstGitRepo], {
+    execute: false,
+  });
 
-    await settingsPage.gitRepositoryConnectDialog.connect("Write API token");
-    await expect(
-      settingsPage.getDisconnectGitRepoButton(firstGitRepo.name),
-    ).toBeVisible();
-  },
-);
+  await settingsPage.gitRepositoryConnectDialog.connect("Write API token");
+  await expect(
+    settingsPage.getDisconnectGitRepoButton(firstGitRepo.name),
+  ).toBeVisible();
+});
 
-test.run()(
-  "I can disconnect a GitHub repository from my Prismic repository",
-  async ({ procedures, settingsPage }) => {
-    procedures.mock("git.fetchLinkedRepos", () => linkedRepos, {
-      execute: false,
-    });
+test("I can disconnect a GitHub repository from my Prismic repository", async ({
+  procedures,
+  settingsPage,
+}) => {
+  procedures.mock("git.fetchLinkedRepos", () => linkedRepos, {
+    execute: false,
+  });
 
-    await settingsPage.goto();
+  await settingsPage.goto();
 
-    const firstLinkedRepo = linkedRepos[0];
-    await settingsPage.getDisconnectGitRepoButton(firstLinkedRepo.name).click();
+  const firstLinkedRepo = linkedRepos[0];
+  await settingsPage.getDisconnectGitRepoButton(firstLinkedRepo.name).click();
 
-    procedures.mock("git.unlinkRepo", () => undefined, { execute: false });
-    procedures.mock("git.fetchLinkedRepos", () => [], { execute: false });
-    procedures.mock("git.fetchOwners", () => gitOwners, { execute: false });
-    procedures.mock("git.fetchRepos", () => [firstLinkedRepo], {
-      execute: false,
-    });
+  procedures.mock("git.unlinkRepo", () => undefined, { execute: false });
+  procedures.mock("git.fetchLinkedRepos", () => [], { execute: false });
+  procedures.mock("git.fetchOwners", () => gitOwners, { execute: false });
+  procedures.mock("git.fetchRepos", () => [firstLinkedRepo], {
+    execute: false,
+  });
 
-    await settingsPage.gitRepositoryDisconnectDialog.disconnect();
-    await expect(
-      settingsPage.getConnectGitRepoButton(firstLinkedRepo.name),
-    ).toBeVisible();
-  },
-);
+  await settingsPage.gitRepositoryDisconnectDialog.disconnect();
+  await expect(
+    settingsPage.getConnectGitRepoButton(firstLinkedRepo.name),
+  ).toBeVisible();
+});
 
-test.run()(
-  "I can see a specific error message when I'm unauthenticated",
-  async ({ procedures, settingsPage }) => {
-    procedures.mock(
-      "git.fetchLinkedRepos",
-      () => {
-        throw new UnauthenticatedError();
-      },
-      { execute: false },
-    );
+test("I can see a specific error message when I'm unauthenticated", async ({
+  procedures,
+  settingsPage,
+}) => {
+  procedures.mock(
+    "git.fetchLinkedRepos",
+    () => {
+      throw new UnauthenticatedError();
+    },
+    { execute: false },
+  );
 
-    await settingsPage.goto();
+  await settingsPage.goto();
 
-    await expect(settingsPage.unauthenticatedErrorTitle).toBeVisible();
-  },
-);
+  await expect(settingsPage.unauthenticatedErrorTitle).toBeVisible();
+});
 
-test.run()(
-  "I can see a specific error message when I'm unauthorized",
-  async ({ procedures, settingsPage }) => {
-    procedures.mock(
-      "git.fetchLinkedRepos",
-      () => {
-        throw new UnauthorizedError();
-      },
-      { execute: false },
-    );
+test("I can see a specific error message when I'm unauthorized", async ({
+  procedures,
+  settingsPage,
+}) => {
+  procedures.mock(
+    "git.fetchLinkedRepos",
+    () => {
+      throw new UnauthorizedError();
+    },
+    { execute: false },
+  );
 
-    await settingsPage.goto();
+  await settingsPage.goto();
 
-    await expect(settingsPage.unauthorizedErrorTitle).toBeVisible();
-  },
-);
+  await expect(settingsPage.unauthorizedErrorTitle).toBeVisible();
+});
 
-test.run()(
-  "I can see a generic error message when something unexpected happened",
-  async ({ procedures, settingsPage }) => {
-    procedures.mock(
-      "git.fetchLinkedRepos",
-      () => {
-        throw new SliceMachineError();
-      },
-      { execute: false },
-    );
+test("I can see a generic error message when something unexpected happened", async ({
+  procedures,
+  settingsPage,
+}) => {
+  procedures.mock(
+    "git.fetchLinkedRepos",
+    () => {
+      throw new SliceMachineError();
+    },
+    { execute: false },
+  );
 
-    await settingsPage.goto();
+  await settingsPage.goto();
 
-    await expect(settingsPage.unknownErrorTitle).toBeVisible();
-  },
-);
+  await expect(settingsPage.unknownErrorTitle).toBeVisible();
+});
