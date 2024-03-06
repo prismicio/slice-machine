b8aa02059c70a8a04a86daed8880d700bbc0236e
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2024-03-06 13:50:41 -1000

    docs: rephrase

--- packages/adapter-next/src/simulator/types.ts
+++ packages/adapter-next/src/simulator/types.ts
@@ -1,5 +1,5 @@
 /**
- * The page parameters provided to the Slice Simulator page.
+ * Parameters provided to the Slice Simulator page.
  *
  * **Note**: `SliceSimulatorParams` should only be used in the App Router with a
  * Server Component.
