5024c6f43ee7c58a659cf593880b24ac6711bc8c
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2024-03-18 14:22:58 -1000

    test: remove Cypress (#1322)

--- cypress/pages/changes/drawers/index.js
+++
@@ -1,9 +0,0 @@
-import { hardDeleteDocumentsDrawer } from "./hardDeleteDocumentsDrawer";
-import { softDeleteDocumentsDrawer } from "./softDeleteDocumentsDrawer";
-import { referencesErrorDrawer } from "./referencesErrorDrawer";
-
-export {
-  hardDeleteDocumentsDrawer,
-  softDeleteDocumentsDrawer,
-  referencesErrorDrawer,
-};
