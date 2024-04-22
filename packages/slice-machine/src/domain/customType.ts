66f6beb49669dc9cb101d9876041c6165b3105a0
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2024-04-22 13:30:29 -1000

    refactor: remove unused custom type utilities

--- packages/slice-machine/src/domain/customType.ts
+++ packages/slice-machine/src/domain/customType.ts
@@ -4,7 +4,6 @@ import {
   DynamicSlices,
   DynamicSlicesConfig,
   Group,
-  GroupFieldType,
   NestableWidget,
   SlicesFieldType,
   UID,
@@ -47,38 +46,6 @@ type ReorderFieldArgs = {
   destinationIndex: number;
 };
 
-type AddGroupField = {
-  customType: CustomType;
-  sectionId: string;
-  groupFieldId: string;
-  newField: NestableWidget;
-  newFieldId: string;
-};
-
-type DeleteGroupField = {
-  customType: CustomType;
-  sectionId: string;
-  groupFieldId: string;
-  fieldId: string;
-};
-
-type UpdateGroupFieldArgs = {
-  customType: CustomType;
-  sectionId: string;
-  groupFieldId: string;
-  previousFieldId: string;
-  newFieldId: string;
-  newField: NestableWidget;
-};
-
-type ReorderGroupFieldArgs = {
-  customType: CustomType;
-  sectionId: string;
-  groupFieldId: string;
-  sourceIndex: number;
-  destinationIndex: number;
-};
-
 type UpdateSectionArgs = {
   customType: CustomType;
   sectionId: string;
@@ -96,12 +63,6 @@ type ReorderFieldsArgs<T> = {
   fields: Record<string, T>;
   sourceIndex: number;
   destinationIndex: number;
-};
-
-type GetGroupFieldArgs = {
-  customType: CustomType;
-  sectionId: string;
-  groupFieldId: string;
 };
 
 type CreateArgs = {
@@ -465,134 +426,6 @@ export function reorderField(args: ReorderFieldArgs): CustomType {
   return newCustomType;
 }
 
-export function addGroupField(args: AddGroupField): CustomType {
-  const { customType, sectionId, groupFieldId, newField, newFieldId } = args;
-  const groupField = getGroupField({
-    customType,
-    sectionId,
-    groupFieldId,
-  });
-  const groupFields = groupField?.config?.fields;
-
-  if (!groupField) {
-    return customType;
-  }
-
-  const newGroupField = updateGroupFields(groupField, {
-    ...groupFields,
-    // Add the new field to the group fields
-    [newFieldId]: newField,
-  });
-  const newCustomType = updateSection({
-    customType,
-    sectionId,
-    updatedSection: {
-      ...customType.json[sectionId],
-      [groupFieldId]: newGroupField,
-    },
-  });
-
-  return newCustomType;
-}
-
-export function deleteGroupField(args: DeleteGroupField): CustomType {
-  const { customType, sectionId, groupFieldId, fieldId } = args;
-  const groupField = getGroupField({
-    customType,
-    sectionId,
-    groupFieldId,
-  });
-  const groupFields = groupField?.config?.fields;
-
-  if (!groupField || !groupFields) {
-    return customType;
-  }
-
-  const updatedFields = removeKey(groupFields, fieldId);
-  const newGroupField = updateGroupFields(groupField, updatedFields);
-  const newCustomType = updateSection({
-    customType,
-    sectionId,
-    updatedSection: {
-      ...customType.json[sectionId],
-      [groupFieldId]: newGroupField,
-    },
-  });
-
-  return newCustomType;
-}
-
-export function updateGroupField(args: UpdateGroupFieldArgs): CustomType {
-  const {
-    customType,
-    sectionId,
-    groupFieldId,
-    previousFieldId,
-    newFieldId,
-    newField,
-  } = args;
-  const groupField = getGroupField({
-    customType,
-    sectionId,
-    groupFieldId,
-  });
-  const groupFields = groupField?.config?.fields;
-
-  if (!groupField || !groupFields) {
-    return customType;
-  }
-
-  const updatedGroupFields = updateFields({
-    fields: groupFields,
-    previousFieldId,
-    newFieldId,
-    newField,
-  });
-  const newGroupField = updateGroupFields(groupField, updatedGroupFields);
-  const newCustomType = updateSection({
-    customType,
-    sectionId,
-    updatedSection: {
-      ...customType.json[sectionId],
-      [groupFieldId]: newGroupField,
-    },
-  });
-
-  return newCustomType;
-}
-
-export function reorderGroupField(args: ReorderGroupFieldArgs): CustomType {
-  const { customType, sectionId, groupFieldId, sourceIndex, destinationIndex } =
-    args;
-  const groupField = getGroupField({
-    customType,
-    sectionId,
-    groupFieldId,
-  });
-  const groupFields = groupField?.config?.fields;
-
-  if (!groupField || !groupFields) {
-    return customType;
-  }
-
-  const updatedGroupFields = reorderFields({
-    fields: groupFields,
-    sourceIndex,
-    destinationIndex,
-  });
-  const newGroupField = updateGroupFields(groupField, updatedGroupFields);
-  const newCustomType = updateSection({
-    customType,
-    sectionId,
-    updatedSection: {
-      ...customType.json[sectionId],
-      [groupFieldId]: newGroupField,
-    },
-  });
-
-  return newCustomType;
-}
-
 export function updateSection(args: UpdateSectionArgs): CustomType {
   const { customType, sectionId, updatedSection } = args;
 
@@ -632,30 +465,6 @@ export function reorderFields<T>(args: ReorderFieldsArgs<T>) {
   const reorderedFields = Object.fromEntries(fieldEntries);
 
   return reorderedFields;
-}
-
-export function getGroupField(args: GetGroupFieldArgs): Group | undefined {
-  const { customType, sectionId, groupFieldId } = args;
-  const field = customType.json[sectionId][groupFieldId];
-
-  if (field.type === GroupFieldType) {
-    return field;
-  }
-
-  return undefined;
-}
-
-export function updateGroupFields(
-  field: Group,
-  updatedGroupFields: Record<string, NestableWidget>,
-): Group {
-  return {
-    ...field,
-    config: {
-      ...field.config,
-      fields: updatedGroupFields,
-    },
-  };
 }
 
 export function create(args: CreateArgs) {
