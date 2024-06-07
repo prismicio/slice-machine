139900d5a27c5b234beaea0088ac25b7c171aeb8
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2024-06-07 11:59:29 +0200

    review

--- packages/manager/src/managers/prismicRepository/types.ts
+++ packages/manager/src/managers/prismicRepository/types.ts
@@ -81,7 +81,7 @@ export type AllChangeTypes =
 	| CustomTypeInsertChange
 	| CustomTypeUpdateChange
 	| CustomTypeDeleteChange;
-export interface PushBody extends Record<string, unknown> {
+export interface PushBody {
 	confirmDeleteDocuments: boolean;
 	changes: AllChangeTypes[];
 }
