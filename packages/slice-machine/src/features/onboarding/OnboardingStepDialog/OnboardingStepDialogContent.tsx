8688b75f7b32c006628c9626e4e98db8aed77e13
José Pereira <7235666+jomifepe@users.noreply.github.com>
2024-07-25 17:01:39 +0100

    feat(onboarding): update onboarding step modals to match design (#1406)

--- packages/slice-machine/src/features/onboarding/OnboardingStepDialog/OnboardingStepDialogContent.tsx
+++ packages/slice-machine/src/features/onboarding/OnboardingStepDialog/OnboardingStepDialogContent.tsx
@@ -1,9 +1,11 @@
 import { Box, ScrollArea, Text, Video } from "@prismicio/editor-ui";
 
-import { getOnboardingStepsContent } from "@/features/onboarding/content";
+import {
+  indexLabel,
+  onboardingStepContent,
+} from "@/features/onboarding/content";
 import { useOnboardingContext } from "@/features/onboarding/OnboardingProvider";
 import { OnboardingStep } from "@/features/onboarding/types";
-import { useRepositoryInformation } from "@/hooks/useRepositoryInformation";
 
 type OnboardingStepDialogContentProps = {
   step: OnboardingStep;
@@ -13,20 +15,19 @@ export const OnboardingStepDialogContent = ({
   step,
 }: OnboardingStepDialogContentProps) => {
   const { getStepIndex } = useOnboardingContext();
-  const { repositoryUrl } = useRepositoryInformation();
 
   const {
     content: Content,
     videoUrl,
     title = step.title,
-  } = getOnboardingStepsContent({ repositoryUrl })[step.id];
+  } = onboardingStepContent[step.id];
 
   return (
     <ScrollArea>
       <Box as="article" flexDirection="column" padding={16} gap={16}>
         <section>
           <Text sx={{ marginBottom: 4 }} color="purple9" variant="bold">
-            Step {getStepIndex(step.id) + 1}
+            Step {indexLabel[getStepIndex(step.id)]}
           </Text>
           <Text variant="h3">{title}</Text>
           <Content />
