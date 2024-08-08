02c1816c958f84c69370c5999d73e75146fecd26
José Pereira <7235666+jomifepe@users.noreply.github.com>
2024-08-08 12:53:45 +0100

    feat(🐌): resume slugification when id is cleared
    refactor(🛡️): make it more resilient by accounting for undefined values

--- packages/slice-machine/src/legacy/lib/builders/common/EditModal/SlugifyLabelObserver.tsx
+++ packages/slice-machine/src/legacy/lib/builders/common/EditModal/SlugifyLabelObserver.tsx
@@ -4,22 +4,30 @@ import { useEffect, useRef } from "react";
 import { slugify } from "@/legacy/lib/utils/str";
 
 export function SlugifyLabelObserver() {
-  const [{ value: label }] = useField<string>("config.label");
+  const [{ value: label }] = useField<string | undefined>("config.label");
   const [{ value: id }, { initialValue: initialId }, { setValue: setId }] =
-    useField<string>("id");
+    useField<string | undefined>("id");
 
-  // if we're editing an existing field, we don't want to keep slugifying the label
-  const wasIdChangedRef = useRef(initialId != null && initialId.length > 0);
+  const wasIdChangedRef = useRef(
+    /* if we're editing an existing field, we don't want to keep slugifying the label */
+    initialId != null && initialId.length > 0,
+  );
 
   useEffect(() => {
-    if (!wasIdChangedRef.current) {
+    if (!wasIdChangedRef.current && label != null) {
       setId(slugify(label));
     }
   }, [label]); // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
-    // if the user changed the id directly, we opt out of the slugification
-    if (!wasIdChangedRef.current && id.length > 0 && id !== slugify(label)) {
+    const isIdEmpty = id == null || id.length === 0;
+    if (wasIdChangedRef.current) {
+      if (isIdEmpty) {
+        // if the id was cleared, we resume the slugification
+        wasIdChangedRef.current = false;
+      }
+    } else if (!isIdEmpty && (label == null || id !== slugify(label))) {
+      // if the id was manually set, we opt out of the slugification
       wasIdChangedRef.current = true;
     }
   }, [id]); // eslint-disable-line react-hooks/exhaustive-deps
