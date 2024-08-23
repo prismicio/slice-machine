f10fdcf5fa6e56f332aa1dac5639773dd7c0196b
Bohdan Imiela <bohdan.imiela@prismic.io>
2024-08-23 16:43:48 +0200

    fix: serve new version to users outside experiment scope

--- packages/slice-machine/src/hooks/usePromptToCreateContentExperiment.ts
+++ packages/slice-machine/src/hooks/usePromptToCreateContentExperiment.ts
@@ -5,6 +5,13 @@ type UsePromptToCreateContentExperimentReturnType = { eligible: boolean };
 export function usePromptToCreateContentExperiment(): UsePromptToCreateContentExperimentReturnType {
   const variant = useExperimentVariant("slicemachine-prompt-to-create-content");
 
-  // serve the experimental version to user with Telemetry deactivated
-  return { eligible: variant === undefined || variant?.value === "on" };
+  return {
+    eligible:
+      // serve the experimental version to user with Telemetry deactivated
+      variant === undefined ||
+      // serve the experimental version to user out of experiment scope
+      variant?.value === "off" ||
+      // serve the experimental version in the experiment test group with variant "on"
+      variant?.value === "on",
+  };
 }
