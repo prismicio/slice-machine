cf311a663d90b4ba073e791d2d281d7bfd26352f
Bohdan Imiela <bohdan.imiela@prismic.io>
2024-08-23 17:41:53 +0200

    refactor: simplify the condition

--- packages/slice-machine/src/hooks/usePromptToCreateContentExperiment.ts
+++ packages/slice-machine/src/hooks/usePromptToCreateContentExperiment.ts
@@ -5,8 +5,12 @@ type UsePromptToCreateContentExperimentReturnType = { eligible: boolean };
 export function usePromptToCreateContentExperiment(): UsePromptToCreateContentExperimentReturnType {
   const variant = useExperimentVariant("slicemachine-prompt-to-create-content");
 
-  // we serve the previous version only to users in the control group
-  // users in variant on, users out of experiment scope, and users with telemetry deactivated will see the new version
+  // we serve the previous version only to users in the "control" group
+  // users in:
+  // - variant "on"
+  // - out of experiment scope,
+  // - with telemetry deactivated
+  // will see the new version
   return {
     eligible: variant?.value !== "control",
   };
