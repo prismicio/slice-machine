3524db62bbebf8006f7428746c8fbf920447e84d
Bohdan Imiela <bohdan.imiela@prismic.io>
2024-08-27 10:56:20 +0200

    feat: update experiment key

--- packages/slice-machine/src/hooks/usePromptToCreateContentExperiment.ts
+++ packages/slice-machine/src/hooks/usePromptToCreateContentExperiment.ts
@@ -3,13 +3,15 @@ import { useExperimentVariant } from "@/hooks/useExperimentVariant";
 type UsePromptToCreateContentExperimentReturnType = { eligible: boolean };
 
 export function usePromptToCreateContentExperiment(): UsePromptToCreateContentExperimentReturnType {
-  const variant = useExperimentVariant("slicemachine-prompt-to-create-content");
+  const variant = useExperimentVariant(
+    "slicemachine-prompt-to-create-document",
+  );
 
   // we serve the previous version only to users in the "control" group
   // users in:
-  // - variant "on"
-  // - out of experiment scope,
-  // - with telemetry deactivated
+  // - variant value "on"
+  // - out of experiment scope (variant value "off"),
+  // - with telemetry deactivated (undefined)
   // will see the new version
   return {
     eligible: variant?.value !== "control",
