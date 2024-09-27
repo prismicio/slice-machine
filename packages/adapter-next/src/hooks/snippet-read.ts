9beb1be3eba3a73cf5258aca8c54c0a2d5a9c170
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2024-09-27 15:30:23 +0200

    temp add correct snipet for Next (to finish)

--- packages/adapter-next/src/hooks/snippet-read.ts
+++ packages/adapter-next/src/hooks/snippet-read.ts
@@ -64,7 +64,7 @@ export const snippetRead: SnippetReadHook<PluginOptions> = async (
 				language: "tsx",
 				code: await format(
 					stripIndent`
-						<PrismicNextLink field={${dotPath(fieldPath)}}>Link</PrismicNextLink>
+						<PrismicNextLink field={${dotPath(fieldPath)}} />
 					`,
 					helpers,
 				),
