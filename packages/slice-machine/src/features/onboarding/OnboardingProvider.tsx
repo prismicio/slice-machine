21bcca655f7c982b58c8c13824490ad99f683a69
Bohdan Imiela <47499119+BohdanOne@users.noreply.github.com>
2024-10-04 13:54:39 +0200

    fix(onboarding): make createProject step optional (#1462)

--- packages/slice-machine/src/features/onboarding/OnboardingProvider.tsx
+++ packages/slice-machine/src/features/onboarding/OnboardingProvider.tsx
@@ -54,11 +54,11 @@ export const OnboardingProvider = ({
   );
 
   const toggleStepComplete = (step: OnboardingStep) => {
-    const isComplete = !stepStatus[step.id];
-    const nextState = { ...stepStatus, [step.id]: isComplete };
+    const newCompleteState = !isStepComplete(step);
+    const nextState = { ...stepStatus, [step.id]: newCompleteState };
     setStepStatus(nextState);
 
-    if (isComplete) {
+    if (newCompleteState) {
       void telemetry.track({
         event: "onboarding:step-completed",
         stepId: step.id,
@@ -76,13 +76,11 @@ export const OnboardingProvider = ({
   };
 
   const isStepComplete = (step: OnboardingStep) => {
-    return stepStatus[step.id] ?? false;
+    return Boolean(stepStatus[step.id]) || Boolean(step.defaultCompleted);
   };
 
-  const stepsIds = steps.map(({ id }) => id);
-  const completedStepCount = Object.entries(stepStatus).filter(
-    // filtering out steps that are not part of the current experiment variant
-    ([key, value]) => stepsIds.includes(key as OnboardingStepId) && value,
+  const completedStepCount = steps.filter((step) =>
+    isStepComplete(step),
   ).length;
 
   return (
