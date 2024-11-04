3c6fc43955aef85bb6afbb604936eb91d1b450e4
Daniel Martín <danimartinprieto@gmail.com>
2024-11-05 00:15:47 +0100

    Update packages/slice-machine/src/legacy/lib/builders/common/Zone/Card/components/Hints/index.tsx

--- packages/slice-machine/src/legacy/lib/builders/common/Zone/Card/components/Hints/index.tsx
+++ packages/slice-machine/src/legacy/lib/builders/common/Zone/Card/components/Hints/index.tsx
@@ -20,7 +20,7 @@ const Hint: React.FC<HintProps> = ({
 }) => {
   const fieldPathString = renderHintBase({ item });
 
-  const snippetCacheKey: string[] = [item.value.type];
+  const snippetCacheKey = [fieldPathString];
   if (item.value.type === "Link") {
     if (item.value.config?.allowText ?? false)
       snippetCacheKey.push("allowText");
