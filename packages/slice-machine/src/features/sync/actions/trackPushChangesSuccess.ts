2f67adb97ac48b138b7a176fa436fbc9b6e76a79
Bohdan Imiela <47499119+BohdanOne@users.noreply.github.com>
2024-12-05 11:03:01 +0100

    feat: track groups pushed (#1493)

--- packages/slice-machine/src/features/sync/actions/trackPushChangesSuccess.ts
+++ packages/slice-machine/src/features/sync/actions/trackPushChangesSuccess.ts
@@ -1,5 +1,12 @@
+import {
+  FieldType,
+  NestableWidget,
+  UID,
+} from "@prismicio/types-internal/lib/customtypes";
+
 import { telemetry } from "@/apiClient";
 import { countMissingScreenshots } from "@/domain/slice";
+import { GroupSM } from "@/legacy/lib/models/common/Group";
 import {
   ChangedCustomType,
   ChangedSlice,
@@ -94,4 +101,85 @@ export function trackPushChangesSuccess(args: TrackPushChangesSuccessArgs) {
     hasDeletedDocuments,
     _includeEnvironmentKind: true,
   });
+
+  trackPushedGroups({ changedCustomTypes, changedSlices });
+}
+
+type TrackPushedGroupsArgs = {
+  changedSlices: ReadonlyArray<ChangedSlice>;
+  changedCustomTypes: ReadonlyArray<ChangedCustomType>;
+};
+type FieldsCount = {
+  [key in FieldType]?: number;
+};
+type FieldStats = {
+  isInStaticZone: boolean;
+  isInSlice: boolean;
+} & FieldsCount;
+
+function trackPushedGroups(args: TrackPushedGroupsArgs) {
+  const { changedCustomTypes, changedSlices } = args;
+
+  const groupsInStaticZone = changedCustomTypes.reduce<FieldStats[]>(
+    (acc, customType) => {
+      if (customType.status !== ModelStatus.New) return acc;
+
+      customType.customType.tabs.forEach((tab) => {
+        const fieldsCount = countGroupFields(acc, tab.value, "custom-type");
+        acc.push(...fieldsCount);
+      });
+
+      return acc;
+    },
+    [],
+  );
+
+  const groupsInSlices = changedSlices.reduce<FieldStats[]>((acc, slice) => {
+    if (slice.status !== ModelStatus.New) return acc;
+
+    slice.slice.model.variations.forEach((variation) => {
+      const fieldsCount = countGroupFields(acc, variation.primary, "slice");
+      acc.push(...fieldsCount);
+    });
+
+    return acc;
+  }, []);
+  [...groupsInSlices, ...groupsInStaticZone].forEach((group) => {
+    void telemetry.track({
+      event: "changes:group-pushed",
+      ...group,
+    });
+  });
+}
+
+interface Field {
+  key: string;
+  value: GroupSM | UID | NestableWidget;
+}
+function countGroupFields<TField extends Field>(
+  fieldStats: FieldStats[],
+  fields: TField[] | undefined,
+  groupParent: "custom-type" | "slice",
+) {
+  if (!fields) return fieldStats;
+
+  const newFieldState = fieldStats.slice();
+
+  fields.forEach((field) => {
+    if (field.value.type === "Group" && field.value.config?.fields) {
+      const fieldsCount: FieldsCount = {};
+      field.value.config.fields.forEach(({ value: fieldValue }) => {
+        const value = fieldsCount[fieldValue.type] ?? 0;
+        fieldsCount[fieldValue.type] = value + 1;
+      });
+
+      newFieldState.push({
+        isInStaticZone: groupParent === "custom-type",
+        isInSlice: groupParent === "slice",
+        ...fieldsCount,
+      });
+    }
+  });
+
+  return newFieldState;
 }
