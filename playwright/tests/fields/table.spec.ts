1b572a140b1d7ca32f14dd3048f766f819199a24
Levi Mykel Gable <9385378+levimykel@users.noreply.github.com>
2025-02-20 18:05:16 +0100

    test: mock getExperimentVariant

--- playwright/tests/fields/table.spec.ts
+++ playwright/tests/fields/table.spec.ts
@@ -4,8 +4,16 @@ import { test } from "../../fixtures";
 
 test("I can create a table field", async ({
   customTypesBuilderPage,
+  procedures,
   singleCustomType,
 }) => {
+  procedures.mock(
+    "telemetry.getExperimentVariant",
+    ({ args }) =>
+      args[0] === "slicemachine-table-field" ? { value: "on" } : undefined,
+    { execute: false },
+  );
+
   await customTypesBuilderPage.goto(singleCustomType.name);
   await customTypesBuilderPage.addStaticField({
     type: "Table",
