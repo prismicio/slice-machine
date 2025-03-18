476846e879269f5710e5916ec64cb00cc948d1f1
José Pereira <7235666+jomifepe@users.noreply.github.com>
2025-03-18 16:59:55 +0000

    refactor: remove test

--- playwright/tests/common/reviewForm.spec.ts
+++ playwright/tests/common/reviewForm.spec.ts
@@ -10,53 +10,6 @@ test.use({
   },
 });
 
-test("I can write a review after creating enough models", async ({
-  sliceMachinePage,
-  procedures,
-}) => {
-  const libraries = generateLibraries({ slicesCount: 6 });
-
-  // We mock a page type with a slice that is a requirement for the review dialog
-  procedures.mock(
-    "getState",
-    ({ data }) => ({
-      ...(data as Record<string, unknown>),
-      libraries,
-      customTypes: generateTypes({ typesCount: 1, libraries }),
-      remoteCustomTypes: [],
-      remoteSlices: [],
-      clientError: undefined,
-    }),
-    { times: 2 },
-  );
-
-  await sliceMachinePage.gotoDefaultPage();
-
-  // We close the first review for onboarding
-  await sliceMachinePage.reviewDialog.closeButton.click();
-
-  procedures.mock("getState", ({ data }) => ({
-    ...(data as Record<string, unknown>),
-    libraries,
-    customTypes: generateTypes({ typesCount: 6, libraries }),
-    remoteCustomTypes: [],
-    remoteSlices: [],
-    clientError: undefined,
-  }));
-
-  // We reload and mock with 6 types to trigger the advanced review dialog
-  await sliceMachinePage.page.reload();
-
-  await sliceMachinePage.reviewDialog.submitReview({
-    rating: 5,
-    message: "I love Slice Machine!",
-  });
-
-  // We verify that the review dialog is not displayed anymore
-  await sliceMachinePage.page.reload();
-  await expect(sliceMachinePage.reviewDialog.title).not.toBeVisible();
-});
-
 test("I can close the review dialog", async ({
   sliceMachinePage,
   procedures,
