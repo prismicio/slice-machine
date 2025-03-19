3cd112430a452d778105e2f466b0d80239072533
Levi Mykel Gable <9385378+levimykel@users.noreply.github.com>
2025-03-19 14:30:00 +0100

    fix: only call onClose when closing

--- packages/slice-machine/src/components/GenerateSliceWithAiModal/GenerateSliceWithAiModal.tsx
+++ packages/slice-machine/src/components/GenerateSliceWithAiModal/GenerateSliceWithAiModal.tsx
@@ -16,7 +16,12 @@ export function GenerateSliceWithAiModal(props: GenerateSliceWithAiModalProps) {
   const { open, onClose } = props;
 
   return (
-    <Dialog open={open} onOpenChange={onClose}>
+    <Dialog
+      open={open}
+      onOpenChange={(open) => {
+        if (!open) onClose();
+      }}
+    >
       <DialogHeader title="Generate with AI" />
       <DialogContent gap={0}>
         <Box height="100%" />
