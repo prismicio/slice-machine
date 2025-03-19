f3fdcc354c260318c4fdcad282d5bc417e762e4c
Levi Mykel Gable <9385378+levimykel@users.noreply.github.com>
2025-03-19 15:15:44 +0100

    feat(ai-slice): add file drop zone and blank state

--- packages/slice-machine/src/components/GenerateSliceWithAiModal/GenerateSliceWithAiModal.tsx
+++ packages/slice-machine/src/components/GenerateSliceWithAiModal/GenerateSliceWithAiModal.tsx
@@ -1,10 +1,18 @@
 import {
+  BlankSlate,
+  BlankSlateActions,
+  BlankSlateDescription,
+  BlankSlateIcon,
+  BlankSlateTitle,
   Box,
   Dialog,
+  DialogActionButton,
   DialogActions,
   DialogCancelButton,
   DialogContent,
   DialogHeader,
+  FileDropZone,
+  FileUploadButton,
 } from "@prismicio/editor-ui";
 
 interface GenerateSliceWithAiModalProps {
@@ -15,6 +23,10 @@ interface GenerateSliceWithAiModalProps {
 export function GenerateSliceWithAiModal(props: GenerateSliceWithAiModalProps) {
   const { open, onClose } = props;
 
+  const onFilesSelected = (files: File[]) => {
+    console.log(files);
+  };
+
   return (
     <Dialog
       open={open}
@@ -24,11 +36,68 @@ export function GenerateSliceWithAiModal(props: GenerateSliceWithAiModalProps) {
     >
       <DialogHeader title="Generate with AI" />
       <DialogContent gap={0}>
-        <Box height="100%" />
+        <Box padding={16} height="100%">
+          <FileDropZone
+            onFilesSelected={onFilesSelected}
+            assetType="image"
+            overlay={
+              <UploadBlankSlate
+                onFilesSelected={onFilesSelected}
+                droppingFiles
+              />
+            }
+          >
+            <UploadBlankSlate onFilesSelected={onFilesSelected} />
+          </FileDropZone>
+        </Box>
+
         <DialogActions>
           <DialogCancelButton />
+          <DialogActionButton disabled onClick={() => undefined}>
+            Add to page
+          </DialogActionButton>
         </DialogActions>
       </DialogContent>
     </Dialog>
+  );
+}
+
+function UploadBlankSlate(props: {
+  droppingFiles?: boolean;
+  onFilesSelected: (files: File[]) => void;
+}) {
+  const { droppingFiles = false, onFilesSelected } = props;
+
+  return (
+    <Box
+      justifyContent="center"
+      flexDirection="column"
+      height="100%"
+      backgroundColor={droppingFiles ? "purple2" : "grey2"}
+      border
+      borderStyle="dashed"
+    >
+      <BlankSlate>
+        <BlankSlateIcon
+          lineColor="purple11"
+          backgroundColor="purple5"
+          name="cloudUpload"
+          size="large"
+        />
+        <BlankSlateTitle>Upload your design images.</BlankSlateTitle>
+        <BlankSlateDescription>
+          Once uploaded, you can generate slices automatically using AI.
+        </BlankSlateDescription>
+        <BlankSlateActions>
+          <FileUploadButton
+            startIcon="attachFile"
+            onFilesSelected={onFilesSelected}
+            color="grey"
+          >
+            Add images
+          </FileUploadButton>
+        </BlankSlateActions>
+      </BlankSlate>
+    </Box>
   );
 }
