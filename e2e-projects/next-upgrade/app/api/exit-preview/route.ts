3f0b264f2d9cabf4db5d5de692e16ac5e0390646
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2025-05-12 00:17:32 +0200

    test: remove unused e2e-projects folder

--- e2e-projects/next-upgrade/app/api/exit-preview/route.ts
+++
@@ -1,5 +0,0 @@
-import { exitPreview } from "@prismicio/next";
-
-export async function GET(): Promise<void | Response> {
-	return await exitPreview();
-}
--- e2e-projects/next-upgrade/app/api/revalidate/route.ts
+++
@@ -1,8 +0,0 @@
-import { revalidateTag } from "next/cache";
-import { NextResponse } from "next/server";
-
-export async function POST(): Promise<Response> {
-	revalidateTag("prismic");
-
-	return NextResponse.json({ revalidated: true, now: Date.now() });
-}
