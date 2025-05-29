4a52ec0f05ee5fac271f34c81f5fa163f4e90017
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2025-05-28 16:05:56 -1000

    fix: correctly resolve svelte

--- packages/adapter-sveltekit/src/lib/getSvelteMajor.ts
+++ packages/adapter-sveltekit/src/lib/getSvelteMajor.ts
@@ -1,10 +1,15 @@
+import { join } from "node:path";
+
 import { requireResolve } from "./requireResolve";
 
 export const getSvelteMajor = async (): Promise<number> => {
 	// A dynamic import lets us easily mock the module.
 	const { readFile } = await import("node:fs/promises");
 
-	const packageJSONPath = requireResolve("svelte/package.json", process.cwd());
+	const packageJSONPath = requireResolve(
+		"svelte/package.json",
+		join(process.cwd(), "package.json"),
+	);
 	const { version } = JSON.parse(await readFile(packageJSONPath, "utf8"));
 
 	const major = Number.parseInt(version.split(".")[0]);
