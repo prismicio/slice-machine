06b34345e510eb20646ab61e3af12c5d43b0b937
José Pereira <7235666+jomifepe@users.noreply.github.com>
2025-06-04 11:03:20 +0100

    feat: improve isValidObject

--- packages/slice-machine/src/utils/isValidObject.ts
+++ packages/slice-machine/src/utils/isValidObject.ts
@@ -1,11 +1,32 @@
+/**
+ * Ensure that the value is an record/object with string keys and existing
+ * properties. Accepts empty objects and objects created with
+ * `Object.create(null)`.
+ */
 export function isValidObject(
   value: unknown,
 ): value is Record<string, unknown> {
   if (typeof value !== "object" || value === null) return false;
+
   if (Array.isArray(value)) return false;
-  return !(
+
+  if (
     value instanceof Date ||
     value instanceof RegExp ||
     value instanceof Error
-  );
+  ) {
+    return false;
+  }
+
+  // Check if all keys are strings and all values are valid
+  for (const key in value) {
+    if (
+      typeof key !== "string" ||
+      !Object.prototype.hasOwnProperty.call(value, key)
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 }
