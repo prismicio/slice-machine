9c3ef67fab72f44ad685b9e809134e36644530e3
José Pereira <7235666+jomifepe@users.noreply.github.com>
2025-06-05 10:02:49 +0100

    [DT-2692] feat: improve content relationship field picker state (#1639)

--- packages/slice-machine/src/features/customTypes/fields/ContentRelationshipFieldPicker.tsx
+++ packages/slice-machine/src/features/customTypes/fields/ContentRelationshipFieldPicker.tsx
@@ -1,213 +1,296 @@
-import { useStableCallback } from "@prismicio/editor-support/React";
 import {
   Box,
   Text,
   TreeView,
   TreeViewCheckbox,
-  TreeViewCheckboxProps,
   TreeViewSection,
 } from "@prismicio/editor-ui";
-import { FormikContext, useField, useFormik } from "formik";
-import { useEffect } from "react";
+import { useMemo } from "react";
 import { useSelector } from "react-redux";
 
 import { selectAllCustomTypes } from "@/modules/availableCustomTypes";
+import { isValidObject } from "@/utils/isValidObject";
 
-type FieldMap = Record<string, boolean>;
-export type CustomTypeFieldMap = Record<string, FieldMap>;
+/**
+ * Picker fields check map types. Used internally to keep track of the checked
+ * fields in the TreeView, as it's easier to handle objects than arrays and
+ * also ensure field uniqueness.
+ *
+ * @example
+ * {
+ *   category: {
+ *     name: {
+ *       type: "checkbox",
+ *       value: true
+ *     }
+ *   },
+ *   author: {
+ *     firstName: {
+ *       type: "checkbox",
+ *       value: true
+ *     }
+ *     lastName: {
+ *       type: "checkbox",
+ *       value: false
+ *     }
+ *   }
+ * }
+ *
+ **/
+interface PickerCustomTypes {
+  [customTypeId: string]: PickerCustomType;
+}
 
-type CustomTypeField = string | { id: string; fields: string[] };
+interface PickerCustomType {
+  [fieldId: string]: PickerCheckboxField;
+}
+
+interface PickerCheckboxField {
+  type: "checkbox";
+  value: boolean;
+}
+
+/**
+ * Copy of types-internal Link customtypes.
+ *
+ * @example
+ * [
+ *   {
+ *     id: "category",
+ *     fields: ["name"],
+ *   },
+ *   {
+ *     id: "author",
+ *     fields: ["firstName", "lastName"],
+ *   },
+ * ],
+ */
+type TICustomTypes = readonly (string | TICustomType)[];
+
+interface TICustomType {
+  id: string;
+  fields: readonly string[];
+}
 
 interface ContentRelationshipFieldPickerProps {
-  initialValues: CustomTypeField[] | undefined;
-  onChange: (fields: CustomTypeField[]) => void;
+  value: TICustomTypes | undefined;
+  onChange: (fields: TICustomTypes) => void;
 }
 
 export function ContentRelationshipFieldPicker(
   props: ContentRelationshipFieldPickerProps,
 ) {
-  const { initialValues, onChange } = props;
+  const { value, onChange } = props;
   const customTypes = useCustomTypes();
+  const fieldCheckMap = value ? convertCustomTypesToFieldCheckMap(value) : {};
 
-  const stableOnChange = useStableCallback(onChange);
-  const form = useFormik<CustomTypeFieldMap>({
-    initialValues: initialValues ? convertCustomTypesToForm(initialValues) : {},
-    onSubmit: () => undefined, // values will be updated on change
-  });
-
-  useEffect(() => {
-    stableOnChange(convertFormToCustomTypes(form.values));
-  }, [form.values, stableOnChange]);
+  function onCustomTypesChange(
+    updater: (prev: PickerCustomTypes) => PickerCustomTypes,
+  ) {
+    onChange(convertFieldCheckMapToCustomTypes(updater(fieldCheckMap)));
+  }
 
   return (
-    <FormikContext.Provider value={form}>
-      <Box overflow="hidden" flexDirection="column" border borderRadius={6}>
-        <Box
-          border={{ bottom: true }}
-          padding={{ inline: 16, bottom: 16, top: 12 }}
-          flexDirection="column"
-          gap={8}
-        >
-          <Box flexDirection="column">
-            <Text variant="h4" color="grey12">
-              Types
-            </Text>
-            <Text color="grey12">
-              Choose which fields you want to expose from the linked document.
-            </Text>
-          </Box>
-          <TreeView
-            title="Exposed fields"
-            subtitle={`(${countPickedFields(form.values)})`}
-          >
-            {customTypes.map((ct) => {
-              const count = countPickedFields(form.values[ct.id]);
-              const countLabel = count === 1 ? "1 field" : `${count} fields`;
-
-              return (
-                <TreeViewSection
-                  key={ct.id}
-                  title={ct.label}
-                  subtitle={count > 0 ? `(${countLabel} exposed)` : undefined}
-                  badge="Custom type"
-                >
-                  {ct.fields.map((field) => (
-                    <TreeViewCheckboxField
-                      key={field.id}
-                      id={field.id}
-                      title={field.label}
-                      customTypeId={ct.id}
-                    />
-                  ))}
-                </TreeViewSection>
-              );
-            })}
-          </TreeView>
-        </Box>
-        <Box backgroundColor="white" flexDirection="column" padding={12}>
-          <Text variant="normal" color="grey11">
-            Have ideas for improving this field?{" "}
-            <a
-              // TODO: Add real URL: https://linear.app/prismic/issue/DT-2693
-              href="https://community.prismic.io/t/TODO"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ color: "inherit", textDecoration: "underline" }}
-            >
-              Please provide your feedback here.
-            </a>
+    <Box overflow="hidden" flexDirection="column" border borderRadius={6}>
+      <Box
+        border={{ bottom: true }}
+        padding={{ inline: 16, bottom: 16, top: 12 }}
+        flexDirection="column"
+        gap={8}
+      >
+        <Box flexDirection="column">
+          <Text variant="h4" color="grey12">
+            Types
+          </Text>
+          <Text color="grey12">
+            Choose which fields you want to expose from the linked document.
           </Text>
         </Box>
+        <TreeView
+          title="Exposed fields"
+          subtitle={`(${countPickedFields(fieldCheckMap)})`}
+        >
+          {customTypes.map((customType) => {
+            const onCustomTypeChange = (
+              updater: (prev: PickerCustomType) => PickerCustomType,
+            ) => {
+              onCustomTypesChange((currentCustomTypes) => ({
+                ...currentCustomTypes,
+                [customType.id]: updater(
+                  currentCustomTypes[customType.id] ?? {},
+                ),
+              }));
+            };
+
+            return (
+              <TreeViewCustomType
+                key={customType.id}
+                customType={customType}
+                onChange={onCustomTypeChange}
+                fieldCheckMap={fieldCheckMap[customType.id] ?? {}}
+              />
+            );
+          })}
+        </TreeView>
       </Box>
-    </FormikContext.Provider>
+      <Box backgroundColor="white" flexDirection="column" padding={12}>
+        <Text variant="normal" color="grey11">
+          Have ideas for improving this field?{" "}
+          <a
+            // TODO: Add real URL: https://linear.app/prismic/issue/DT-2693
+            href="https://community.prismic.io/t/TODO"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "inherit", textDecoration: "underline" }}
+          >
+            Please provide your feedback here.
+          </a>
+        </Text>
+      </Box>
+    </Box>
   );
 }
 
-function TreeViewCheckboxField(
-  props: {
-    id: string;
-    customTypeId: string;
-  } & Omit<TreeViewCheckboxProps, "checked" | "onCheckedChange">,
-) {
-  const { id, customTypeId, ...checkboxProps } = props;
-  const [field, _, helpers] = useField<boolean>(`${customTypeId}.${id}`);
+interface TreeViewCustomTypeProps {
+  customType: TICustomType;
+  fieldCheckMap: PickerCustomType;
+  onChange: (fn: (prev: PickerCustomType) => PickerCustomType) => void;
+}
+
+function TreeViewCustomType(props: TreeViewCustomTypeProps) {
+  const { customType, fieldCheckMap, onChange } = props;
+
+  const fieldCount = countPickedFields(fieldCheckMap);
+  const fieldCountLabel = fieldCount === 1 ? "1 field" : `${fieldCount} fields`;
 
   return (
-    <TreeViewCheckbox
-      {...checkboxProps}
-      checked={field.value}
-      onCheckedChange={(checked) => helpers.setValue(checked)}
-    />
+    <TreeViewSection
+      key={customType.id}
+      title={customType.id}
+      subtitle={fieldCount > 0 ? `(${fieldCountLabel} exposed)` : undefined}
+      badge="Custom type"
+    >
+      {customType.fields.map((fieldId) => {
+        const checked = fieldCheckMap[fieldId]?.value ?? false;
+
+        function onCheckedChange(value: boolean) {
+          onChange((currentFields) => ({
+            ...currentFields,
+            [fieldId]: { type: "checkbox", value },
+          }));
+        }
+
+        return (
+          <TreeViewCheckbox
+            key={fieldId}
+            title={fieldId}
+            checked={checked}
+            onCheckedChange={onCheckedChange}
+          />
+        );
+      })}
+    </TreeViewSection>
   );
 }
 
-type SimplifiedCustomTypeField = {
-  id: string;
-  label: string;
-};
-
-interface SimplifiedCustomType {
-  id: string;
-  label: string;
-  fields: SimplifiedCustomTypeField[];
-}
-
+/**
+ * Get all the existing local custom types from the store and process them into
+ * a single array to be rendered by the picker. For this we use the same as the
+ * Link config `customtypes` structure {@link TICustomTypes}.
+ */
 function useCustomTypes() {
-  const customTypes = useSelector(selectAllCustomTypes);
-  const simplifiedCustomTypes = customTypes.flatMap<SimplifiedCustomType>(
-    (customType) => {
-      // In the store we have remote and local custom types, we want to show
-      // the local ones, so that the user is able to create a content
-      // relationship with custom types present on the user's computer (pushed
-      // or not).
-      if (!("local" in customType)) return [];
+  const allCustomTypes = useSelector(selectAllCustomTypes);
 
-      const { id, label, tabs } = customType.local;
+  return useMemo(() => {
+    const customTypes = allCustomTypes.flatMap<TICustomType>(
+      (storeCustomType) => {
+        // In the store we have remote and local custom types, we want to show
+        // the local ones, so that the user is able to create a content
+        // relationship with custom types present on the user's computer (pushed
+        // or not).
+        if (!("local" in storeCustomType)) return [];
+        const customType = storeCustomType.local;
 
-      const fields = tabs.flatMap<SimplifiedCustomTypeField>((tab) => {
-        return tab.value.flatMap((field) => {
-          // filter out uid fields because it's a special field returned by the
-          // API and is not part of the data object in the document.
-          if (field.value.type === "UID" || field.key === "uid") {
-            return [];
-          }
+        const fields = customType.tabs.flatMap((tab) => {
+          return tab.value.flatMap((field) => {
+            // Filter out uid fields because it's a special field returned by the
+            // API and is not part of the data object in the document.
+            // We also filter by key "uid", because (as of the time of writing
+            // this), creating any field with that API id will result in it being
+            // used for metadata.
+            if (field.value.type === "UID" || field.key === "uid") {
+              return [];
+            }
 
-          return {
-            id: field.key,
-            label: field.value.config?.label ?? field.key,
-          };
+            return field.key;
+          });
         });
-      });
 
-      if (fields.length === 0) return [];
+        if (fields.length === 0) return [];
 
-      return { id, label: label ?? id, fields };
-    },
-  );
+        return { id: customType.id, fields };
+      },
+    );
 
-  simplifiedCustomTypes.sort((a, b) => a.id.localeCompare(b.id));
-  return simplifiedCustomTypes;
+    customTypes.sort((a, b) => a.id.localeCompare(b.id));
+
+    return customTypes;
+  }, [allCustomTypes]);
 }
 
-function countPickedFields(fields: CustomTypeFieldMap | FieldMap | undefined) {
-  if (!fields) return 0;
+/**
+ * Converts a Link config `customtypes` ({@link TICustomTypes}) structure into
+ * picker fields check map ({@link PickerCustomTypes}).
+ */
+function convertCustomTypesToFieldCheckMap(customtypes: TICustomTypes) {
+  return customtypes.reduce<PickerCustomTypes>((customTypes, customType) => {
+    if (typeof customType === "string") return customTypes;
 
-  return Object.values(fields).reduce<number>(
-    (count, value: boolean | FieldMap) => {
-      if (typeof value === "boolean" && value) return count + 1;
-      return count + Object.values(value).filter(Boolean).length;
-    },
-    0,
-  );
-}
-
-function convertCustomTypesToForm(value: CustomTypeField[]) {
-  return value.reduce<CustomTypeFieldMap>((customTypes, customType) => {
-    if (typeof customType === "string") {
-      customTypes[customType] = {};
-      return customTypes;
-    }
-
-    const { id, fields } = customType;
-    customTypes[id] = fields.reduce<FieldMap>((customTypeFields, field) => {
-      customTypeFields[field] = true;
-      return customTypeFields;
-    }, {});
+    customTypes[customType.id] = customType.fields.reduce<PickerCustomType>(
+      (customTypeFields, field) => {
+        customTypeFields[field] = { type: "checkbox", value: true };
+        return customTypeFields;
+      },
+      {},
+    );
 
     return customTypes;
   }, {});
 }
 
-/** Convert the picked fields map to the customtypes config and filter out empty customtypes */
-function convertFormToCustomTypes(fields: CustomTypeFieldMap) {
-  return Object.entries(fields).flatMap<CustomTypeField>(([ctId, fields]) => {
-    const fieldEntries = Object.entries(fields);
-    if (!fieldEntries.some(([_, checked]) => checked)) return [];
-    return [
-      {
-        id: ctId,
-        fields: fieldEntries.flatMap(([id, checked]) => (checked ? [id] : [])),
-      },
-    ];
+/**
+ * Converts a picker fields check map structure ({@link PickerCustomTypes}) into
+ * Link config `customtypes` ({@link TICustomTypes}) and filter out empty Custom
+ * types.
+ */
+function convertFieldCheckMapToCustomTypes(map: PickerCustomTypes) {
+  return Object.entries(map).flatMap<TICustomType>(([ctId, ctFields]) => {
+    const fields = Object.entries(ctFields).flatMap(([fieldId, checkbox]) =>
+      checkbox.value ? [fieldId] : [],
+    );
+    return fields.length > 0 ? { id: ctId, fields } : [];
   });
+}
+
+/**
+ * Generic recursive function that goes down the fields check map and counts all
+ * the properties that are set to true, which correspond to selected fields.
+ *
+ * It's not type safe, but checks the type of the values at runtime so that
+ * it only recurses into valid objects, and only counts checkbox fields.
+ */
+function countPickedFields(
+  fields: Record<string, unknown> | undefined,
+): number {
+  if (!fields) return 0;
+  return Object.values(fields).reduce<number>((count, value) => {
+    if (!isValidObject(value)) return count;
+    if (isCheckboxField(value)) return count + (value.value ? 1 : 0);
+    return count + countPickedFields(value);
+  }, 0);
+}
+
+function isCheckboxField(value: unknown): value is PickerCheckboxField {
+  if (!isValidObject(value)) return false;
+  return "type" in value && value.type === "checkbox";
 }
