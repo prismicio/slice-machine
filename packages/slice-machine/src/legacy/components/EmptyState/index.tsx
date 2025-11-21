9c9c292a1bb517e5458e8789eda270228f573c2a
José Pereira <7235666+jomifepe@users.noreply.github.com>
2025-11-21 14:31:24 +0000

    feat: slice dropdown menu for empty state

--- packages/slice-machine/src/legacy/components/EmptyState/index.tsx
+++ packages/slice-machine/src/legacy/components/EmptyState/index.tsx
@@ -1,12 +1,22 @@
+import {
+  ActionList,
+  ActionListItem,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@prismicio/editor-ui";
 import { Video } from "cloudinary-react";
 import React from "react";
 import { Box, Button, Heading, Text } from "theme-ui";
 
 import { telemetry } from "@/apiClient";
+import { getSliceCreationOptions } from "@/features/customTypes/customTypesBuilder/sliceCreationOptions";
 
 interface Props {
   title: string;
   onCreateNew: () => void;
+  onCreateFromImage: () => void;
   buttonText: string;
   documentationComponent: React.ReactNode;
   videoPublicIdUrl: string;
@@ -15,111 +25,132 @@ interface Props {
 const EmptyState: React.FunctionComponent<Props> = ({
   title,
   onCreateNew,
+  onCreateFromImage,
   buttonText,
   documentationComponent,
   videoPublicIdUrl,
   ...restProps
-}) => (
-  <Box
-    sx={{
-      display: "flex",
-      width: "80%",
-      flexWrap: "wrap",
-      justifyContent: "center",
-    }}
-    {...restProps}
-  >
-    <Box
-      sx={(theme) => ({
-        display: "flex",
-        flex: 1,
-        alignItems: "center",
-        minWidth: "400px",
-        maxWidth: "70%",
-        border: `1px solid ${theme.colors?.grey02 as string}`,
-      })}
-    >
-      <Video
-        cloudName="dmtf1daqp"
-        controls
-        loop
-        style={{
-          maxWidth: "100%",
-          objectFit: "contain",
-        }}
-        publicId={videoPublicIdUrl}
-        onPlay={() => {
-          void telemetry.track({
-            event: "open-video-tutorials",
-            video: videoPublicIdUrl,
-          });
-        }}
-      />
-    </Box>
+}) => {
+  const sliceCreationOptions = getSliceCreationOptions({
+    menuType: "Dropdown",
+  });
 
+  return (
     <Box
-      sx={(theme) => ({
-        bg: "white",
+      sx={{
         display: "flex",
-        flexDirection: "column",
-        border: `1px solid ${theme.colors?.grey02 as string}`,
-        flex: 1,
-        minWidth: "400px",
-        maxWidth: "70%",
-      })}
+        width: "80%",
+        flexWrap: "wrap",
+        justifyContent: "center",
+      }}
+      {...restProps}
     >
       <Box
         sx={(theme) => ({
           display: "flex",
-          flexDirection: "column",
-          p: 4,
-          borderBottom: `1px solid ${theme.colors?.grey02 as string}`,
+          flex: 1,
+          alignItems: "center",
+          minWidth: "400px",
+          maxWidth: "70%",
+          border: `1px solid ${theme.colors?.grey02 as string}`,
         })}
       >
-        <Heading
-          as="h3"
-          variant={"heading"}
-          sx={{ fontSize: "16px", lineHeight: "24px", mb: 2 }}
-        >
-          {title}
-        </Heading>
-        <Text variant="xs" sx={{ lineHeight: "24px", fontSize: "13px" }}>
-          {documentationComponent}
-        </Text>
+        <Video
+          cloudName="dmtf1daqp"
+          controls
+          loop
+          style={{
+            maxWidth: "100%",
+            objectFit: "contain",
+          }}
+          publicId={videoPublicIdUrl}
+          onPlay={() => {
+            void telemetry.track({
+              event: "open-video-tutorials",
+              video: videoPublicIdUrl,
+            });
+          }}
+        />
       </Box>
+
       <Box
-        sx={{
+        sx={(theme) => ({
+          bg: "white",
           display: "flex",
-          p: 4,
-          alignItems: "center",
-        }}
+          flexDirection: "column",
+          border: `1px solid ${theme.colors?.grey02 as string}`,
+          flex: 1,
+          minWidth: "400px",
+          maxWidth: "70%",
+        })}
       >
-        <Button
-          onClick={onCreateNew}
-          data-testid="empty-state-main-button"
+        <Box
+          sx={(theme) => ({
+            display: "flex",
+            flexDirection: "column",
+            p: 4,
+            borderBottom: `1px solid ${theme.colors?.grey02 as string}`,
+          })}
+        >
+          <Heading
+            as="h3"
+            variant={"heading"}
+            sx={{ fontSize: "16px", lineHeight: "24px", mb: 2 }}
+          >
+            {title}
+          </Heading>
+          <Text variant="xs" sx={{ lineHeight: "24px", fontSize: "13px" }}>
+            {documentationComponent}
+          </Text>
+        </Box>
+        <Box
           sx={{
             display: "flex",
-            justifyContent: "center",
+            p: 4,
             alignItems: "center",
-            flexShrink: 0,
-            mr: 4,
           }}
         >
-          {buttonText}
-        </Button>
-        <Text
-          sx={{
-            fontSize: "12px",
-            color: "grey04",
-            maxWidth: "280px",
-          }}
-        >
-          It will be stored locally and you will be able to push it to your
-          repository
-        </Text>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Button data-testid="empty-state-main-button" sx={{ mr: 4 }}>
+                {buttonText}
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="center">
+              <DropdownMenuItem
+                renderStartIcon={() =>
+                  sliceCreationOptions.fromImage.BackgroundIcon
+                }
+                onSelect={onCreateFromImage}
+                description={sliceCreationOptions.fromImage.description}
+              >
+                {sliceCreationOptions.fromImage.title}
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                renderStartIcon={() =>
+                  sliceCreationOptions.fromScratch.BackgroundIcon
+                }
+                onSelect={onCreateNew}
+                description={sliceCreationOptions.fromScratch.description}
+              >
+                {sliceCreationOptions.fromScratch.title}
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <Text
+            sx={{
+              fontSize: "12px",
+              color: "grey04",
+              maxWidth: "280px",
+            }}
+          >
+            It will be stored locally and you will be able to push it to your
+            repository
+          </Text>
+        </Box>
       </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default EmptyState;
